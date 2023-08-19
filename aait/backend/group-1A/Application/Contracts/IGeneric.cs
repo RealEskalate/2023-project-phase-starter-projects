51c83a7d3e16806c92e37b4abf9a5b368533e2ae
Tofik Abdu <tofikabdu2002@gmail.com>
2023-08-19 10:04:19 +0300

    fix(AAiT-backend-1A): Create Post Domain

---
+++ aait/backend/group-1A/Application/Contracts/IGeneric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Contracts
+{
+    public interface IGeneric<T>
+    {
+        public Task<T> Get(int id);
+
+        public Task<List<T>> GetAll();
+
+        public Task<T> Add(T entity);
+
+        public Task<int> Delete(int id);
+
+        public Task<T> Update(int id, T entity);   
+
+        public Task<bool> Exists(int id);
+
+    }
+}
---
+++ aait/backend/group-1A/Domain/Entites/Post.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entites
+{
+    public class Post : BaseEntity
+    {
+        public string Title { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
+    }
+}
