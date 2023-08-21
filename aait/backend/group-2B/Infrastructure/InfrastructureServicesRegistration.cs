b0d71221e33574a9b0be5c7cef3b0bb641e1f159
ffekirnew <ffekirnew0808@gmail.com>
2023-08-21 17:05:44 +0300

    feat(AAiT-backend-2b): Add authentication and authorization logic and dependency injection to the Infrastructure layer

--- aait/backend/group-2B/Infrastructure/InfrastructureServicesRegistration.cs
+++ aait/backend/group-2B/Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,5 +1,8 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using SocialSync.Application.Contracts.Infrastructure;
 using SocialSync.Infrastructure.DateTimeService;
 using SocialSync.Infrastructure.JWT;
@@ -8,14 +11,42 @@ namespace SocialSync.Infrastructure;
 
 public static class InfrastructureServicesRegistration
 {
-    public static IServiceCollection RegiserInfrastructureServices(
+    public static IServiceCollection RegisterInfrastructureServices(
         this IServiceCollection services,
         IConfiguration configuration
     )
     {
-        services.AddSingleton<IJwtGenerator, JwtGenerator>();
+        services.RegisterAuthenticationServices(configuration);
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        return services;
+    }
+
+    public static IServiceCollection RegisterAuthenticationServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
         services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+        services.AddSingleton<IJwtGenerator, JwtGenerator>();
+
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+          .AddJwtBearer(options =>
+          {
+              var jwtSettings = configuration.GetSection(JwtSettings.SectionName).Get<JwtSettings>()!;
+
+              options.TokenValidationParameters = new TokenValidationParameters
+              {
+                  ValidateIssuer = true,
+                  ValidateAudience = true,
+                  ValidateLifetime = true,
+                  ValidateIssuerSigningKey = true,
+                  ValidIssuer = jwtSettings.Issuer,
+                  ValidAudience = jwtSettings.Audience,
+                  IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+              };
+          });
+
         return services;
     }
 }
