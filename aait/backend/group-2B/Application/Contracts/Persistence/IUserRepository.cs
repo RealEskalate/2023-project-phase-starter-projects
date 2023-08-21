657a13a4113f85a94463fe3070089809ed94aaae
ffekirnew <ffekirnew0808@gmail.com>
2023-08-21 14:04:50 +0300

    feat(AAiT-backend-2B): Add additional methods for IUserRepository

--- aait/backend/group-2B/Application/Contracts/Persistence/IUserRepository.cs
+++ aait/backend/group-2B/Application/Contracts/Persistence/IUserRepository.cs
@@ -2,8 +2,9 @@ using SocialSync.Domain.Entities;
 
 namespace SocialSync.Application.Contracts.Persistence;
 
-public interface IUserRepository : IGenericRepository<User> {
-  public Task<bool> UsernameExists(string username);
-  public Task<User> GetByUsername(string username);
-  public  Task<bool> EmailExists(string email);
+public interface IUserRepository : IGenericRepository<User>
+{
+    public Task<bool> UsernameExists(string username);
+    public Task<User> GetByUsername(string username);
+    public Task<bool> EmailExists(string email);
 }
