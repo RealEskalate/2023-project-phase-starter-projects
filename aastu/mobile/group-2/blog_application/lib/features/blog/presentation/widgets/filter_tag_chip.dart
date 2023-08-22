c3e0361230ac208761060522b242bbe5b0e75dc4
natnael-tafesse-a2sv <natnael.tafesse@a2sv.org>
2023-08-22 20:10:58 +0300

    fix(Mobile): add flexible to the rows

--- aastu/mobile/group-2/blog_application/lib/features/blog/presentation/widgets/filter_tag_chip.dart
+++ aastu/mobile/group-2/blog_application/lib/features/blog/presentation/widgets/filter_tag_chip.dart
@@ -11,33 +11,35 @@ class FilterTagChip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FilterChip(
-              selected: selected,
-              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 1),
-              showCheckmark: false,
-              selectedColor: Theme.of(context).colorScheme.primary,
-              onSelected: (bool value) {},
-              labelStyle: TextStyle(
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              side: BorderSide(
-                  
-                  width: 2, 
+    return Flexible(
+      child: FilterChip(
+                selected: selected,
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 1),
+                showCheckmark: false,
+                selectedColor: Theme.of(context).colorScheme.primary,
+                onSelected: (bool value) {},
+                labelStyle: TextStyle(
                   color: Theme.of(context).colorScheme.primary,
-                  ),
-              label: Center(
-                child: Text(
-                  name,
-                  style: TextStyle(
-                    color: selected ? Colors.white : Theme.of(context).colorScheme.primary,
-                      
-                  ),
-                  ),
+                ),
+                side: BorderSide(
+                    
+                    width: 2, 
+                    color: Theme.of(context).colorScheme.primary,
+                    ),
+                label: Center(
+                  child: Text(
+                    name,
+                    style: TextStyle(
+                      color: selected ? Colors.white : Theme.of(context).colorScheme.primary,
+                        
+                    ),
+                    ),
+                ),
+                shape: RoundedRectangleBorder(
+    
+                  borderRadius: BorderRadius.circular(75),
+                ),
               ),
-              shape: RoundedRectangleBorder(
-
-                borderRadius: BorderRadius.circular(75),
-              ),
-            );
+    );
   }
 }
