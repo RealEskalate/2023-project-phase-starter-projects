29cb73ed17defc3f22aabbeee4a77f22bec2da49
= <amanuel.tewodros@a2sv.org>
2023-08-22 14:47:04 +0300

    feat(AAiT-backend-2b): Add dbsets to dbcontext

--- aait/backend/group-2B/Persistence/SocialSyncDbContext.cs
+++ aait/backend/group-2B/Persistence/SocialSyncDbContext.cs
@@ -1,9 +1,48 @@
 using Microsoft.EntityFrameworkCore;
+using SocialSync.Domain.Common;
+using SocialSync.Domain.Entities;
 
 namespace SocialSync.Persistence;
 
 public class SocialSyncDbContext : DbContext
 {
+    public DbSet<User> Users{get; set;}
+    public DbSet<Post> Posts{get; set;}
+    public DbSet<Notification> Notifications{get; set;}
+    public DbSet<Interaction> Interactions{get; set;}
     public SocialSyncDbContext(DbContextOptions<SocialSyncDbContext> options)
         : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseAuditableEntity>())
+        {
+            entry.Entity.LastModified = DateTime.Now.ToUniversalTime();
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.Now.ToUniversalTime();
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+
+    }
+
 }
+
+
+
+
+
+
