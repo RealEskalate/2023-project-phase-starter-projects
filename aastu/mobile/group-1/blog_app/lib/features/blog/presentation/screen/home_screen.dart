9d724cadcbe3d6f4e96c1161bc86c09a57006fa6
gemechis-elias-a2sv <gemechis.elias@a2sv.org>
2023-08-22 10:33:34 +0300

    Setup(AASTU Mobile G1): Project Structure Setup

---
+++ aastu/mobile/group-1/blog_app/lib/features/blog/presentation/screen/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:blog_app/features/blog/presentation/widgets/home_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: getHomeAppBar(),
+        body: const Center(
+          child: Text("Home Screen"),
+        ));
+  }
+}
