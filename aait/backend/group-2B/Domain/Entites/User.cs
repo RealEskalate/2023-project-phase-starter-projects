1c907c2f30d9ef14375e161c7014add647254780
ffekirnew <ffekirnew0808@gmail.com>
2023-08-22 10:38:47 +0300

    feat(aait.bac.2b): Create user entity

---
+++ aait/backend/group-2B/Domain/Entites/User.cs
@@ -0,0 +1,14 @@
+using SocialSync.Domain.Common;
+
+namespace SocialSync.Domain.Entities;
+
+public class User : BaseAuditableEntity
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Bio { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+}
