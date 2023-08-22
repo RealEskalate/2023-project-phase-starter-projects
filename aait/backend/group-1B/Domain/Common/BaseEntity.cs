26c935e14065940a7fb5165bdf21694928d7a036
Sahib Semahegn <semahegnsahib@gmail.com>
2023-08-22 10:26:45 +0300

    Add(AAiT-backend-1B) : Add user and follow entities (#12)

---
+++ aait/backend/group-1B/Domain/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+    }
+}
