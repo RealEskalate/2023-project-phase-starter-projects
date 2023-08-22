0bf482a1cf5c1c3e7407bddaff7f94dd058330e6
Sahib Semahegn <semahegnsahib@gmail.com>
2023-08-22 10:26:45 +0300

    Add(AAiT-backend-1B) : Add user and follow entities (#12)

---
+++ aait/backend/group-1B/Domain/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string NumFollowers { get; set; } 
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
+        public string Bio { get; set; }
+
+        public virtual IEnumerable<Follow> Followers { get; set; }
+
+        public virtual IEnumerable<Follow> Followees { get; set; }
+
+    }
+}
