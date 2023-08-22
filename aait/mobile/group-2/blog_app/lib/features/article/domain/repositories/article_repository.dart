09caedbea37ce1a25320753ed199f76aa0be6ab0
betabravahC:/Users/betty/AppData/Local/Programs/Python/Python310/python.exe <betabravah@gmail.com>
2023-08-22 14:35:42 +0300

    fix(AAiT-mobile-2): Remove Pubspec Lock

--- aait/mobile/group-2/blog_app/lib/features/article/domain/repositories/article_repository.dart
+++ aait/mobile/group-2/blog_app/lib/features/article/domain/repositories/article_repository.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 
-import '../../../../core/error/failures.dart';
+import '../../../../core/error/failure.dart';
 import '../entities/article.dart';
 
 abstract class ArticleRepository {
