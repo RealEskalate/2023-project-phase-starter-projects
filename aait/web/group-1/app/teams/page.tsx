891176c73ee3eabd4845746b36acb57844f090bf
unknown <pauldessie@gmail.com>
2023-08-22 22:19:03 +0300

    feat(web): make nav-bar ui

---
+++ aait/web/group-1/app/teams/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import React from 'react'
+import TeamImage from '../../public/images/team-work.svg'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
