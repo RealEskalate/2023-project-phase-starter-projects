120766d37552152692d53fca6734f0bb54d651dc
amanuel <amanuelyihunie27@gmail.com>
2023-08-22 16:23:43 +0300

    feat(AAiT-backend-1a) folder rename

--- aait/backend/group-1A/Application/Profiles/MappingProfile.cs
+++ aait/backend/group-1A/Application/Profiles/MappingProfile.cs
@@ -19,6 +19,8 @@ namespace Application.Profiles
             CreateMap<ReactionResponseDTO, PostReaction>().ReverseMap();
             CreateMap<PostUpdateDTO, Post>().ReverseMap();
             
+            CreateMap<RegisterUserDto, User>();
+            CreateMap<LoginUserDto, User>();
         }
     }
 }
