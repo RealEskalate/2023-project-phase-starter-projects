6edfc836765a415e95f5f5f2e750bfb97f1cf7bc
Tofik Abdu <104857366+tofik-abdu-a2sv@users.noreply.github.com>
2023-08-22 11:32:16 +0300

    fix(AAiT-backend-1A): Project Setup and Post Feature Created (#14)

---
+++ aait/backend/group-1A/Application/DTO/Common/ReactionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTO.Common
+{
+    public class ReactionDTO
+    {
+        public int ReactedId { get; set; }
+
+        public string ReactionType { get; set; }
+    }
+}
---
+++ aait/backend/group-1A/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Application.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+using Persistence.Repositories;
+using System.Reflection;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<SocialMediaDbContext>(opt =>
+                 opt.UseNpgsql(configuration.GetConnectionString("SocialMediaApp")));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IPostRepository, PostRepository>();
+            services.AddScoped<IPostReactionRepository, PostReactionRepository>();
+
+            return services;
+        }
+    }
+}
+
+
+
+//
