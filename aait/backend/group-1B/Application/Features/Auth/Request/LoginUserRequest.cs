a8f5e3e0aed39f5076dbb4966260a6785215c01e
Sahib Semahegn <semahegnsahib@gmail.com>
2023-08-22 21:09:09 +0300

    add(AAiT-backend-1B) : add user authetication - login and register resolved merge conflicts

---
+++ aait/backend/group-1B/Application/Features/Auth/Request/LoginUserRequest.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOS.Auth;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Request
+{
+    public class LoginUserRequest : IRequest<LoginResponseDto>
+    {
+        public LoginRequestDto loginRequest { get; set; }
+    }
+}
