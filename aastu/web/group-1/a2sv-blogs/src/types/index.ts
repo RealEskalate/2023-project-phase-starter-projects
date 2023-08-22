ff3f754c457d0509a93378cd02f4de98194baffa
Metsehafe Eyasu Tesfu <118991565+Metsehafe-Eyasu@users.noreply.github.com>
2023-08-22 11:06:30 +0300

    feat(AASTU-web-1): Project setup (#22)

---
+++ aastu/web/group-1/a2sv-blogs/src/types/index.ts
@@ -0,0 +1,6 @@
+export interface Credentials {
+  name?: string,
+  email: string,
+  password: string
+}
+
---
+++ aastu/web/group-1/a2sv-blogs/tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        nunito: ['Nunito', 'sans-serif'],
+        french: ['IM Fell French Canon', 'serif'],
+      },
+      colors: {
+        primary: '#264FAD',
+        secondary: '#5F0A87',
+        "text-header" : {
+          1: '#363636',
+          2: '#424242',
+        },
+        "text-content" : "#7d7d7d"
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
