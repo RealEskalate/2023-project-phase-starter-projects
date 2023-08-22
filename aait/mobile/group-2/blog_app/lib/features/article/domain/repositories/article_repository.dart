f8938ec5d602a17870f1fadfd9a3e08ef4639788
Bethelhem Yemane <101593044+Betabravah@users.noreply.github.com>
2023-08-22 15:43:28 +0300

    feat(AAiT-mobile-2): Add Usecases (#37)
    Co-authored-by: Leul Wujira <meraf000@gmail.com>

--- aait/mobile/group-2/blog_app/lib/features/article/domain/repositories/article_repository.dart
+++ aait/mobile/group-2/blog_app/lib/features/article/domain/repositories/article_repository.dart
@@ -6,7 +6,7 @@ import '../entities/article.dart';
 abstract class ArticleRepository {
   Future<Either<Failure, List<Article>>> getAllArticles();
   Future<Either<Failure, Article>> createArticle(Article article);
-  Future<Either<Failure, Article>> getArticle(Article article);
+  Future<Either<Failure, Article>> getArticle(String id);
   Future<Either<Failure, Article>> updateArticle(Article article);
-  Future<Either<Failure, Article>> deleteArticle(Article article);
+  Future<Either<Failure, Article>> deleteArticle(String id);
 }
