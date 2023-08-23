87641a6f964a67e27f1606d206a68f0667884d53
zerubabel-kassahun-a2sv <142803563+zerubabel-kassahun-a2sv@users.noreply.github.com>
2023-08-23 11:42:45 +0300

    feat(aastu-web-g1): added redux wrapper and custom hooks

---
+++ aastu/web/group-1/a2sv-blogs/src/lib/redux/services/baseApi.ts
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { RootState } from "../store";
+import { HYDRATE } from "next-redux-wrapper";
+
+const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://a2sv-backend.onrender.com/api/",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  extractRehydrationInfo(action, { reducerPath }) {
+    if (action.type === HYDRATE) {
+      return action.payload[reducerPath];
+    }
+  },
+  endpoints: (builder) => ({}),
+});
+
+export default baseApi;
--- aastu/web/group-1/a2sv-blogs/src/types/index.ts
+++ aastu/web/group-1/a2sv-blogs/src/types/index.ts
@@ -1,6 +1,74 @@
 export interface Credentials {
-  name?: string,
-  email: string,
-  password: string
+  name?: string;
+  email: string;
+  password: string;
 }
 
+export interface SignupResponse {
+  token: string;
+  userId: string;
+}
+
+export interface LoginResponse {
+  user: string;
+  token: string;
+  userRole: string;
+  userName: string;
+}
+export interface User {
+  name: string;
+  email: string;
+}
+
+export interface Author {
+  _id: string;
+  name: string;
+  email: string;
+  image: string;
+  role: string;
+}
+
+export interface Blog {
+  _id: string;
+  image: string;
+  title: string;
+  description: string;
+  author: Author | null;
+  isPending: boolean;
+  tags: string[];
+  likes: number;
+  relatedBlogs: string[];
+  skills: string[];
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+export interface SuccessStory {
+  _id: string;
+  personName: string;
+  imgURL: string;
+  role: string;
+  location: string;
+  story: StorySection[];
+  __v: number;
+}
+
+export interface StorySection {
+  heading: string;
+  paragraph: string;
+  _id: string;
+}
+
+export interface Member {
+  socialMedia: {
+    linkedin: string;
+    facebook: string;
+    instagram: string;
+  };
+  _id: string;
+  name: string;
+  bio: string;
+  department: string;
+  __v: number;
+}
