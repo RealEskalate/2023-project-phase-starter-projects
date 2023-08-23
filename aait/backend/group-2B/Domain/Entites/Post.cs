8c64df4968248780e86b67d1f3f5040ffc49105b
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-08-23 10:14:45 +0300

    feat(AAiT-backend-2b): Set up project files (#44)
    Co-authored-by: ffekirnew <ffekirnew0808@gmail.com>
    Co-authored-by: ffekirnew <fikernew.birhanu@a2sv.org>

---
+++ aait/backend/group-2B/Domain/Entites/Post.cs
@@ -0,0 +1,13 @@
+using SocialSync.Domain.Common;
+
+namespace SocialSync.Domain.Entities;
+
+public class Post : BaseEntity
+{
+    public string Content { get; set; } = null!;
+    public int UserId { get; set; }
+
+    public virtual User User { get; set; } = null!;
+
+    public virtual ICollection<Interaction> Interactions { get; set; } = null!;
+}
