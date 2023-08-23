f2607374c82ab9d267f055bde5cd22241ae02065
= <amanuel.tewodros@a2sv.org>
2023-08-23 16:32:45 +0300

    fix(AAiT-backend-2b): merge conflict

--- aait/backend/group-2B/Application/Contracts/Persistence/IPostRepository.cs
+++ aait/backend/group-2B/Application/Contracts/Persistence/IPostRepository.cs
@@ -1,5 +1,10 @@
 using SocialSync.Domain.Entities;
 
 namespace SocialSync.Application.Contracts.Persistence;
+public interface IPostRepository : IGenericRepository<Post>
+{
+    public Task<IReadOnlyList<Post>> GetPostsByUserId(int userId);
+    public Task<IReadOnlyList<Post>> GetPostsByTags(List<string> tags);
 
-public interface IPostRepository : IGenericRepository<Post> { }
+    public Task<bool> Exists(int Id);
+}
---
+++ aait/backend/group-2B/Application/Response/BaseResponse.cs
@@ -0,0 +1,10 @@
+namespace SocialSync.Application.Responses
+{
+    public class BaseCommandResponse
+    {
+        public int Id { get; set; }
+        public bool Success { get; set; } = true;
+        public string? Message { get; set; }
+        public List<string> Errors { get; set; } = null!;
+    }
+}
