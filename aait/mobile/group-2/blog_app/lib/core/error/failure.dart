e799564569a7c7ea332809f6c87e5fcc923855b3
Leul Wujira <meraf000@gmail.com>
2023-08-23 11:31:48 +0300

    feat(AAiT-mobile-2): Add Network Exception and Failure

--- aait/mobile/group-2/blog_app/lib/core/error/failure.dart
+++ aait/mobile/group-2/blog_app/lib/core/error/failure.dart
@@ -6,3 +6,9 @@ abstract class Failure extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
+
+class NetworkFailure extends Failure {}
