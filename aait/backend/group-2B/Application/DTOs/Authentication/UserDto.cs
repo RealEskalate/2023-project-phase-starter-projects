cc3827d257222f48d3788353c514ec2f99fcee08
ffekirnew <fikernew.birhanu@a2sv.org>
2023-08-23 17:29:54 +0300

    fix(aait-backend-2b): Remove password from the dto

--- aait/backend/group-2B/Application/DTOs/Authentication/UserDto.cs
+++ aait/backend/group-2B/Application/DTOs/Authentication/UserDto.cs
@@ -9,7 +9,6 @@ public class UserDto : BaseDto
     public string Bio { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Username { get; set; } = null!;
-    public string Password { get; set; } = null!;
     public string Phone { get; set; } = null!;
 }
 
