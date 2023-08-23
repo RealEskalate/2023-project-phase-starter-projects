e1dbcfa8dfdcfc0ddea9da796123cd07fa373226
Yakin Teshome <yakin16tm@gmail.com>
2023-08-24 02:16:09 +0300

    customize fonts and color values

--- aait/web/group-2/tailwind.config.ts
+++ aait/web/group-2/tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary':'#264FAD',
+        'secondary': '#2B2A35',
+        'tertiary': '#545465',
+      },
+      fontFamily: {
+        'poppins': ['Poppins'],
+        'nunito':['Nunito'],
+      }
     },
   },
   plugins: [],
