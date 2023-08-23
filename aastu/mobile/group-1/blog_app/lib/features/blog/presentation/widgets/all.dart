7b93241a6c4ad004a453385160d38f827c8ffaf5
Milkaai-Getachew <139752751+Milkaai-Getachew@users.noreply.github.com>
2023-08-23 16:09:47 +0300

    feat(mobile): add home page and widgets (#114)

---
+++ aastu/mobile/group-1/blog_app/lib/features/blog/presentation/widgets/all.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class All extends StatefulWidget {
+  const All({super.key});
+
+  @override
+  State<All> createState() => _AllState();
+}
+
+class _AllState extends State<All> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('all'),
+        )
+      );
+  }
+}
