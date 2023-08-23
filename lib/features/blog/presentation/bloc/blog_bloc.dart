ca224737301126d8148f233f9ec7c47471013bd9
abrhame <ablakew7ab@gamil.com>
2023-08-23 09:49:17 +0300

    merge

---
+++ lib/features/blog/presentation/bloc/blog_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'blog_event.dart';
+part 'blog_state.dart';
+
+class BlogBloc extends Bloc<BlogEvent, BlogState> {
+  BlogBloc() : super(BlogInitial()) {
+    on<BlogEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
