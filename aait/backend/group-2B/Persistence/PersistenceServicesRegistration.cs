84097e55c71b630568489be5d1530b30b78f1bf2
Amanuel94 <54166751+Amanuel94@users.noreply.github.com>
2023-08-23 10:14:45 +0300

    feat(AAiT-backend-2b): Set up project files (#44)
    Co-authored-by: ffekirnew <ffekirnew0808@gmail.com>
    Co-authored-by: ffekirnew <fikernew.birhanu@a2sv.org>

---
+++ aait/backend/group-2B/Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SocialSync.Application.Contracts.Persistence;
+using SocialSync.Persistence.Repositories;
+
+namespace SocialSync.Persistence;
+public static class PersistenceServicesRegistration
+{
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddDbContext<SocialSyncDbContext>(options =>
+        {
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConn")
+            );
+        });
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        return services;
+    }
+}
