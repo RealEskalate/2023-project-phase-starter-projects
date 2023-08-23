e01137e4c40489e068721cdbd8bb630d186b16eb
Natnael Tafesse <142803038+natnael-tafesse-a2sv@users.noreply.github.com>
2023-08-23 11:59:14 +0300

    fix(Mobile):Update main.dart

--- aastu/mobile/group-2/blog_application/lib/main.dart
+++ aastu/mobile/group-2/blog_application/lib/main.dart
@@ -1,30 +1,11 @@
-import 'package:blog_application/features/blog/presentation/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-
-import 'features/blog/presentation/screens/home_page.dart';
+import 'package:blog_application/features/blog/presentation/pages/articles_reading_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF376AED)),
-        fontFamily: GoogleFonts.poppins().fontFamily,
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-
-      home: const ProfileScreen(),
-    );
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ArticleReading(
+      likes: 0,
+    ),
+  ));
 }
