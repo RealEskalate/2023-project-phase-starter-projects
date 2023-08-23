2d1d80da36421cae05bfe39699a3d6718fbb8ed6
Shamil <shamilbedru47@gmail.com>
2023-08-23 14:41:03 +0300

    feature(aait.mobile.group1.Shamil-Bedru.Profile-page) (#79)

---
+++ aait/mobile/group-1/blog_app/lib/core/errors/failures/server_failure.dart
@@ -0,0 +1,5 @@
+import 'failure.dart';
+
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+}
---
+++ aait/mobile/group-1/blog_app/lib/features/user_profile/data/models/user_model.dart
@@ -0,0 +1,60 @@
+import 'package:equatable/equatable.dart';
+
+import '../../domain/entities/user_entity.dart';
+
+class UserModel extends User implements Equatable {
+  UserModel(
+      {required this.id,
+      required this.fullName,
+      required this.email,
+      this.expertise,
+      this.bio,
+      this.image})
+      : super(
+            id: id,
+            fullName: fullName,
+            email: email,
+            expertise: expertise,
+            bio: bio,
+            image: image);
+
+  @override
+  final String id;
+  @override
+  final String fullName;
+  @override
+  final String email;
+  @override
+  final String? expertise;
+  @override
+  final String? bio;
+  @override
+  final String? image;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+        id: json['id'],
+        fullName: json['fullName'],
+        email: json['email'],
+        expertise: json['expertise'],
+        bio: json['bio'],
+        image: json['image']);
+  }
+
+  Map<String?, dynamic> toJson(UserModel userModel) {
+    return {
+      id: userModel.id,
+      fullName: userModel.fullName,
+      email: userModel.email,
+      expertise: userModel.expertise,
+      bio: userModel.bio,
+      image: userModel.image
+    };
+  }
+
+  @override
+  List<Object?> get props => [id];
+
+  @override
+  bool? get stringify => false;
+}
