f9c8fa17b05a9ceff721ba00a824d1de44b4744e
heyeman-urgessa-a2sv <heyeman@a2sv.org>
2023-08-23 12:15:07 +0300

    feat(mobile): Created custom form widgets

---
+++ aastu/mobile/group-1/blog_app/lib/features/blog/presentation/widgets/AddButtonCustom.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AddButtonCustom extends StatelessWidget {
+  AddButtonCustom({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+      child: CircleAvatar(
+        backgroundColor: Colors.white,
+        // radius: 12,
+        child: IconButton(
+          padding: EdgeInsets.zero,
+          icon: Icon(Icons.add_circle_outline),
+          color: Colors.blue,
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
