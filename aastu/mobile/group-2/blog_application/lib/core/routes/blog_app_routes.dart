eb3b5167e08fe5b96a32dfae7af90b04176da51e
Natnael Yeshiwas <natiyeshi16@gmail.com>
2023-08-23 16:17:18 +0300

    feat(mobile): add routing

--- aastu/mobile/group-2/blog_application/lib/core/routes/blog_app_routes.dart
+++ aastu/mobile/group-2/blog_application/lib/core/routes/blog_app_routes.dart
@@ -5,4 +5,6 @@ class BlogAppRoutes {
   static const ARTICLE_CREATE = '/article-create';
   static const ARTICLE_EDIT = '/article-edit';
   static const AUTH = '/auth';
+  static const PROFILE = '/profile';
+  static const ONBOARDING = '/on-boarding';
 }
--- aastu/mobile/group-2/blog_application/lib/main.dart
+++ aastu/mobile/group-2/blog_application/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:blog_application/features/blog/presentation/pages/articles_reading_screen.dart';
+import "./Route.dart" as route;
+import "core/routes/blog_app_routes.dart";
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: ArticleReading(
-      likes: 0,
-    ),
+    onGenerateRoute: route.controller,
+    initialRoute: BlogAppRoutes.AUTH,
   ));
 }
