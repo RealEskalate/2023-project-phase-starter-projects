8a98ef3db7032ca8ef920f35d5813aa498489bad
Gemechis Elias <141812075+gemechis-elias-a2sv@users.noreply.github.com>
2023-08-23 09:24:42 +0300

    feat(AASTU-Mobile-1): Added Article Model on data layer (#64)

---
+++ aastu/mobile/group-1/blog_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:blog_app/core/bloc.dart';
+import 'package:blog_app/features/blog/presentation/screen/home_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/material.dart';
+import 'injection.dart' as di;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await di.init();
+  Bloc.observer = const AppBlocObserver();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blog App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.white, secondary: const Color(0xffEE6F57)),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(), // TODO: Replace with the Onboarding
+      routes: {
+        '/home': (context) => const HomeScreen(),
+        // define routes here
+      },
+    );
+  }
+}
