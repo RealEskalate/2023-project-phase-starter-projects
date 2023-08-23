275a433ec2024b679337ee5ec44d358b38d6fac1
Yeabtsega Yifat <yeabtse7@gmail.com>
2023-08-23 11:50:51 +0300

    feat(AAiT-bac-g2a): add Comment DTO and CQRS (#56)

---
+++ aait/backend/group-2A/Application/Features/Comment/Handlers/Commands/DeleteCommentCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Contracts.Persistance;
+using Application.Features.Comment.Requests.Commands;
+using Application.Features.Post.Request.Commands;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Comment.Handlers.Commands;
+
+public class DeleteCommentCommandHandler :  IRequestHandler<DeleteCommentCommand, Unit>
+{
+    private readonly ICommentRepository _commentRepository;
+    private readonly IMapper _mapper;
+
+    public DeleteCommentCommandHandler(ICommentRepository commentRepository, IMapper mapper)
+    {
+        _commentRepository = commentRepository;
+        _mapper = mapper;
+    }
+    
+    public async Task<Unit> Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+    {
+        var comment = await _commentRepository.Get(request.Id);
+        
+        if (comment == null)
+        {
+            throw new Exception();
+        }
+        
+        
+        await _commentRepository.Delete(comment);
+        return Unit.Value;
+    }
+}
