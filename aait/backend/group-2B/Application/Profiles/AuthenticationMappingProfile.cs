4d9c1a2780358b74338a98e990dde36f92943acb
ffekirnew <fikernew.birhanu@a2sv.org>
2023-08-23 11:16:57 +0300

    feat(aait.bac.2b.authentication): Add automapper mapping profiles for authentication DTOs

---
+++ aait/backend/group-2B/Application/Profiles/AuthenticationMappingProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using SocialSync.Application.DTOs.Authentication;
+using SocialSync.Domain.Entities;
+
+namespace HRLeaveManagement.Application.Profiles;
+
+public class AuthenticationMappingProfile : Profile
+{
+    public AuthenticationMappingProfile()
+    {
+        CreateMap<RegisterUserDto, User>();
+    }
+}
