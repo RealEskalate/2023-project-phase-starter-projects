d7822b4d08bed3eb79cedb80fbac0a8cf35ac363
matiyas-seifu-a2sv <142802988+matiyas-seifu-a2sv@users.noreply.github.com>
2023-08-23 11:27:25 +0300

    Mati.folder structure (#39)

---
+++ aastu/mobile/group-3/blog_application_aastu_grp3/lib/features/onboarding/onboarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+}
