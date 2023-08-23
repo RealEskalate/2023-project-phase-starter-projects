5a0ff3dd79a6110bf23e668d7076ddc57da6a0b4
abrhame <ablakew7ab@gamil.com>
2023-08-23 10:08:04 +0300

    Revert "merge"
    This reverts commit ca224737301126d8148f233f9ec7c47471013bd9.

--- lib/core/routes/blog_app_routes.dart
+++
@@ -1,8 +0,0 @@
-class BlogAppRoutes {
-  static const HOME = '/';
-  static const ARTICLES = '/articles';
-  static const ARTICLE_DETAIL = '/article-detail';
-  static const ARTICLE_CREATE = '/article-create';
-  static const ARTICLE_EDIT = '/article-edit';
-  static const AUTH = '/auth';
-}
--- lib/features/blog/presentation/bloc/blog_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'blog_bloc.dart';
-
-abstract class BlogEvent extends Equatable {
-  const BlogEvent();
-
-  @override
-  List<Object> get props => [];
-}
