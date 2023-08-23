493d2b428ba7cb7ab2ada57b03a5a5f42881d334
natnael-tafesse-a2sv <natnael.tafesse@a2sv.org>
2023-08-23 15:16:52 +0300

    Fix(Mobile): Add usecases for article fetch

---
+++ aastu/mobile/group-2/blog_application/lib/features/blog/domain/usecases/get_article.dart
@@ -0,0 +1,28 @@
+import 'package:blog_application/core/exceptions/Failure.dart';
+import 'package:blog_application/features/blog/domain/entities/article.dart';
+import 'package:blog_application/features/blog/domain/repositories/article_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../../core/usecases/usecase.dart';
+
+class GetArticle extends UseCase<Article , GetArticleParams>{
+  final ArticleRepository repository;
+  GetArticle(this.repository);
+
+  @override
+  Future<Either<Failure, Article>> call(GetArticleParams param) async{
+    return await repository.getArticle(param.id);
+  }
+
+  
+}
+
+class GetArticleParams extends Equatable{
+  final String id;
+  const GetArticleParams(this.id);
+  
+  @override
+  List<Object?> get props => [id];
+
+}
