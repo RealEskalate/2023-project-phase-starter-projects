eb6c417fd9ff31185f85de7b765a4da6763ee5fb
Natanim Ashenafi Kefeni <142803189+natanim-ashenafi@users.noreply.github.com>
2023-08-23 20:12:27 +0300

    feat(AASTU.web.G2): setup redux-toolkit and rtk (#139)
    Co-authored-by: Natanim Ashenafi <natanimashenafi20@gmail.com>

--- aastu/web/group-2/app/components/header.tsx
+++ aastu/web/group-2/app/components/header.tsx
@@ -22,7 +22,7 @@ const Header: FC = () => {
                 <li className={`mr-9 font-secondaryFont font-semibold ${currentRoute === "" ? "text-[#264FAD] pb-8 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"}`}><Link href='#'>Teams</Link></li>
                 <li className={`mr-9 font-secondaryFont font-semibold ${currentRoute === "" ? "text-[#264FAD] pb-8 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"}`}><Link href='#'>Success Stories</Link></li>
                 <li className={`mr-9 font-secondaryFont font-semibold ${currentRoute === "" ? "text-[#264FAD] pb-8 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"}`}><Link href='#'>About Us</Link></li>
-                <li className={`mr-9 font-secondaryFont font-semibold ${currentRoute === "" ? "text-[#264FAD] pb-8 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"}`}><Link href='#'>Blogs</Link></li>
+                <li className={`mr-9 font-secondaryFont font-semibold ${currentRoute === "" ? "text-[#264FAD] pb-8 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"}`}><Link href='/blog'>Blogs</Link></li>
                 <li className={`mr-9 font-secondaryFont font-semibold ${currentRoute === "" ? "text-[#264FAD] pb-8 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"}`}><Link href='#'>Get Involved</Link></li>
             </nav>
             {openMenu && (
@@ -31,7 +31,7 @@ const Header: FC = () => {
                 <li className={`mr-9 font-secondaryFont font-light ${currentRoute === "" ? "text-[#264FAD] font-medium mb-2 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"} pt-2`}><Link href='#'>Teams</Link></li>
                 <li className={`mr-9 font-secondaryFont font-light ${currentRoute === "" ? "text-[#264FAD] font-medium mb-2 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"} pt-2`}><Link href='#'>Success Stories</Link></li>
                 <li className={`mr-9 font-secondaryFont font-light ${currentRoute === "/" ? "text-[#264FAD] font-medium mb-2 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"} pt-2`}><Link href='#'>About Us</Link></li>
-                <li className={`mr-9 font-secondaryFont font-light ${currentRoute === "" ? "text-[#264FAD] font-medium mb-2 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"} pt-2`}><Link href='#'>Blogs</Link></li>
+                <li className={`mr-9 font-secondaryFont font-light ${currentRoute === "" ? "text-[#264FAD] font-medium mb-2 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"} pt-2`}><Link href='/blog'>Blogs</Link></li>
                 <li className={`mr-9 font-secondaryFont font-light ${currentRoute === "" ? "text-[#264FAD] font-medium mb-2 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"} pt-2`}><Link href='#'>Get Involved</Link></li>
             </nav>
             )}
