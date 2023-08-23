f6d9c4a04d8fd954825041c43aa15c92513256a9
solomon-abate-a2sv <142803643+solomon-abate-a2sv@users.noreply.github.com>
2023-08-23 17:24:06 +0300

    feat(AASTU-web-G2): Setup profile page (#130)
    authored by: solomon-abate-a2sv

---
+++ aastu/web/group-2/app/profile/accountsetting/page.tsx
@@ -0,0 +1,5 @@
+export default function Section() {
+    return <div>
+        <h1>Account Setting</h1>
+    </div>
+}
---
+++ aastu/web/group-2/app/profile/myblogs/page.tsx
@@ -0,0 +1,7 @@
+export default function Section() {
+    return (
+        <div >
+            <h1>My Blogs</h1>
+        </div>
+    )
+}
