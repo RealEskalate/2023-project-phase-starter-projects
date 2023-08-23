9b32f52e639d8c67d09e9b93855bc910b7a597da
Gemechis Elias <141812075+gemechis-elias-a2sv@users.noreply.github.com>
2023-08-23 09:24:42 +0300

    feat(AASTU-Mobile-1): Added Article Model on data layer (#64)

---
+++ aastu/mobile/group-1/blog_app/lib/features/blog/domain/usecases/get_single_article.dart
@@ -0,0 +1,12 @@
+import '../entities/article.dart';
+import '../repositories/article_repository.dart';
+
+class GetArticleUseCase {
+  final ArticleRepository repository;
+
+  GetArticleUseCase(this.repository);
+
+  Future<Article> call(String articleId) async {
+    return await repository.getSingleArticle(articleId);
+  }
+}
