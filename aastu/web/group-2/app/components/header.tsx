5c811b53ae534c32e49500603ec9c5b00d0a678b
Wubshet Zeleke Genta <wubezeleke@gmail.com>
2023-08-23 11:39:34 +0300

    fix(AASTU-web-G2): Create Footer and Fix Header (#90)

--- aastu/web/group-2/app/components/header.tsx
+++ aastu/web/group-2/app/components/header.tsx
@@ -13,7 +13,7 @@ const Header: FC = () => {
     const [openMenu, setOpenMenu] = useState<boolean>(false);
 
   return (
-    <div className="shadow-sm sticky top-0 bg-white flex justify-between w-full h-[90px] py-4 px-9">
+    <div className="shadow-sm sticky z-10 top-0 bg-white flex justify-between text-center w-full h-[90px] py-4 px-9">
             <div className="w-[165px] h-[45px] py-2">
                 <Image width={500} height={500}  src={A2SVLogo} alt="A2SV Logo" />
             </div>
@@ -26,7 +26,7 @@ const Header: FC = () => {
                 <li className={`mr-9 font-secondaryFont font-semibold ${currentRoute === "" ? "text-[#264FAD] pb-8 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"}`}><Link href='#'>Get Involved</Link></li>
             </nav>
             {openMenu && (
-            <nav className={`absolute right-0 px-4 py-2 list-none mt-12 bg-slate-100 lg:hidden`}>
+            <nav className={`absolute right-0 px-4 py-2 list-none mt-12 bg-slate-100 text-left lg:hidden`}>
                 <li className= {`mr-9 font-secondaryFont font-light ${currentRoute === "" ? "text-[#264FAD] font-medium border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"}`}><Link href='#'>Home</Link></li>
                 <li className={`mr-9 font-secondaryFont font-light ${currentRoute === "" ? "text-[#264FAD] font-medium mb-2 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"} pt-2`}><Link href='#'>Teams</Link></li>
                 <li className={`mr-9 font-secondaryFont font-light ${currentRoute === "" ? "text-[#264FAD] font-medium mb-2 border-b-[4px] border-[#264FAD] rounded-sm" : " text-[#565656]"} pt-2`}><Link href='#'>Success Stories</Link></li>
@@ -38,7 +38,7 @@ const Header: FC = () => {
 
             <button className="text-2xl lg:hidden " onClick={() => setOpenMenu(!openMenu)}> <AiOutlineMenu /> </button>
             <div className="hidden lg:block py-1">
-                <button className="pr-8 font-secondaryFont font-bold"> Login</button>
+                <button className="mr-4 font-secondaryFont font-bold"> Login</button>
                 <button className="bg-[#264FAD] text-white font-secondaryFont font-bold rounded-lg px-6 py-3">Donate</button>
             </div>
     </div>
