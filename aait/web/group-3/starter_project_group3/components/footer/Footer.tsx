e77fcbe0aa5cd90648f7757a7eaaf5313038434e
Dawit Andargachew <hereisdawit@gmail.com>
2023-08-23 16:05:59 +0300

    feat(AAiT-web_g3): add footer page (#62)

---
+++ aait/web/group-3/starter_project_group3/components/footer/Footer.tsx
@@ -0,0 +1,122 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <section className="bg-white mx-1  flex justify-center flex-col items-center  py-4 mb-2 mt-5 font-Montserrat">
+      <div className=" w-[90%] p-5  flex justify-around gap-4 flex-col md:flex-row  ">
+        {/* image for desktop */}
+        <div className=" hidden md:block flex w-[20%]  justify-center items-center ">
+          <Image
+            width={450}
+            height={200}
+            src="/assets/footer.svg"
+            alt="My Image"
+          />
+        </div>
+
+        {/* image for mobile */}
+        <div className="  md:hidden flex w-full  justify-center items-center">
+          <Image
+            width={450}
+            height={200}
+            src="/assets/footer.svg"
+            alt="My Image"
+          />
+        </div>
+
+        {/* for text */}
+        <div className="w-full md:w-[25%] font-bold my:8 md:my-0  py-3 flex gap-y-10 justify-center items-center flex-col ">
+          <p className=" m-1">
+            Get involved in improving tech education in Africa!
+          </p>
+
+          <button className="rounded-lg bg-primary  text-white w-3/4   py-2">
+            Support Us
+          </button>
+        </div>
+
+        {/* for links */}
+        <section className="  w-full md:w-[55%] flex  justify-between md:justify-around  items-start  flex-wrap flex-row  py-5 md:py-2 gap-y-10">
+          {/* Links */}
+          <div className=" flex justify-center flex-col font-light gap-y-5">
+            <p className="font-semibold ">Links</p>
+
+            <Link href={" #"}>Homes</Link>
+            <Link href={" #"}>Success Stories</Link>
+            <Link href={" #"}>About Us</Link>
+            <Link href={" #"}>Get Involved</Link>
+          </div>
+          {/* Teams */}
+          <div className=" flex justify-center flex-col font-light gap-y-5">
+            <p className="font-semibold">Teams</p>
+
+            <Link href={"#"}>Broad Members</Link>
+            <Link href={"#"}>Advisor/Mentors</Link>
+            <Link href={"#"}>Executives</Link>
+            <Link href={"#"}>Staffs</Link>
+          </div>
+
+          {/* Blogs */}
+          <div className=" flex justify-center flex-col font-light gap-y-5">
+            <p className=" font-semibold">Blogs</p>
+
+            <Link href={"#"}>Recent Blogs</Link>
+            <Link href={"#"}>New Blogs</Link>
+          </div>
+        </section>
+      </div>
+
+      {/* copyright */}
+      <div className="w-[98%] md:border-t-2 pb-4 md:py-8   mx-auto flex  mt-5 flex-col md:flex-row justify-between gap-y-5  ">
+        <div className="flex justify-start">
+          <Image
+            width={15}
+            height={15}
+            alt="copyright image"
+            src={"/assets/copyright.svg"}
+          />
+          <p className="text-sm md:text-base">
+            2020 Africa to Silicon Valley, Inc. All right reserved.
+          </p>{" "}
+        </div>
+
+        <p className="flex justify-center md:justify-end gap-x-8   ">
+          <Image
+            width={25}
+            height={25}
+            alt="twitter image"
+            src={"/assets/twitter.svg"}
+          />
+          <Image
+            width={25}
+            height={25}
+            alt="facebook image"
+            src={"/assets/facebook.svg"}
+          />
+          <Image
+            width={25}
+            height={25}
+            alt="youtube image"
+            src={"/assets/youtube.svg"}
+          />
+          <Image
+            width={25}
+            height={25}
+            alt="linkedin image"
+            src={"/assets/linkedin.svg"}
+          />
+          <Image
+            width={25}
+            height={25}
+            alt="instagram image"
+            src={"/assets/instagram.svg"}
+          />
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
