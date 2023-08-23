bc1d62c4cfce158a1f41089e34f5a0770bc45706
naol-zebene-a2sv <142803211+naol-zebene-a2sv@users.noreply.github.com>
2023-08-23 15:43:10 +0300

    feat(mobile) : Added Authentication and Register Page (#110)
    Co-authored-by: Naol Zebene <zebenenaol@gmail.com>

---
+++ aastu/mobile/group-3/blog_application_aastu_grp3/lib/features/authentication/presentation/screens.dart
@@ -0,0 +1,29 @@
+import 'package:blog_application_aastu_grp3/features/authentication/presentation/screens/login_screen.dart';
+import 'package:blog_application_aastu_grp3/features/authentication/presentation/screens/sign_up_screen.dart';
+import 'package:flutter/material.dart';
+
+class SigninToggle extends StatefulWidget {
+  const SigninToggle({super.key});
+
+  @override
+  State<SigninToggle> createState() => _SigninToggleState();
+}
+
+class _SigninToggleState extends State<SigninToggle> {
+  @override
+  bool is_login = true;
+
+  void toggleAuth(){
+    setState(() {
+      is_login = !is_login;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    if(is_login){
+      return Login(toggleAuth: toggleAuth);
+    }else{
+      return SignUp(toggleAuth: toggleAuth);
+    }
+  }
+}
