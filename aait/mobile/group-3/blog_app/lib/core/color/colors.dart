bdce28162e382be628e1e6f5407e3bac79c91b35
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-08-23 10:39:39 +0300

    feat(AAiT-mobile-3):  Add files in blog app core directory (#77)

---
+++ aait/mobile/group-3/blog_app/lib/core/color/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const darkBlue = Color.fromARGB(255, 13, 37, 60);
+const darkBlueText = Color.fromARGB(255, 45, 67, 121);
+const darkGrey = Color.fromARGB(255, 123, 139, 178);
+const blue = Color.fromARGB(255, 55, 107, 237);
+const lightGrey = Color.fromARGB(255, 135, 133, 133);
+const fieldBorderColor = Color.fromARGB(68, 123, 139, 178);
+const hintTextColor = Color.fromARGB(255, 151, 158, 165);
+const fieldFillColor = Color.fromARGB(255, 248, 250, 255);
