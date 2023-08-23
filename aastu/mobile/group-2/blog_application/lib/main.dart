810ed20f5ae5a81259cc0fc0120471ef7f1b1bd6
abrham-lakew-a2sv <142803188+abrham-lakew-a2sv@users.noreply.github.com>
2023-08-23 11:40:11 +0300

    Abrham.article reading (#81)
    Co-authored-by: abrhame <ablakew7ab@gamil.com>

--- aastu/mobile/group-2/blog_application/lib/main.dart
+++ aastu/mobile/group-2/blog_application/lib/main.dart
@@ -1,41 +1,13 @@
-import 'package:blog_application/features/blog/presentation/screens/onboardingscreen.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+
+=======
+import 'package:blog_application/features/blog/presentation/pages/articles_reading_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ArticleReading(
+      likes: 0,
+    ),
+  ));
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Onboarding Screen',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Onboarding(),
-    );
-  }
-}
-
+>>>>>>> c9e5312 (Abrham.article reading (#81))
