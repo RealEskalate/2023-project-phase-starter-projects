1301d450cdf9c0c107fc7f354222c66f52b56d61
ffekirnew <fikernew.birhanu@a2sv.org>
2023-08-23 11:21:13 +0300

    feat(aait.bac.2b.authentication): Add general user dto

---
+++ aait/backend/group-2B/Application/DTOs/Authentication/UserDto.cs
@@ -0,0 +1,15 @@
+using SocialSync.Application.DTOs.Common;
+
+namespace SocialSync.Domain.Entities;
+
+public class UserDto : BaseDto
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Bio { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+}
+
