9e6f666b9f06fb18995add66b50918070fa9bc47
solomon-abate-a2sv <142803643+solomon-abate-a2sv@users.noreply.github.com>
2023-08-23 15:05:28 +0300

    feat(AASTU-web-G2): succsess rate section (#105)
    authored by: solomon-abate-a2sv

--- aastu/web/group-2/app/page.tsx
+++ aastu/web/group-2/app/page.tsx
@@ -4,12 +4,14 @@ import LandingSection from "./components/home/LandingSection"
 import BuildBetterSection from "./components/home/BuildBetterSection"
 import HelpUs from "./components/home/HelpUs"
 import ImpactStoriesSection from "./components/home/ImpactStoriesSection"
+import SuccsessRateSection from "./components/home/SuccsessRateSection"
 
 export default function IndexPage() {
   return (
   <>
     <LandingSection />
     <BuildBetterSection />
+    <SuccsessRateSection />
     <HelpUs />
     <ImpactStoriesSection />
   </>
