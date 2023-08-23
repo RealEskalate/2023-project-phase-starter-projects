13af24542f88cebcffdb5e33be2fa5a72fe3c9c0
unknown <pauldessie@gmail.com>
2023-08-23 11:47:24 +0300

    added highlighted routes

--- aait/web/group-1/components/LinkItems.tsx
+++ aait/web/group-1/components/LinkItems.tsx
@@ -1,4 +1,6 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
 import React from "react";
 
 interface Props {
@@ -7,15 +9,18 @@ interface Props {
 }
 
 export const LinkItems: React.FC<Props> = ({ name, link }) => {
+  const pathname = usePathname();
+  const isCurrentRoute = pathname === link;
+
   return (
-    <div className="group hover:cursor-pointer font-semibold hover:font-bold">
+    <div className={`group ${isCurrentRoute ? 'cursor-pointer font-bold' : 'hover:cursor-pointer font-semibold'}`}>
       <Link
-        className="text-sm capitalize group-hover:text-blue-800"
+        className={`text-sm capitalize ${isCurrentRoute ? 'text-blue-800' : 'group-hover:text-blue-800'}`}
         href={link}
       >
         {name}
       </Link>
-      <div className="h-1 mt-1 mx-0.5 rounded-full bg-blue-800 opacity-0 group-hover:opacity-100 transition-opacity"></div>
+      <div className={`h-1 mt-1 mx-0.5 rounded-full bg-blue-800 opacity-0 ${isCurrentRoute ? 'opacity-100' : 'group-hover:opacity-100'} transition-opacity`}></div>
     </div>
   );
 };
