9af63eae93041a3aee62db7ad8c39b90924d60df
ffekirnew <fikernew.birhanu@a2sv.org>
2023-08-23 11:18:19 +0300

    feat(aait.bac.2b.authentication): Add user interface as a contract

--- aait/backend/group-2B/Application/Contracts/Persistence/IUserRepository.cs
+++ aait/backend/group-2B/Application/Contracts/Persistence/IUserRepository.cs
@@ -2,4 +2,13 @@ using SocialSync.Domain.Entities;
 
 namespace SocialSync.Application.Contracts.Persistence;
 
+<<<<<<< HEAD
 public interface IUserRepository : IGenericRepository<User> { }
+=======
+public interface IUserRepository : IGenericRepository<User>
+{
+    public Task<bool> UsernameExists(string username);
+    public Task<bool> EmailExists(string email);
+    public Task<User> GetByUsername(string username);
+}
+>>>>>>> d91c235 (feat(aait.bac.2b.authentication): Add user interface as a contract)
