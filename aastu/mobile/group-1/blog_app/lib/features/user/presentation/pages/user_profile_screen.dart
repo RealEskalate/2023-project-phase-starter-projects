120a1b278dea2e58944e7c4ee9fdf5533b018fc1
mehari-tesfaye-a2sv <142803548+mehari-tesfaye-a2sv@users.noreply.github.com>
2023-08-23 14:33:48 +0300

    Aastu.mob.g1.mehari.profile page (#98)
    Co-authored-by: Mehari Tesfaye <meharithegreat@gmail.com>

---
+++ aastu/mobile/group-1/blog_app/lib/features/user/presentation/pages/user_profile_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../widgets/about_me.dart';
+import '../widgets/my_post.dart';
+import '../widgets/header.dart';
+
+class UserProfileScreen extends StatelessWidget {
+  const UserProfileScreen({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromARGB(255, 255, 255, 255),
+      child: MaterialApp(
+        title: 'Profile',
+        home: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+          appBar: header(),
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Aboutme(),
+              ),
+              const Expanded(child: Myposts()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
