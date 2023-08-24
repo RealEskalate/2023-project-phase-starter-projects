049b33cbaf298959aba0549d9b7b24e7d4aa04ba
Yohannes Teffera <yohannesahunm7@gmail.com>
2023-08-24 22:58:58 +0300

    feat(aait.web.g3):Add create blog page

--- aait/web/group-3/starter_project_group3/components/blog/AddBlog.tsx
+++ aait/web/group-3/starter_project_group3/components/blog/AddBlog.tsx
@@ -36,18 +36,29 @@ const AddBlog:React.FC<Props> = ({
     handleTitleChange
 }) => {
   return (
+<<<<<<< HEAD
    <form className='pl-16 pr-16 pt-16'>
     <div className="flex flex-col md:grid grid-cols-4 md:pl-16 md:pr-16 pt-16 h-screen mb-48">
       <div className=" flex flex-col gap-8 md:col-span-3 rounded-lg  max-w-screen-lg w-full">
+=======
+   <form className='p-16'>
+    <div className="flex flex-col md:flex-row p-16 h-screen mb-48">
+      <div className=" flex flex-col gap-8 rounded-lg  max-w-screen-lg w-full">
+>>>>>>> 71ad5a6 (feat(aait.web.g3):Add create blog page)
         <div className="bg-white border-l-4 border-primary p-4">
             <input
                 type="text"
                 placeholder="Enter the blog title"
+<<<<<<< HEAD
                 className="border-none rounded-full text-sm md:text-3xl focus:outline-none"
+=======
+                className="border-none rounded-full text-3xl focus:outline-none"
+>>>>>>> 71ad5a6 (feat(aait.web.g3):Add create blog page)
                 value={title}
                 onChange={handleTitleChange}
             />
         </div>
+<<<<<<< HEAD
         <div className="md:mr-20 pb-16 bg-bg_blog_upload relative">
             <img
                 src={image ? URL.createObjectURL(image) : "../assets/upload_image.png"}
@@ -55,6 +66,15 @@ const AddBlog:React.FC<Props> = ({
                 className="mx-auto mt-24 mb-8 object-center object-cover"
             />
             <div className="flex md:justify-center items-center">
+=======
+        <div className="mr-20 pb-16 bg-bg_blog_upload relative">
+            <img
+                src={image ? URL.createObjectURL(image) : "../assets/upload_image.svg"}
+                alt="Blog Image"
+                className="mx-auto mt-24 mb-8 object-center object-cover"
+            />
+            <div className="flex justify-center items-center">
+>>>>>>> 71ad5a6 (feat(aait.web.g3):Add create blog page)
                 <label className="cursor-pointer">
                     <input
                         type="file"
@@ -81,6 +101,7 @@ const AddBlog:React.FC<Props> = ({
             </div>
         </div>
 
+<<<<<<< HEAD
         <div className="border-l-4  border-primary pl-4 md:mr-20 ">
             <ReactQuill value={content} onChange={handleContentChange} className="h-48 border-none"/>       
         </div>
@@ -88,13 +109,26 @@ const AddBlog:React.FC<Props> = ({
         <div className="flex gap-8 justify-end my-16 md:my-8 md:mr-20" >
             <button
                 className="mt-4 bg-gray-300 px-2 md:px-4 md:py-2 text-primary rounded hover:bg-primary hover:text-white"
+=======
+        <div className="border-l-4  border-primary pl-4 mr-20 ">
+            <ReactQuill value={content} onChange={handleContentChange} className="h-48 border-none"/>       
+        </div>
+    
+        <div className="flex gap-8 justify-end my-8 mr-20" >
+            <button
+                className="mt-4 bg-gray-300 px-4 py-2 text-blue-500 rounded hover:bg-blue-600 hover:text-white"
+>>>>>>> 71ad5a6 (feat(aait.web.g3):Add create blog page)
                 onClick={handleCancel}
             >
             Cancel
             </button>
 
             <button
+<<<<<<< HEAD
                 className="mt-4 bg-primary text-white px-4 py-2 rounded"
+=======
+                className="mt-4 bg-primary text-white px-4 py-2 rounded hover:bg-blue-600"
+>>>>>>> 71ad5a6 (feat(aait.web.g3):Add create blog page)
                 onClick={handleSubmit}
             >
                 Save changes
@@ -103,7 +137,11 @@ const AddBlog:React.FC<Props> = ({
         </div>
       </div>
 
+<<<<<<< HEAD
       <div className="flex flex-col  col-span-1 gap-4">
+=======
+      <div className="flex flex-col gap-4">
+>>>>>>> 71ad5a6 (feat(aait.web.g3):Add create blog page)
         <h1 className='font-bold'>Select Tag</h1>
         <div className="flex flex-wrap gap-3">
             {tags.map((tag) => (
@@ -114,7 +152,11 @@ const AddBlog:React.FC<Props> = ({
                 onClick={handleTagClick}
                 />
             ))}
+<<<<<<< HEAD
         </div>
+=======
+            </div>
+>>>>>>> 71ad5a6 (feat(aait.web.g3):Add create blog page)
       </div>
     </div>
   </form>
