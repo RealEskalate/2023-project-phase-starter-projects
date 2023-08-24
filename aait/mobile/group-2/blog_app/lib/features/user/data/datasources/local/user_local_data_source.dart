3033aba65428c88560ba827f158d15160a12f0bf
Nathan Zelalem <95658930+NathanZK@users.noreply.github.com>
2023-08-24 09:38:09 +0300

    feat(AAiT-mobile-2): Add User Local data source (#140)

---
+++ aait/mobile/group-2/blog_app/lib/features/user/data/datasources/local/user_local_data_source.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import '../../../../article/data/models/article_model.dart';
+
+abstract class UserLocalDataSource {
+  Future<void> cacheBookmarkedArticles(List<ArticleModel> articlesToCache);
+  Future<List<ArticleModel>> getAllBookmarkedArticles();
+  Future<void> removeBookmark(String userArticleId);
+
+  Future<void> cacheToken(String token);
+  Future<String> getToken();
+  Future<void> clearCache();
+}
