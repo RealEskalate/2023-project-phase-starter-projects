dc49e00741a247c6b290cd1e6d5fd34b3d312de5
Son-OfAnton <admasterefe00@gmail.com>
2023-08-24 11:46:39 +0300

    fix(aait.web.g1): Removed whitespace in home

---
+++ aait/web/group-1/app/signup/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignUpForm from '@/components/signup/SignUpForm'
+
+
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  )
+}
+
+export default SignUpPage
