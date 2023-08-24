3b1ee456ae3ac7fcc1b0dd31e258746dce1f5f78
Dawit Melka <davebadeg@gmail.com>
2023-08-24 14:40:36 +0300

    fix(AAiT-mobile-3): Update PostText Custom Widget

--- aait/mobile/group-3/blog_app/lib/features/article/presentation/widgets/post_text.dart
+++ aait/mobile/group-3/blog_app/lib/features/article/presentation/widgets/post_text.dart
@@ -9,16 +9,17 @@ class PostText extends StatelessWidget {
   const PostText({
     super.key,
   });
-
+  final String articleContent =
+      '''That marked a turnaround from last year, when the social network reported a decline in users for the first time. 
+        \nThe drop wiped billions from the firm's market value.
+        \nSince executives disclosed the fall in February, the firm's share price has nearly halved. But shares jumped 19% in after-hours trade on Wednesday.
+''';
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 23.w),
       child: Text(
-        '''That marked a turnaround from last year, when the social network reported a decline in users for the first time. 
-        \nThe drop wiped billions from the firm's market value.
-        \nSince executives disclosed the fall in February, the firm's share price has nearly halved. But shares jumped 19% in after-hours trade on Wednesday.
-''',
+        articleContent,
         style: GoogleFonts.poppins(
           fontSize: 16.sp,
           fontWeight: FontWeight.w400,
