e6d7e1302fd95864f58a741fc67d1b4b8a1cc6ce
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-08-24 14:08:09 +0300

    feat(AAiT-mobile-2): Add Article Bloc (#160)

---
+++ aait/mobile/group-2/blog_app/lib/features/article/presentation/bloc/article_event.dart
@@ -0,0 +1,48 @@
+import 'package:equatable/equatable.dart';
+
+import '../../domain/entities/article.dart';
+
+sealed class ArticleEvent extends Equatable {
+  const ArticleEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class LoadAllArticlesEvent extends ArticleEvent {}
+
+final class GetSingleArticleEvent extends ArticleEvent {
+  final String id;
+
+  const GetSingleArticleEvent(this.id);
+
+  @override
+  List<Object> get props => [id];
+}
+
+final class CreateArticleEvent extends ArticleEvent {
+  final Article article;
+
+  const CreateArticleEvent(this.article);
+
+  @override
+  List<Object?> get props => [article];
+}
+
+final class UpdateArticleEvent extends ArticleEvent {
+  final Article article;
+
+  const UpdateArticleEvent(this.article);
+
+  @override
+  List<Object?> get props => [article];
+}
+
+final class DeleteArticleEvent extends ArticleEvent {
+  final String id;
+
+  const DeleteArticleEvent(this.id);
+
+  @override
+  List<Object> get props => [id];
+}
