7df28873ae32e1d75667a62699f2dc7f47cf4c79
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-08-24 17:01:52 +0300

    feat(AAiT-backend-1B) : add post, like comment and notification features (#186)

---
+++ aait/backend/group-1B/Domain/Entities/PostTag.cs
@@ -0,0 +1,10 @@
+namespace Domain.Entities;
+
+public class PostTag
+{
+    public int TagId { get; set; }
+    public int PostId { get; set; }
+
+    public virtual Post Post { get; set; }
+    public virtual Tag Tag { get; set; }
+}
--- aait/backend/group-1B/Persistence/PersistenceServiceRegistration.cs
+++ aait/backend/group-1B/Persistence/PersistenceServiceRegistration.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Contracts.Persistence;
+using Persistence.Repositories;
 
 namespace Persistence
 {
@@ -23,7 +25,12 @@ namespace Persistence
            
 
             services.AddScoped<IAuthService, AuthService>();
-
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IPostRepository, PostRepository>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPostLikesRepository, PostLikesRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             return services;
         }
