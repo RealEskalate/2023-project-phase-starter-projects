4ae0b6410fea7889b49c4c44a16003dd67bdfb46
Yoseph Shemeles <142782245+yoseph-shemeles-a2sv@users.noreply.github.com>
2023-08-24 11:34:55 +0300

    feat(aastu.web.g1):impact stories images added (#143)
    Co-authored-by: Yoseph Shemeles <jossyfresh2002@gmail.com>

---
+++ aastu/web/group-1/a2sv-blogs/src/app/stories/page.tsx
@@ -0,0 +1,20 @@
+import InterviewPartners from "@/components/stories/InterviewPartners";
+import SuccessStoriesHeader from "@/components/stories/SuccessStoriesHeader";
+import SuccessStoryCard from "@/components/stories/SuccessStoryCard";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex flex-col mt-36 gap-14 w-[90%] mx-auto">
+      <div className="">
+        <SuccessStoriesHeader />
+      </div>
+      <div className="flex flex-col gap-28 mt-20">
+        <SuccessStoryCard />
+      </div>
+      <div>
+        <InterviewPartners />
+      </div>
+    </div>
+  );
+}
