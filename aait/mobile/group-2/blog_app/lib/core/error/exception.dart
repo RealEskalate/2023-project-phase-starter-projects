5fb1965b23bd32aec6dc042ed4ededdc65842c89
Temesgen Zewude <90067770+Temesgenzewude@users.noreply.github.com>
2023-08-24 14:11:31 +0300

    feat(AAiT-mobile-2): implement data layer for authentication feature (#165)

--- aait/mobile/group-2/blog_app/lib/core/error/exception.dart
+++ aait/mobile/group-2/blog_app/lib/core/error/exception.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
 class CacheException extends Equatable implements Exception {
@@ -25,4 +26,33 @@ class NetworkException extends Equatable implements Exception {
 
   @override
   List<Object> get props => [message];
+}
+
+class LoginException extends Equatable implements Exception {
+  final String message;
+  const LoginException({
+    required this.message,
+  });
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class SignUpException extends Equatable implements Exception {
+  final String message;
+  const SignUpException({
+    required this.message,
+  });
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class LogoutException extends Equatable implements Exception {
+  final String message;
+  const LogoutException({
+    required this.message,
+  });
+  @override
+  List<Object?> get props => [message];
 }
---
+++ aait/mobile/group-2/blog_app/lib/features/authentication/data/data_sources/local_data_source.dart
@@ -0,0 +1,28 @@
+import '../models/authenticated_user_info_model.dart';
+
+abstract class AuthLocalDataSource {
+  /// Caches a [token] in the local data source
+  ///
+  /// Throws a [CacheException] for cache errors
+  Future<void> cacheToken(String token);
+
+  /// Gets the cached token from the local data source
+  ///
+  /// Throws a [CacheException] for cache errors
+  Future<String> getToken();
+
+  /// Caches a [AuthenticatedUserInfoModel] in the local data source
+  ///
+  /// Throws a [CacheException] for cache errors
+  Future<void> cacheLoggedInUser(AuthenticatedUserInfoModel userToCache);
+
+  /// Gets the cached user from the local data source
+  ///
+  /// Throws a [CacheException] for cache errors
+  Future<AuthenticatedUserInfoModel> getLoggedInUser();
+
+  /// Deletes the cached user  and it's data from the local data source
+  ///
+  /// Throws a [CacheException] for cache errors
+  Future<void> deleteLoggedInUser();
+}
