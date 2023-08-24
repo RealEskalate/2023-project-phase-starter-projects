f3b14bf62982cf8c3619632566644dc0a6260a9f
Metsehafe Eyasu Tesfu <118991565+Metsehafe-Eyasu@users.noreply.github.com>
2023-08-24 16:13:08 +0300

    feat(aastu-web-g1): Created the basic layout of add blog page (#184)

---
+++ aastu/web/group-1/a2sv-blogs/src/components/blogs/TagsSelector.tsx
@@ -0,0 +1,58 @@
+type Props = {
+  selectedTags: string[];
+  setSelectedTags: (tags: string[]) => void;
+};
+
+const TagSelectionForm: React.FC<Props> = ({
+  selectedTags,
+  setSelectedTags,
+}) => {
+  const availableTags = [
+    "Development",
+    "Sports",
+    "Writing",
+    "Self Improvement",
+    "Technology",
+    "Social",
+    "Data Science",
+    "Programming",
+  ];
+
+  const handleTagChange = (tag: string) => {
+    if (selectedTags.includes(tag)) {
+      setSelectedTags(
+        selectedTags.filter((selectedTag) => selectedTag !== tag)
+      );
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h3 className="font-bold">Select Tag</h3>
+      <div className="flex flex-wrap gap-2">
+        {availableTags.map((tag) => (
+          <label
+            key={tag}
+            className={`font-montserrat text-sm flex items-center rounded-full border px-4 ${
+              selectedTags.includes(tag)
+                ? "border-blue-500 bg-blue-100 font-bold"
+                : "border-gray-300 bg-gray-200"
+            }`}
+          >
+            <input
+              type="checkbox"
+              checked={selectedTags.includes(tag)}
+              onChange={() => handleTagChange(tag)}
+              className="sr-only"
+            />
+            <span>{tag}</span>
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TagSelectionForm;
