53795336d5665147de8191180bddef3ecd7b698f
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-08-24 10:40:55 +0300

    feat(Aait.mob.g2): add user remote data source (#134)

---
+++ aait/mobile/group-2/blog_app/lib/features/user/data/datasources/user_remote_data_source_impl.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+import '../../../../core/error/exception.dart';
+import '../../domain/entities/user_data.dart';
+import '../models/user_data_model.dart';
+import 'user_remote_data_source.dart';
+
+class UserRemoteDataSourceImpl implements UserRemoteDataSource {
+  final http.Client client;
+
+  UserRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<UserData> getUserData(String token) async {
+    final http.Response response = await client
+        .get(Uri.parse('http://localhost:4500/api/v1/user'), headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    });
+
+    if (response.statusCode == 200) {
+      return UserDataModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw const ServerException();
+    }
+  }
+
+  @override
+  Future<UserData> updateUserPhoto(String token, String imagePath) async {
+    final Map<String, String> headers = {
+      'Authorization': 'Bearer $token',
+    };
+
+    final request = http.MultipartRequest('PUT', Uri.parse(imagePath));
+    request.headers.addAll(headers);
+
+    final imageBytes = await File(imagePath).readAsBytes();
+
+    request.files.add(
+      http.MultipartFile.fromBytes(
+        'photo',
+        imageBytes,
+        filename: 'profile_image',
+      ),
+    );
+
+    try {
+      final response = await request.send();
+
+      if (response.statusCode == 200) {
+        final responseString = await response.stream.bytesToString();
+        return UserDataModel.fromJson(json.decode(responseString));
+      } else {
+        throw const ServerException();
+      }
+    } catch (e) {
+      throw const ServerException();
+    }
+  }
+}
