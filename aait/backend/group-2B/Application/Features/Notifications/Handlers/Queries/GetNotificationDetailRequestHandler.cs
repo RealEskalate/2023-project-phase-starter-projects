dce5dbf2e1791a022f8a720a73e1d2d59347a46a
petros beyene <petrosbeyene611@gmail.com>
2023-08-24 02:50:29 -0400

    Implement GetNotificationDetailRequestHandler

--- aait/backend/group-2B/Application/Features/Notifications/Handlers/Queries/GetNotificationDetailRequestHandler.cs
+++ aait/backend/group-2B/Application/Features/Notifications/Handlers/Queries/GetNotificationDetailRequestHandler.cs
@@ -1,5 +1,28 @@
-namespace Application.Features.Notifications.Handlers.Queries;
+using AutoMapper;
+using MediatR;
+using SocialSync.Application.Contracts.Persistence;
+using SocialSync.Application.DTOs.Notifications;
+using SocialSync.Application.Features.Notifications.Requests.Queries;
 
-public class GetNotificationDetailRequestHandler
+
+namespace SocialSync.Application.Features.Notifications.Handlers.Queries;
+
+public class GetNotificationDetailRequestHandler : IRequestHandler<GetNotificationDetailRequest, NotificationDto>
 {
+    private readonly INotificationRepository _notificationRepository;
+    private readonly IMapper _mapper;
+
+    public GetNotificationDetailRequestHandler(INotificationRepository notificationRepository, IMapper mapper){
+        _notificationRepository = notificationRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<NotificationDto> Handle(
+        GetNotificationDetailRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        var notification = await _notificationRepository.GetAsync(request.Id);
+        return _mapper.Map<NotificationDto>(notification);
+    }
 }
