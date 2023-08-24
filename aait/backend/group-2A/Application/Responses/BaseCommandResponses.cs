3a6985560e6bfe6c2cc34f93fa0a71231d334481
Dagmawi Muluwork <dagmawimuluwork@gmail.com>
2023-08-24 11:32:20 +0300

    feat(AAiT-backend-g2A): Exceptions,Response and followController

---
+++ aait/backend/group-2A/Application/Responses/BaseCommandResponses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Responses
+{
+    public class BaseCommandResponse
+    {
+        public int Id { get; set; }
+        public bool Success { get; set; } = true;
+
+        public string Message { get; set; }
+
+        public List<string> Errors { get; set; }
+    }
+}
