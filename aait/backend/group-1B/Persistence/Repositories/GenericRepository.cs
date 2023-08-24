b31423c88033a1eb870ea23e72d62bcee86d48ea
Ananiya <ananiyat24@gmail.com>
2023-08-24 09:52:18 +0300

    implement repositories

--- aait/backend/group-1B/Persistence/Repositories/GenericRepository.cs
+++ aait/backend/group-1B/Persistence/Repositories/GenericRepository.cs
@@ -30,6 +30,7 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
     public async Task<T> Add(T item)
     {
         await _context.Set<T>().AddAsync(item);
+        await _context.SaveChangesAsync();
         return item;
     }
 
