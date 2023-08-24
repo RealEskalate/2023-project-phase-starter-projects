39c88cb1c32d3767e87c990efe5a8d3e64b2bb9b
Nathan Kebede <nathanzelalem100@gmail.com>
2023-08-24 11:37:53 +0300

    Add user repositoy implementation

---
+++ aait/mobile/group-2/blog_app/lib/features/user/data/repositories/user_repository_impl.dart
@@ -0,0 +1,64 @@
+import '../../../../core/error/exception.dart';
+import '../../../../core/error/failure.dart';
+import '../../../../core/network/network_info.dart';
+import '../../domain/entities/user_data.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../domain/repositories/user_repository.dart';
+import '../datasources/local/user_local_data_source.dart';
+import '../datasources/user_remote_data_source.dart';
+import '../models/user_data_model.dart';
+
+class UserRespositoryImpl extends UserRepository {
+  final UserLocalDataSource localDataSource;
+  final UserRemoteDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+
+  UserRespositoryImpl({
+    required this.localDataSource,
+    required this.remoteDataSource,
+    required this.networkInfo,
+  });
+  @override
+  Future<Either<Failure, UserData>> getUserData(String token) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final user = await remoteDataSource.getUserData(token);
+        await localDataSource.cacheUserData(user as UserDataModel);
+        return Right(user);
+      } catch (e) {
+        try {
+          final user = await localDataSource.getUserData();
+          return Right(user);
+        } catch (e) {
+          return Left(CacheFailure());
+        }
+      }
+    } else {
+      try {
+        final article = await localDataSource.getUserData();
+        return Right(article);
+      } catch (e) {
+        return Left(CacheFailure());
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserData>> updateUserPhoto(
+      String token, String imagePath) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final uploadedPhoto =
+            await remoteDataSource.updateUserPhoto(token, imagePath);
+        return Right(uploadedPhoto);
+      } on ServerException {
+        return Left(ServerFailure());
+      } on NetworkException {
+        return Left(NetworkFailure());
+      }
+    } else {
+      return Left(NetworkFailure());
+    }
+  }
+}
