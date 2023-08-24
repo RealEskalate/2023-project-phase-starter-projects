e54b7a7f109c2378ecf94bd567c8f43f835e49a9
Ananiya <ananiyat24@gmail.com>
2023-08-24 11:33:47 +0300

    add hashtag functionality

--- aait/backend/group-1B/Application/Features/Posts/Handlers/Commands/UpdatePostRequestHandler.cs
+++ aait/backend/group-1B/Application/Features/Posts/Handlers/Commands/UpdatePostRequestHandler.cs
@@ -9,26 +9,42 @@ namespace Application.Features.Posts.Handlers.Commands;
 
 public class UpdatePostRequestHandler : IRequestHandler<UpdatePostRequest, Post>
 {
-    private readonly IPostRepository _postRepository;
     private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public UpdatePostRequestHandler(IPostRepository postRepository, IMapper mapper)
+    public UpdatePostRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
     {
-        _postRepository = postRepository;
+        _unitOfWork = unitOfWork;
         _mapper = mapper;
     }
 
     public async Task<Post> Handle(UpdatePostRequest request, CancellationToken token)
     {
-        var validator = new UpdatePostDtoValidator(_postRepository);
+        var validator = new UpdatePostDtoValidator(_unitOfWork.PostRepository);
         var validationResult = await validator.ValidateAsync(request.Post, token);
 
         if (validationResult.IsValid == false)
             throw new ValidationException(validationResult);
-        
-        Post postToUpdate = _mapper.Map<Post>(request.Post);
-        var post = await _postRepository.Update(postToUpdate);
 
+        var oldContent = (await _unitOfWork.PostRepository.Get(request.Post.Id)).Content;
+        var postToUpdate = _mapper.Map<Post>(request.Post);
+        var post = await _unitOfWork.PostRepository.Update(postToUpdate);
+        
+        var oldTags = PostTagHelpers.GetTags(oldContent);
+        var newTags = PostTagHelpers.GetTags(postToUpdate.Content);
+        var differences = PostTagHelpers.GetTagDifferences(oldTags, newTags);
+
+        foreach (var addedTag in differences.Item1)
+        {
+            await _unitOfWork.PostTagRepository.AddTagToPost(post.Id, addedTag);
+        }
+
+        foreach (var removedTag in differences.Item2)
+        {
+            await _unitOfWork.PostTagRepository.DeletePostTag(post.Id, removedTag);
+        }
+        
+        await _unitOfWork.Save();
         return post;
     }
 }
