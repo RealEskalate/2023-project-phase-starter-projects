83accdd14eb6304da38b027976e4bf68e8df6c95
mintesnot-markos <mintesnot.markos@a2sv.org>
2023-08-24 22:18:30 +0300

    Fix(aastu-web-g2): fix bugs addBlog

--- aastu/web/group-2/app/page.tsx
+++ aastu/web/group-2/app/page.tsx
@@ -1,25 +1,25 @@
-"use client"
+'use client';
 
 /* Components */
 
-import LandingSection from "./components/home/LandingSection"
-import BuildBetterSection from "./components/home/BuildBetterSection"
-import HelpUs from "./components/home/HelpUs"
-import ImpactStoriesSection from "./components/home/ImpactStoriesSection"
-import SuccsessRateSection from "./components/home/SuccsessRateSection"
+import LandingSection from './components/home/LandingSection';
+import BuildBetterSection from './components/home/BuildBetterSection';
+import HelpUs from './components/home/HelpUs';
+import ImpactStoriesSection from './components/home/ImpactStoriesSection';
+import SuccsessRateSection from './components/home/SuccsessRateSection';
 
 export default function IndexPage() {
   return (
-  <>
-    <LandingSection />
-    <BuildBetterSection />
-    <SuccsessRateSection />
-    <HelpUs />
-    <ImpactStoriesSection />
-  </>
-  )
+    <>
+      <LandingSection />
+      <BuildBetterSection />
+      <SuccsessRateSection />
+      <HelpUs />
+      <ImpactStoriesSection />
+    </>
+  );
 }
 
-export const metadata = {
-  title: 'Redux Toolkit',
-}
+// export const metadata = {
+//   title: 'Redux Toolkit',
+// }
