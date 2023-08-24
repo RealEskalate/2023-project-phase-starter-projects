42d78a5eedf820493cdb20eca51bdf339efd101f
Yoseph Shemeles <142782245+yoseph-shemeles-a2sv@users.noreply.github.com>
2023-08-24 11:34:55 +0300

    feat(aastu.web.g1):impact stories images added (#143)
    Co-authored-by: Yoseph Shemeles <jossyfresh2002@gmail.com>

--- aastu/web/group-1/a2sv-blogs/src/components/stories/SuccessStoriesHeader.tsx
+++ aastu/web/group-1/a2sv-blogs/src/components/stories/SuccessStoriesHeader.tsx
@@ -1,7 +1,7 @@
 export default function SuccessStoriesHeader() {
   return (
     <div className="w-[80%] flex flex-col items-center mx-auto">
-      <h1 className="text-5xl text-text-header-1 font-semibold text-center font-poppins">
+      <h1 className="text-[44px] text-text-header-1 font-semibold text-center font-poppins">
         Impact Stories
       </h1>
       <p className="text-3xl text-center text-text-content font-poppins font-normal mt-5">
