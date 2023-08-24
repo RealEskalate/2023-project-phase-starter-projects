1eec42f38f22bcad32e85f3bf5493bf420763ad4
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-08-24 16:45:20 +0300

    feat(AAiT-mobile-3): Add Article Reading and Writing Page (#172)

---
+++ aait/mobile/group-3/blog_app/lib/features/article/presentation/widgets/head_title.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HeadTitle extends StatelessWidget {
+  String headTitle;
+  HeadTitle({
+    super.key,
+    required this.headTitle,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 40.w),
+      child: Text(
+        headTitle,
+        style: GoogleFonts.poppins(
+          fontWeight: FontWeight.w600,
+          fontSize: 24.sp,
+        ),
+      ),
+    );
+  }
+}
