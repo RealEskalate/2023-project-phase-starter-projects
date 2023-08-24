bb0b5dc44ee7d4d87f7890de23528272d151194e
nefelibatala1993 <leuela1993@gmail.com>
2023-08-24 10:22:14 +0300

    made the neccessary changes

--- aait/web/group-1/.next/static/chunks/webpack.js
+++ aait/web/group-1/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bc591c897a7a203f"; }
+/******/ 		__webpack_require__.h = function() { return "5c274f786d6fd3ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
