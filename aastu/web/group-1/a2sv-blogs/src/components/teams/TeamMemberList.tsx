ed445f274ccdff27a66e70f7e9d7f41702a176e4
ruth-wossen-a2sv <142803496+ruth-wossen-a2sv@users.noreply.github.com>
2023-08-24 15:08:17 +0300

    aastu.web.g1.wossen-ruth.team-page (#173)
    Co-authored-by: Ruth Wossen <ruthwossen75@gmail.com>

---
+++ aastu/web/group-1/a2sv-blogs/src/components/teams/TeamMemberList.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import TeamMemberCard from "./TeamMemberCard"; 
+import { useState, useEffect } from "react";
+import { TeamMember } from "@/types/TeamMember";
+import PaginationButtons from "./PaginationButton";
+
+export async function fetchMembers(){
+  const res = await fetch("https://a2sv-backend.onrender.com/api/members");
+  return await res.json();
+}
+
+export default function TeamMemberList() {
+  const [members, setMembers] = useState<TeamMember[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 6;
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const data = await fetchMembers();
+      setMembers(data);
+    }
+    fetchData();
+  }, [])
+  
+  const startIndex = (currentPage-1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const currentMembers = members.slice(startIndex, endIndex);
+
+
+
+  return (
+    <div>
+    <div className="grid grid-cols-3 gap-10">
+      {
+        currentMembers.map((member) => (
+          <TeamMemberCard key= {member.id} member={member} />
+        ))
+      }
+    </div>
+    <PaginationButtons 
+        currentPage = {currentPage}
+        totalPages = {Math.ceil(members.length / pageSize)}
+        setCurrentPage = {setCurrentPage}
+      />
+    </div>
+
+
+  );
+}
