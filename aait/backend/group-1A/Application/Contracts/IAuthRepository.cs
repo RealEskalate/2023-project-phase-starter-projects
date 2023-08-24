4a6343b5c6014a108f801636c6c27661151796dd
Amanuel Yihune <105606795+amanyih@users.noreply.github.com>
2023-08-24 15:27:44 +0300

    feat(AAiT-backend-1A): authentication (#91)

---
+++ aait/backend/group-1A/Application/Contracts/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using SocialSync.Domain.Entities;
+
+namespace SocialSync.Application.Contracts;
+
+public interface IAuthRepository{
+
+    Task<User> RegisterUser(User user);
+
+    Task<User> LoginUser(User user);
+
+    Task<bool> UserExists(string username);
+}
