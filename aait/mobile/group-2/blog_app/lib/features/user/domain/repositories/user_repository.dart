d58ce72f35aff754fd6e93c089cc4a1fa19a52c7
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-08-24 10:40:55 +0300

    feat(Aait.mob.g2): add user remote data source (#134)

--- aait/mobile/group-2/blog_app/lib/features/user/domain/repositories/user_repository.dart
+++ aait/mobile/group-2/blog_app/lib/features/user/domain/repositories/user_repository.dart
@@ -4,6 +4,6 @@ import '../../../../core/error/failure.dart';
 import '../entities/user_data.dart';
 
 abstract class UserRepository {
-  Future<Either<Failure, UserData>> getUserData(String userId);
-  Future<Either<Failure, UserData>> updateUserPhoto(String userId, String imageUrl);
+  Future<Either<Failure, UserData>> getUserData(String token);
+  Future<Either<Failure, UserData>> updateUserPhoto(String token, String imagePath);
 }
