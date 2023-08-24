90c2c456462e8ea5d0e43b73cbbf8d0b19f1fed2
naolt <naoltamrat11@gmai.com>
2023-08-24 15:43:12 +0300

    feat(aastu-web-g2): Use Api Data

--- aastu/web/group-2/app/blog/[id]/page.tsx
+++ aastu/web/group-2/app/blog/[id]/page.tsx
@@ -36,38 +36,13 @@ const First = () => {
   const { data: blogs } = useGetBlogsQuery();
   const { data: blogInfo } = useGetBlogByIdQuery(id);
 
-  //useEffect(() => {
-  //  fetch('https://a2sv-backend.onrender.com/api/blogs/')
-  //    .then((response) => response.json())
-  //    .then((data) => {
-  //      const filteredBlogs = data.filter((blog: any) => blog.id !== id);
-  //      setBlogs(filteredBlogs);
-  //    });
-  //}, []);
-  //console.log(blogs);
-
-  //useEffect(() => {
-  //  fetch(`https://a2sv-backend.onrender.com/api/blogs/${id}`)
-  //    .then((res) => {
-  //      return res.json();
-  //    })
-  //    .then((data) => {
-  //      console.log(data);
-  //      setBlogInfo(data);
-  //    });
-  //}, [id]);
-
-  console.log(blogInfo);
-
   let authorName = blogInfo ? (blogInfo.author ? blogInfo.author.name : '') : '';
   authorName = authorName.replace(' ', '_');
   console.log(authorName);
 
   return (
     <div className="grid grid-rows gap-0">
-      <div className="text-3xl font-primaryFont flex items-center justify-center h-8 mt-20">
-        {blogInfo?.title}
-      </div>
+      <div className="text-3xl font-primaryFont text-center mt-20 mx-auto">{blogInfo?.title}</div>
       <div className="text-xs font-secondaryFont flex items-center justify-center font-light uppercase text-gray-500 h-5 m-3">
         {blogInfo?.tags
           .map((tag) => {
@@ -77,7 +52,13 @@ const First = () => {
         &nbsp;&nbsp;|&nbsp;&nbsp;6 min read
       </div>
       <div className="flex items-center justify-center m-9">
-        <Image src={blogInfo ? blogInfo.image : ''} width={980} height={514.5} alt="Picture" />
+        <Image
+          src={blogInfo ? blogInfo.image : ''}
+          width={980}
+          height={514.5}
+          alt="Picture"
+          className="w-full max-w-screen-xl mx-auto"
+        />
       </div>
       {blogInfo?.author && (
         <div>
@@ -99,13 +80,35 @@ const First = () => {
           </div>
         </div>
       )}
-      <div className="text-2xl font-primaryFont flex items-center justify-center h-8 mx-60 my-24 pl-2">
+      <div className="text-xl lg:text-2xl font-primaryFont flex items-center justify-center mx-4 lg:mx-64 mb-4 lg:mb-8 mt-8 pl-2">
         {blogInfo?.description}
       </div>
-      <div className="text-xl font-secondaryFont font-semibold h-5 mt-20 mb-4 mx-48">
+      <div className="text-base lg:text-lg font-primaryFont flex flex-col items-center text-gray-600 justify-center mx-4 lg:mx-64 pl-2">
+        <p className="mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nemo aspernatur, est
+          tempore facilis assumenda? Soluta itaque deserunt veniam ut nemo, culpa fugiat odit!
+          Fugiat consequatur sed minima totam perspiciatis.
+        </p>
+        <p className="mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nemo aspernatur, est
+          tempore facilis assumenda? Soluta itaque deserunt veniam ut nemo, culpa fugiat odit!
+          Fugiat consequatur sed minima totam perspiciatis.
+        </p>
+        <p className="mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nemo aspernatur, est
+          tempore facilis assumenda? Soluta itaque deserunt veniam ut nemo, culpa fugiat odit!
+          Fugiat consequatur sed minima totam perspiciatis.
+        </p>
+        <p className="mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nemo aspernatur, est
+          tempore facilis assumenda? Soluta itaque deserunt veniam ut nemo, culpa fugiat odit!
+          Fugiat consequatur sed minima totam perspiciatis.
+        </p>
+      </div>
+      <div className="text-xl font-secondaryFont font-semibold mt-20 mb-4 text-center lg:text-left lg:mx-48">
         Related Blogs
       </div>
-      <div className="flex pl-40 mb-32">
+      <div className="flex flex-col lg:flex-row mx-auto lg:pl-40 mb-32">
         {blogs.slice(0, 3).map((blog: any) => {
           console.log(blog.skills);
           return (
---
+++ aastu/web/group-2/app/blog/layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const layout = ({ children }) => {
+  return <div className="min-h-screen">{children}</div>;
+};
+
+export default layout;
