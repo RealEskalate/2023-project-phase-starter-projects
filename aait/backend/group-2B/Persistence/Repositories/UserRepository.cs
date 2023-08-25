bbaad84b5880176938d945d65fd977e71b374f89
ffekirnew <fikernew.birhanu@a2sv.org>
2023-08-25 11:52:18 +0300

    feat(aait-backend-2b.authentication): Add concrete implementation for IUserRepository

--- aait/backend/group-2B/Persistence/Repositories/UserRepository.cs
+++ aait/backend/group-2B/Persistence/Repositories/UserRepository.cs
@@ -1,15 +1,34 @@
 using SocialSync.Application.Contracts.Persistence;
 using SocialSync.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace SocialSync.Persistence.Repositories;
-
-public class UserRepository : GenericRepository<User>, IUserRepository
+namespace SocialSync.Persistence.Repositories
 {
-    private readonly SocialSyncDbContext _dbContext;
-
-    public UserRepository(SocialSyncDbContext dbContext)
-        : base(dbContext)
+    public class UserRepository : GenericRepository<User>, IUserRepository
     {
-        _dbContext = dbContext;
+        private readonly SocialSyncDbContext _dbContext;
+
+        public UserRepository(SocialSyncDbContext dbContext)
+            : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> EmailExists(string email)
+        {
+            return await _dbContext.Users.AnyAsync(user => user.Email == email);
+        }
+
+        public async Task<User> GetByUsername(string username)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(user => user.Username == username);
+            return user!;
+        }
+
+        public async Task<bool> UsernameExists(string username)
+        {
+            return await _dbContext.Users.AnyAsync(user => user.Username == username);
+        }
     }
 }
+
