76d8e4aefae6379def0cc0c86a28e843a6e94a6d
Dawit Melka <davebadeg@gmail.com>
2023-08-25 17:06:12 +0300

    fix(AAiT-mobile-3): Change return type

--- aait/mobile/group-3/blog_app/lib/features/article/domain/use_case/delete_article.dart
+++ aait/mobile/group-3/blog_app/lib/features/article/domain/use_case/delete_article.dart
@@ -1,15 +1,14 @@
 import '../../../../core/use_case/usecase.dart';
 import '../../../../core/util/typedef.dart';
-import '../entity/article.dart';
 import '../repository/article_repository.dart';
 
-class DeleteArticleUsecase implements UseCase<Article, String>{
+class DeleteArticleUsecase implements UseCase<bool, String>{
   final ArticleRepository _repository;
 
   DeleteArticleUsecase(this._repository);
 
   @override
-  ResultFuture<Article> call(String params) async {
+  ResultFuture<bool> call(String params) async {
     return _repository.deleteArticle(params);
   } 
 }
