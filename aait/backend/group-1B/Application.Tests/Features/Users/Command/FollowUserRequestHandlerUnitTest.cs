193ab9601db7cc654ac50740a759e462bc9d5d42
Sahib Semahegn <semahegnsahib@gmail.com>
2023-08-25 11:48:59 +0300

    add(AAiT-backend-1B) : follow and unfollow feature with unit test

---
+++ aait/backend/group-1B/Application.Tests/Features/Users/Command/FollowUserRequestHandlerUnitTest.cs
@@ -0,0 +1,91 @@
+﻿using Application.Contracts.Persistence;
+using Application.DTOs.Users;
+using Application.Common.Exceptions;
+using Application.features.Users.Handler.Command;
+using Application.features.Users.Request.command;
+using Application.Profiles;
+using Application.Tests.Mocks;
+using AutoMapper;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Tests.Features.Users.Command
+{
+    public class FollowUserRequestHandlerUnitTest
+    {
+
+     
+        private readonly Mock<IUserRepository> _mockRepo;
+        public FollowUserRequestHandlerUnitTest()
+        {
+            _mockRepo = MockUserRepository.GetMockUserRepo();
+        }
+
+
+        [Fact]
+
+        public async Task FollowUserRequest()
+        {
+            var handler = new FollowUserRequestHandler(_mockRepo.Object);
+            var followDto = new FollowDto
+            {
+                FollowerId = 2,
+                FolloweeId = 1
+            };
+            var result = await handler.Handle(
+                new FollowUserRequest() { followDto = followDto} , CancellationToken.None
+                );
+
+            result.ShouldBe(true);
+
+
+
+        }
+
+        [Fact] 
+
+        public async Task FollowUserRequest_ThrowBadRequest()
+        {
+            var handler = new FollowUserRequestHandler(_mockRepo.Object);
+            var followDto = new FollowDto
+            {
+                FollowerId = 1,
+                FolloweeId = 2
+            };
+
+            await Should.ThrowAsync<BadRequestException>(
+
+                handler.Handle(new FollowUserRequest() { 
+                
+                followDto = followDto
+                } , CancellationToken.None)  );
+
+        }
+
+        [Fact]
+        public async Task FollowUserRequest_ThrowBadRequest2()
+        {
+            var handler = new FollowUserRequestHandler(_mockRepo.Object);
+            var followDto = new FollowDto
+            {
+                FollowerId = 1,
+                FolloweeId = 10
+            };
+
+            await Should.ThrowAsync<NotFoundException>(
+
+                handler.Handle(new FollowUserRequest()
+                {
+
+                    followDto = followDto
+                }, CancellationToken.None));
+
+        }
+
+    }
+}
---
+++ aait/backend/group-1B/Application/Features/Users/Handler/Queries/GetUserDetailRequestHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Application.DTOs.Users;
+using MediatR;
+using Application.Features.Users.Request;
+using Application.Contracts.Persistence;
+
+namespace Application.features.Users.Handler.Queries
+{
+    public class GetUserDetailRequestHandler : IRequestHandler<GetUserDetailRequest, UserDetail>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public GetUserDetailRequestHandler(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+        public async Task<UserDetail> Handle(GetUserDetailRequest request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserDetail(request.Id);
+
+            var userDetail = _mapper.Map<UserDetail>(user);
+
+            return userDetail;
+        }
+    }
+}
