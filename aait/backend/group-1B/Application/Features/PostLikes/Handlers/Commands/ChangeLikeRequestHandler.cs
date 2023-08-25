182c371ef5718639e6c5b2bd96c54ec79cb31c5f
Ananiya <ananiyat24@gmail.com>
2023-08-25 10:38:05 +0300

    complete notification feature for postlikes and comments

--- aait/backend/group-1B/Application/Features/PostLikes/Handlers/Commands/ChangeLikeRequestHandler.cs
+++ aait/backend/group-1B/Application/Features/PostLikes/Handlers/Commands/ChangeLikeRequestHandler.cs
@@ -11,25 +11,38 @@ namespace Application.Features.PostLikes.Handlers.Commands;
 
 public class ChangeLikeRequestHandler : IRequestHandler<ChangeLikeRequest, Unit>
 {
-    private readonly IPostRepository _postRepository;
-    private readonly IPostLikesRepository _likesRepository;
+    private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
 
-    public ChangeLikeRequestHandler(IPostLikesRepository postLikesRepository, IPostRepository postRepository, IMapper mapper)
+    public ChangeLikeRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
     {
-        _postRepository = postRepository;
-        _likesRepository = postLikesRepository;
+        _unitOfWork = unitOfWork;
         _mapper = mapper;
     }
 
     public async Task<Unit> Handle(ChangeLikeRequest request, CancellationToken token)
     {
-        var validator = new ChangeLikeDtoValidator(_postRepository);
+        var validator = new ChangeLikeDtoValidator(_unitOfWork.PostRepository);
         var validationResult = await validator.ValidateAsync(request.ChangeLike, token);
         if (validationResult.IsValid == false)
             throw new ValidationException(validationResult);
 
-        await _likesRepository.ChangeLike(_mapper.Map<PostLike>(request.ChangeLike));
+        var liked = await _unitOfWork.PostLikesRepository.ChangeLike(_mapper.Map<PostLike>(request.ChangeLike));
+        if (liked == 1)
+        {
+            var recievingPost = await _unitOfWork.PostRepository.Get(request.ChangeLike.PostId);
+
+            var notif = new Notification()
+            {
+                UserId = recievingPost.UserId,
+                NotificationType = NotificationType.Like,
+                Message = $"User {request.ChangeLike.UserId} has liked your post with Id {request.ChangeLike.PostId}."
+            };
+        
+            await _unitOfWork.NotificationRepository.Add(notif);
+        }
+
+        await _unitOfWork.Save();
         return Unit.Value;
     }
     
