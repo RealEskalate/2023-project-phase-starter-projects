565904a505f03454681bac6416f152e99b6cd823
Sahib Semahegn <semahegnsahib@gmail.com>
2023-08-25 11:48:59 +0300

    add(AAiT-backend-1B) : follow and unfollow feature with unit test

--- aait/backend/group-1B/Application/Contracts/Persistence/IUserRepository.cs
+++ aait/backend/group-1B/Application/Contracts/Persistence/IUserRepository.cs
@@ -1,8 +1,23 @@
+
 using Domain.Entities;
 
 namespace Application.Contracts.Persistence;
 
-public interface IUserRepository : IGenericRepository<User>
+
+
+public interface IUserRepository
 {
-    
+    Task<bool> FollowUser(int followerId, int followeeId);
+
+    Task<bool> UnFollowUser(int followerId, int followeeId);
+
+    Task<List<User>> GetUsers(int userId, bool getFollower);
+
+    Task UpdateUser(User user);
+
+    Task<User?> GetUserDetail(int userId);
+
+
+    Task<bool> Exists(int userId);
+
 }
---
+++ aait/backend/group-1B/Application/DTOS/Users/FollowDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Users
+{
+    public class FollowDto
+    {
+
+        public int FollowerId { get; set; }
+
+        public int FolloweeId { get; set; }
+
+    }
+}
