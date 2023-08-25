bdabb8d9d87a2f915978ad2f4e00dff875cbd8af
yohanse mehabaw <105191845+yohanse@users.noreply.github.com>
2023-08-25 13:56:33 +0300

    feat(AAit-mobile-g3): add presentaion bloc layer (#251)

---
+++ aait/mobile/group-3/blog_app/lib/features/auth/presentation/bloc/signup_bloc/bloc/signup_bloc.dart
@@ -0,0 +1,39 @@
+import 'package:bloc/bloc.dart';
+import 'package:blog_app/core/use_case/usecase.dart';
+import 'package:blog_app/features/auth/domain/entity/auth_entitie.dart';
+import 'package:blog_app/features/auth/domain/use_case/signup_usecase.dart';
+import 'package:equatable/equatable.dart';
+
+part 'signup_event.dart';
+part 'signup_state.dart';
+
+class SignupBloc extends Bloc<SignupEvent, SignupState> {
+  final SignUpUsecase signUpUsecase;
+  SignupBloc({required this.signUpUsecase}) : super(SignupInitial()) {
+    on<SignupEvent>((event, emit) async {
+      emit(SignupLoadingState());
+      var result = await signUpUsecase(
+        Params(
+          AuthEntitie(
+            email: event.email,
+            password: event.password,
+            fullName: event.fullName,
+            expertise: event.expertise,
+            bio: event.bio,
+          ),
+        ),
+      );
+
+      result.fold(
+        (l) => emit(
+          SignupErrorState(
+            errorStateMessage: l.message,
+          ),
+        ),
+        (r) => emit(
+          SignupSuccessState(),
+        ),
+      );
+    });
+  }
+}
