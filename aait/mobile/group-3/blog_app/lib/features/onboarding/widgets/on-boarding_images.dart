0f679c7d4b1da8bd8d5359e7baff428921e8e578
abi26anamo <91598495+abi26anamo@users.noreply.github.com>
2023-08-25 11:22:46 +0300

    aait.mobile.g3.abinet_onboarding_pages (#206)

---
+++ aait/mobile/group-3/blog_app/lib/features/onboarding/widgets/on-boarding_images.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class OnboardingImagesWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: ScreenUtil().setWidth(40),
+      ),
+      height: ScreenUtil().setHeight(332),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              OnboardingImage(
+                imagePath: "assets/images/onboarding1.png",
+                width: 92,
+              ),
+              SizedBox(
+                width: ScreenUtil().setWidth(10),
+              ),
+              OnboardingImage(
+                imagePath: "assets/images/onboarding2.png",
+                width: 180,
+              ),
+            ],
+          ),
+          SizedBox(height: 10.h),
+          Row(
+            children: [
+              OnboardingImage(
+                imagePath: "assets/images/onboarding3.png",
+                width: 180,
+              ),
+              SizedBox(
+                width: ScreenUtil().setWidth(10),
+              ),
+              OnboardingImage(
+                imagePath: "assets/images/onboarding4.png",
+                width: 92,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnboardingImage extends StatelessWidget {
+  final String imagePath;
+  final double width;
+
+  const OnboardingImage({
+    required this.imagePath,
+    required this.width,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(157),
+      width: ScreenUtil().setWidth(width),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(24),
+        child: Image.asset(
+          imagePath,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
