0f4920a71d2e761ea72e6c54ce35069010d490d6
Dawit Melka <davebadeg@gmail.com>
2023-08-25 17:08:01 +0300

    fix(AAiT-mobile-3): Update parameter of getArticle

--- aait/mobile/group-3/blog_app/lib/features/article/domain/use_case/get_article.dart
+++ aait/mobile/group-3/blog_app/lib/features/article/domain/use_case/get_article.dart
@@ -1,15 +1,41 @@
+import 'package:equatable/equatable.dart';
+
 import '../../../../core/use_case/usecase.dart';
 import '../../../../core/util/typedef.dart';
 import '../entity/article.dart';
 import '../repository/article_repository.dart';
 
-class GetArticleUsecase implements UseCase<Article, String>{
+class GetArticleUsecase implements UseCase<List<Article>, GetArticleParams>{
   final ArticleRepository _repository;
 
   GetArticleUsecase(this._repository);
 
   @override
-  ResultFuture<Article> call(String params) {
-    return _repository.getArticle(params);
+  ResultFuture<List<Article>> call(GetArticleParams params) {
+    return _repository.getArticle(
+      tags: params.tags ?? "",
+      searchParams: params.searchParams ?? "",
+    );
   }
+}
+
+class GetArticleParams extends Equatable {
+  const GetArticleParams({
+    this.tags,
+    this.searchParams
+  });
+
+
+  final String? tags;
+  final String? searchParams;
+
+  const GetArticleParams.empty()
+    :this(
+          tags: 'Other',
+          searchParams: 'wo',
+        );
+
+  @override
+  List<Object?> get props =>
+      [tags, searchParams];
 }
