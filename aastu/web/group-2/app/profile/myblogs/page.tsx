f7413d4b985ec4db1de233eb266b3c0d8cb02be1
Natanim Ashenafi Kefeni <142803189+natanim-ashenafi@users.noreply.github.com>
2023-08-25 06:15:05 +0300

    feat(AASTU-web-G2): Add RTK and organize some pages (#223)
    Co-authored-by: Natanim Ashenafi <natanimashenafi20@gmail.com>

--- aastu/web/group-2/app/profile/myblogs/page.tsx
+++ aastu/web/group-2/app/profile/myblogs/page.tsx
@@ -1,19 +1,18 @@
 "use client"
 import { Blog, User } from '@/lib/types';
-const startingId = 10;
-const users: User[] = [
-  ];
-  
-  const blogs: Blog[] = [
-    
-  ];
+
 
 import MyBlogCard from "@/app/components/profile/MyblogCard"
+import { useGetBlogsQuery, useMyBlogsQuery } from '@/lib/redux/slices/blogsApi';
+
 export default function Section() {
+
+    const {data: blogs, error, isSuccess, isLoading} = useMyBlogsQuery()
+
     return (
-        <div 
+        <div
             className='grid font-secondaryFont mb-12 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
-            {blogs.map((blog, i) => {
+            {blogs?.map((blog, i) => {
                 return (
                     <MyBlogCard key={i} {...blog} />
                 )
