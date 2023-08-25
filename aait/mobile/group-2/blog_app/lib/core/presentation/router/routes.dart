6b5019efaa1b513baae504badf53edc5c8c1dded
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-08-25 09:34:15 +0300

    feat(AAiT-mobile-2): Add Article Create/Update and Detail Screen (#222)

---
+++ aait/mobile/group-2/blog_app/lib/core/presentation/router/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const home = '/';
+  static const articleDetail = '/article-detail';
+  static const createArticle = '/create-article';
+  static const editArticle = '/edit-article';
+}
