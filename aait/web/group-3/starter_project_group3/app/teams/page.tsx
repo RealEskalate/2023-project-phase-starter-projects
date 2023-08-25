47dca1fc6798c9bc0202c84e6f7ea77dcfc3c3e9
surafel58 <surafelsentayehu58@gmail.com>
2023-08-25 23:31:01 +0300

    feat(AAiT-web_g3): Add teams page

---
+++ aait/web/group-3/starter_project_group3/app/teams/page.tsx
@@ -0,0 +1,81 @@
+"use client";
+import HeadLineSection from "@/components/team/HeadLineSection";
+import Pagination from "@/components/team/Pagination";
+import { teams } from "@/data/team/Teams";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+const Teams = () => {
+  const itemsPerPage = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+  const indexOfLast = currentPage * itemsPerPage;
+  const indexOfFirst = indexOfLast - itemsPerPage;
+
+  const currentTeam = teams.slice(indexOfFirst, indexOfLast);
+  const totalPages = Math.ceil(teams.length / itemsPerPage);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div className="flex flex-col items-center mx-8 md:mx-20 gap-36 font-Poppins">
+      <HeadLineSection />
+      <hr className="w-full" />
+      {/* <div className="flex flex-row gap-4 flex-wrap justify-center "> */}
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {currentTeam.map((team) => (
+          <div className="flex flex-col gap-6 shadow-lg items-center border-2 p-6">
+            <Image src={team.imageUrl} width={100} height={100} alt="teams" />
+            <div className="flex flex-col gap-2 justify-center text-center">
+              <p className="font-bold  text-2xl">{team.name}</p>
+              <p className="font-semibold text-lg">{team.role}</p>
+            </div>
+            <p>{team.description}</p>
+            <hr className="w-full" />
+            <div className="flex justify-between w-full">
+              <Link href={"#"}>
+                <Image
+                  src="/assets/facebook.svg"
+                  width={30}
+                  height={30}
+                  alt="social media"
+                />
+              </Link>
+
+              <Link href={"#"}>
+                <Image
+                  src="/assets/linkedin.svg"
+                  width={30}
+                  height={30}
+                  alt="social media"
+                />
+              </Link>
+
+              <Link href={"#"}>
+                <Image
+                  src="/assets/instagram.svg"
+                  width={30}
+                  height={30}
+                  alt="social media"
+                />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="mb-16 -mt-16 flex justify-center">
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+          className="pagination"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Teams;
---
+++ aait/web/group-3/starter_project_group3/components/team/HeadLineSection.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+const HeadLineSection = () => {
+  return (
+    <div className="headline grid gap-9 nav_bar_screen:gap-0 nav_bar_screen:grid-cols-2 font-Poppins mt-20">
+      <div className="description flex flex-col gap-12 nav_bar_screen:w-3/4">
+        <h1 className="uppercase font-bold text-4xl nav_bar_screen:text-6xl">
+          The team we're currently working with
+        </h1>
+        <p className="text-lg text-light_gray_text_color nav_bar_screen:text-2xl text-left leading-relaxed">
+          Meet our development team is a small but highly skilled and
+          experienced group of professionals. We have a talented mix of web
+          developers, software engineers, project managers and quality assurance
+          specialists who are passionate about developing exceptional products
+          and services.
+        </p>
+      </div>
+      <Image
+        objectFit="cover"
+        src="/assets/Teams_team_work.svg"
+        alt="team work"
+        width={800}
+        height={800}
+      />
+    </div>
+  );
+};
+
+export default HeadLineSection;
