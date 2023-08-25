476047de9396034e3bca249a59ca29339bc905c6
ffekirnew <fikernew.birhanu@a2sv.org>
2023-08-25 11:27:07 +0300

    feat(aait-backend-2b.authentication): Added the SaveAsync method to the mock unit of work

--- aait/backend/group-2B/Application.Tests/Mocks/Persistence/MockUnitOfWork.cs
+++ aait/backend/group-2B/Application.Tests/Mocks/Persistence/MockUnitOfWork.cs
@@ -6,13 +6,13 @@ namespace SocialSync.Application.Tests.Mocks;
 
 public class MockUnitOfWork {
   public static Mock<IUnitOfWork> GetMockUnitOfWork() {
-    var unitOfWork = new Mock<IUnitOfWork>();
+    var mockUnitOfWork = new Mock<IUnitOfWork>();
 
     var mockUserRepository = MockUserRepository.GetUserRepository();
 
-    unitOfWork.Setup(uow => uow.UserRepository).Returns(mockUserRepository.Object);
-    unitOfWork.Setup(uow => uow.SaveAsync()).ReturnsAsync(1);
+    mockUnitOfWork.Setup(uow => uow.UserRepository).Returns(mockUserRepository.Object);
+    mockUnitOfWork.Setup(uow => uow.SaveAsync()).ReturnsAsync(1);
 
-    return unitOfWork;
+    return mockUnitOfWork;
   }
 }
