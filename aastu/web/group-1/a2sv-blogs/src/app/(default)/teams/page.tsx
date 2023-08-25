2f8f08b6e9c16a8b661a5259ad32b29d4af25afd
ruth-wossen-a2sv <142803496+ruth-wossen-a2sv@users.noreply.github.com>
2023-08-25 12:01:14 +0300

    Aastu.ruth.wossen.team.page (#249)
    Co-authored-by: Ruth Wossen <ruthwossen75@gmail.com>

---
+++ aastu/web/group-1/a2sv-blogs/src/app/(default)/teams/page.tsx
@@ -0,0 +1,15 @@
+import TeamHome from "@/components/teams/TeamHome";
+import TeamMemberList from "@/components/teams/TeamMemberList";
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <TeamHome />
+      <TeamMemberList />
+    </div>
+  )
+}
+
+export default page
