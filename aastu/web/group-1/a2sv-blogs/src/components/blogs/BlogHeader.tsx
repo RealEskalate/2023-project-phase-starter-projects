c91fec15b82a3f8fdcb06c87c2775564330eb6e9
Yoseph Shemeles <142782245+yoseph-shemeles-a2sv@users.noreply.github.com>
2023-08-25 10:36:47 +0300

    feat(aastu.web):Homepage-stories-responsivness (#229)
    Co-authored-by: Yoseph Shemeles <jossyfresh2002@gmail.com>

--- aastu/web/group-1/a2sv-blogs/src/components/blogs/BlogHeader.tsx
+++ aastu/web/group-1/a2sv-blogs/src/components/blogs/BlogHeader.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 export default function BlogHeader() {
   return (
-    <div className="w-[60%] mx-28 flex justify-between">
-      <h1 className="text-xl font-montserrat font-extrabold text-black leading-5">
+    <div className="w-[60%] mx-28 flex lg:flex-row flex-col justify-between">
+      <h1 className="text-xl font-montserrat lg:visible invisible font-extrabold text-black leading-5">
         Blogs
       </h1>
-      <div className="flex gap-5">
+      <div className="flex lg:gap-5 justify-between">
         <input
-          className="h-10 border border-black px-5 w-60 rounded-3xl placeholder:text-sm placeholder:font-montserrat placeholder:font-medium placeholder:pl-4"
+          className="h-10 border border-black px-5 md:w-60 sm:w-48 w-36 rounded-3xl placeholder:text-sm placeholder:font-montserrat placeholder:font-medium placeholder:pl-4"
           placeholder="Search . . ."
           type="text"
           name=""
@@ -17,7 +17,7 @@ export default function BlogHeader() {
         />
         <Link
           href="/blogs/create"
-          className="h-10 text-white font-montserrat text-sm font-semibold w-32 bg-blue-800 px-5 rounded-full flex items-center"
+          className="h-10 text-white font-montserrat md:text-sm text-xs font-semibold md:w-32 w-32 bg-blue-800 px-5 rounded-full  flex items-center"
         >
           + New Blog
         </Link>
