3b14743241bd423b5d1ae83f68e24421c18680b5
primequantuM4 <gediezra@gmail.com>
2023-08-25 15:29:21 +0300

    fix: merge

---
+++ aait/mobile/group-3/blog_app/lib/core/util/image_sheet.dart
@@ -0,0 +1,51 @@
+import 'package:blog_app/core/util/image_converter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ImageSheet {
+  static void show(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
+            child: Column(
+              children: [
+                Text(
+                  'Update profile picture',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                ListTile(
+                  leading: Icon(Icons.camera_alt_outlined),
+                  title: Text('Take a Photo'),
+                  onTap: () async {
+                    final chosenImage = ImageConverter().pickImageFromCamera();
+                    Navigator.pop(context, chosenImage);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.photo_library),
+                  title: Text('Choose from gallery'),
+                  onTap: () async {
+                    final chosenImage = ImageConverter().pickImageFromGallery();
+                    Navigator.pop(context, chosenImage);
+                  },
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel'),
+                )
+              ],
+            ),
+          );
+        });
+  }
+}
