d771a5c33a22aab8bc5898d31a1d86c3ffc1461b
Nathan Kebede <nathanzelalem100@gmail.com>
2023-08-27 12:28:37 +0300

    Make the profile and posts bar responsive

--- aait/mobile/group-2/blog_app/lib/features/user/presentation/widgets/userprofile/posts_bar.dart
+++ aait/mobile/group-2/blog_app/lib/features/user/presentation/widgets/userprofile/posts_bar.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ArticleTitleBar extends StatelessWidget {
   final String title;
+
   const ArticleTitleBar({
-    super.key,
+    Key? key,
     required this.title,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context);
+
+    double titleFontSize = 20.sp;
+    double iconSize = 24.w;
+    double iconSpacing = 12.w;
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
           title,
           style: TextStyle(
-            fontSize: 20,
+            fontSize: titleFontSize,
             fontWeight: FontWeight.w500,
             color: Color(0xFF0D253C),
           ),
@@ -26,14 +34,16 @@ class ArticleTitleBar extends StatelessWidget {
             Icon(
               Icons.grid_view,
               color: Color(0xFF376AED),
+              size: iconSize,
             ),
-            SizedBox(width: 12),
+            SizedBox(width: iconSpacing),
             Icon(
               Icons.list,
               color: Color(0xFF7B8BB2),
+              size: iconSize,
             ),
           ],
-        )
+        ),
       ],
     );
   }
--- aait/mobile/group-2/blog_app/lib/features/user/presentation/widgets/userprofile/profile_bar.dart
+++ aait/mobile/group-2/blog_app/lib/features/user/presentation/widgets/userprofile/profile_bar.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProfileBar extends StatelessWidget {
   const ProfileBar({
-    super.key,
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context);
+
+    double marginHorizontal = 40.w;
+    double marginTop = 24.h;
+    double fontSize = 24.sp;
+    double iconSize = 24.w;
+
     return Container(
-      margin: const EdgeInsets.fromLTRB(40, 24, 40, 0),
-      child: const Row(
+      margin: EdgeInsets.fromLTRB(marginHorizontal, marginTop, marginHorizontal, 0),
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
             "Profile",
             style: TextStyle(
-              fontSize: 24,
+              fontSize: fontSize,
               fontWeight: FontWeight.w500,
               color: Color(0xFF0D253C),
             ),
@@ -23,6 +31,7 @@ class ProfileBar extends StatelessWidget {
           Icon(
             Icons.more_horiz,
             color: Color(0xFF0D253C),
+            size: iconSize,
           ),
         ],
       ),
