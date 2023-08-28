dd1b25834e8123c0e1d785bdfa936b8a8be006af
Yoseph Shemeles <jossyfresh2002@gmail.com>
2023-08-28 10:01:57 +0300

    feat(aastu.web.g1):responsiveness

--- aastu/web/group-1/a2sv-blogs/src/components/blogs/BlogHeader.tsx
+++ aastu/web/group-1/a2sv-blogs/src/components/blogs/BlogHeader.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 export default function BlogHeader() {
   return (
-    <div className="w-[60%] mx-28 flex lg:flex-row flex-col justify-between">
+    <div className="md:w-[60%] mx-28 flex lg:flex-row flex-col justify-between">
       <h1 className="text-xl font-montserrat lg:visible invisible font-extrabold text-black leading-5">
         Blogs
       </h1>
-      <div className="flex lg:gap-5 justify-between">
+      <div className="flex lg:gap-5 justify-center gap-5">
         <input
           className="h-10 border border-black px-5 md:w-60 sm:w-48 w-36 rounded-3xl placeholder:text-sm placeholder:font-montserrat placeholder:font-medium placeholder:pl-4"
           placeholder="Search . . ."
--- aastu/web/group-1/a2sv-blogs/src/components/common/NavBar.tsx
+++ aastu/web/group-1/a2sv-blogs/src/components/common/NavBar.tsx
@@ -2,9 +2,23 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/hooks/useAuth";
+import ProfileAvatar from "./ProfileAvatar";
 
 export default function Nav() {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const router = useRouter();
+  const auth = useAuth().auth.isAuthenticated;
+  const imageUrl = useAuth().auth.userProfile;
+
+  const handleAuth = () => {
+    if (auth) {
+      router.push("/auth/profile");
+    } else {
+      router.push("/auth/login");
+    }
+  };
 
   const handleToggle = () => {
     setOpenMenu(!openMenu);
@@ -45,106 +59,6 @@ export default function Nav() {
               </button>
             </span>
           </div>
-          <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
-            onClick={handleToggle}
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 lg focus:outline-none focus:ring-2"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-        </div>
-        <div
-          className={`${
-            openMenu ? "" : "hidden"
-          } items-center justify-between w-full lg:flex lg:w-auto lg:order-1`}
-          id="navbar-sticky"
-        >
-          <ul className="flex flex-col p-4 lg:p-0 mt-4 font-medium border  border-gray-100 rounded-lg  lg:flex-row lg:space-x-8 lg:mt-0 lg:border-0 lg:bg-white ">
-            <li>
-              <Link
-                href="/"
-                className="block py-2 pl-3 pr-4  rounded lg:bg-transparent  lg:p-0 lg:hover:bg-white text-[#3C3C3C] text-base hover:bg-gray-100  font-semibold font-montserrat"
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/team"
-                className="block py-2 pl-3 pr-4  rounded lg:bg-transparent  lg:p-0 lg:hover:bg-white  text-[#3C3C3C] text-base hover:bg-gray-100  font-semibold font-montserrat"
-              >
-                Team
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/stories"
-                className="block py-2 pl-3 pr-4  rounded lg:bg-transparent  lg:p-0 lg:hover:bg-white  text-[#3C3C3C] text-base hover:bg-gray-100 font-semibold font-montserrat"
-              >
-                Success Stories
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="block py-2 pl-3 pr-4  rounded lg:bg-transparent   lg:hover:bg-white  lg:p-0  text-[#3C3C3C] text-base hover:bg-gray-100   font-semibold font-montserrat"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/blogs"
-                className="block py-2 pl-3 pr-4  rounded lg:bg-transparent lg:hover:bg-white  lg:p-0  text-[#3C3C3C] text-base hover:bg-gray-100   font-semibold font-montserrat"
-              >
-                Blogs
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="block py-2 pl-3 pr-4  rounded lg:bg-transparent  lg:hover:bg-white  lg:p-0  text-[#3C3C3C] text-base hover:bg-gray-100  font-semibold font-montserrat"
-              >
-                Get Involved
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="lg:hidden block py-2 pl-3 pr-4 rounded text-[#3C3C3C] text-base font-semibold font-montserrat hover:bg-gray-100 lg:hover:bg-white   lg:p-0   "
-              >
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="lg:hidden block py-2 pl-3 pr-4 rounded text-[#3C3C3C] text-base font-semibold font-montserrat hover:bg-gray-100 lg:hover:bg-white   lg:p-0   "
-              >
-                Donate
-              </Link>
-            </li>
-          </ul>
-        </div>
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
