cef221a8c4655e921714ee14afa5ca0d5df6e5d2
ante-neh <amareanteneh12@gmail.com>
2023-08-28 20:51:27 +0300

    feat(aait.web.g4): impact-stories

--- aait/web/group-4/starter-project/app/page.tsx
+++ aait/web/group-4/starter-project/app/page.tsx
@@ -1,10 +1,4 @@
 'use client'
-
-import { store } from "@/store/page";
-import { storiesApi } from "@/store/story/story-api";
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
-import { Provider } from "react-redux";
-
 export default function Home() {
   return (
     <main>
