b00c6742e6a3a0ccf0191de0729890baa569324d
Leul Wujira <meraf000@gmail.com>
2023-08-28 10:24:34 +0300

    feat(AAiT-mobile-2): Add Tag Selector Bloc

---
+++ aait/mobile/group-2/blog_app/lib/features/article/presentation/bloc/tag_selector_event.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+import '../../domain/entities/tag.dart';
+
+sealed class TagSelectorEvent extends Equatable {
+  const TagSelectorEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AddTagEvent extends TagSelectorEvent {
+  final Tag tag;
+
+  const AddTagEvent(this.tag);
+}
+
+class RemoveTagEvent extends TagSelectorEvent {
+  final Tag tag;
+
+  const RemoveTagEvent(this.tag);
+}
