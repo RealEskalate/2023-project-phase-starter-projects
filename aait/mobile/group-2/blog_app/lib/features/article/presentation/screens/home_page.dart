ee9fbf292374d277bca23c78669322aa4700c14d
betabravah <betabravah@gmail.com>
2023-08-28 17:00:55 +0300

    feat(AAiT-mobile-2): Add Home Page

---
+++ aait/mobile/group-2/blog_app/lib/features/article/presentation/screens/home_page.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../injection_container.dart';
+
+import '../bloc/article_bloc.dart';
+import '../widgets/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //
+  @override
+  Widget build(BuildContext context) {
+    //
+    return Scaffold(
+      appBar: CustomAppBar(
+        leading: SvgPicture.asset(
+          'assets/svg/menu.svg',
+          fit: BoxFit.scaleDown,
+        ),
+        title: 'Welcome Back!',
+        actions: const [
+          ProfileAvatar(image: 'assets/images/profile.png'),
+        ],
+      ),
+      body: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) =>
+                serviceLocator<ArticleBloc>()..add(LoadAllArticlesEvent()),
+          ),
+          BlocProvider(
+            create: (context) =>
+                serviceLocator<ArticleBloc>()..add(LoadAllTagsEvent()),
+          ),
+        ],
+        child: BlocBuilder<ArticleBloc, ArticleState>(
+          builder: (context, _) => buildBody(context),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          size: 35,
+        ),
+      ),
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        height: MediaQuery.of(context).size.height - 100,
+        padding: const EdgeInsets.all(20.0),
+        color: Theme.of(context).colorScheme.background,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CustomSearchBar(),
+            const SizedBox(
+              height: 20,
+            ),
+
+            BlocConsumer<ArticleBloc, ArticleState>(
+              listener: (context, state) {
+                if (state is ArticleErrorState) {
+                  showError(context, state.message);
+                }
+              },
+              builder: (context, state) {
+                if (state is TagLoadingState) {
+                  return const LoadingWidget();
+                } else if (state is AllTagsLoadedState) {
+                  return SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: TagList(tagList: state.tags));
+                }
+                return const LoadingWidget();
+              },
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+
+            //
+            BlocConsumer<ArticleBloc, ArticleState>(
+              listener: (context, state) {
+                if (state is ArticleErrorState) {
+                  showError(context, state.message);
+                }
+              },
+              builder: (context, state) {
+                if (state is ArticleLoadingState) {
+                  return const LoadingWidget();
+                } else if (state is AllArticlesLoadedState) {
+                  return ArticleList(articles: state.articles);
+                }
+                return const LoadingWidget();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
