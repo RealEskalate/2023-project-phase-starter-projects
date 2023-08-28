2052018e3c9305fe6e2a26a0fa1ae6923b98c89e
Dawit Andargachew <hereisdawit@gmail.com>
2023-08-28 11:36:57 +0300

    feat(AAiT-web_g3): add login and registration redux implementation (#298)

--- aait/web/group-3/starter_project_group3/store/index.ts
+++ aait/web/group-3/starter_project_group3/store/index.ts
@@ -2,20 +2,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import { createBlogApi } from "./features/create-blog/create-blog-api";
 import { singleBlogApi } from "./features/blog-detail/blog-detail";
 import { blogsApi } from "./features/blogs/blogs-api";
+import { authApi } from "./features/auth";
 
 export const store = configureStore({
-    reducer: {
-        [blogsApi.reducerPath]: blogsApi.reducer,
-        [singleBlogApi.reducerPath]: singleBlogApi.reducer,
-        [createBlogApi.reducerPath]: createBlogApi.reducer
-    },
-    middleware: getDefaultMiddleware =>
+  reducer: {
+    [blogsApi.reducerPath]: blogsApi.reducer,
+    [singleBlogApi.reducerPath]: singleBlogApi.reducer,
+    [createBlogApi.reducerPath]: createBlogApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
-    .concat(blogsApi.middleware)
-    .concat(singleBlogApi.middleware)
-    .concat(createBlogApi.middleware)
-    
-})
+      .concat(blogsApi.middleware)
+      .concat(singleBlogApi.middleware)
+      .concat(createBlogApi.middleware)
+      .concat(authApi.middleware),
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
---
+++ aait/web/group-3/starter_project_group3/types/auth/authTypes.ts
@@ -0,0 +1,10 @@
+interface authTypes {
+  user: string;
+  userName: string;
+  userRole: string;
+  userEmail: string;
+  userProfile: string;
+  token: string;
+}
+
+export type { authTypes };
