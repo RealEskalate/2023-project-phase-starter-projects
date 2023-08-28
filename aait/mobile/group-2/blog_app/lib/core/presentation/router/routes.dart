61f69246d440b0615b700089f7361149a50f680f
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-08-28 23:00:03 +0300

    update router

--- aait/mobile/group-2/blog_app/lib/core/presentation/router/routes.dart
+++ aait/mobile/group-2/blog_app/lib/core/presentation/router/routes.dart
@@ -1,12 +1,12 @@
 class Routes {
-  static const home = '/';
+  static const splashScreen = '/';
 
   static const articles = '/articles';
   static const articleDetail = '/article-detail';
   static const createArticle = '/create-article';
   static const editArticle = '/edit-article';
 
-  static const splashScreen = '/splash-screen';
+  static const home = '/home';
   static const onBoard = '/onboarding';
 
   static const auth = '/auth';
