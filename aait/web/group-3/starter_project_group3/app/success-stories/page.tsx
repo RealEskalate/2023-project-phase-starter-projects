238662d659a64b4d86c179f2d96f3c7dd4115eb4
surafel58 <surafelsentayehu58@gmail.com>
2023-08-28 10:19:05 +0300

    feat(AAiT-web-g3): Add sucess-stories page

---
+++ aait/web/group-3/starter_project_group3/app/success-stories/page.tsx
@@ -0,0 +1,15 @@
+import StoriesSection from "@/components/success-stories/StoriesSection";
+import HeadLineSection from "@/components/success-stories/HeadLineSection";
+import InterviewPartnerSection from "@/components/success-stories/InterviewPartnerSection";
+
+const SuccessStories = () => {
+  return (
+    <div className="flex flex-col gap-14 items-center mx-10 md:mx-20 mt-20">
+      <HeadLineSection />
+      <StoriesSection />
+      <InterviewPartnerSection />
+    </div>
+  );
+};
+
+export default SuccessStories;
