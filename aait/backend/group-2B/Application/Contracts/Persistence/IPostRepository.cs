228dc3d3ccb90c21a5b93f5a8c308fcbcce4ade1
= <amanuel.tewodros@a2sv.org>
2023-08-28 10:01:33 +0300

    fix(AAiT-backend-2b): Add the post repositoty interface methods

--- aait/backend/group-2B/Application/Contracts/Persistence/IPostRepository.cs
+++ aait/backend/group-2B/Application/Contracts/Persistence/IPostRepository.cs
@@ -1,5 +1,10 @@
 using SocialSync.Domain.Entities;
 
 namespace SocialSync.Application.Contracts.Persistence;
+public interface IPostRepository : IGenericRepository<Post>
+{
+    public Task<IReadOnlyList<Post>> GetPostsByUserIdAsync(int userId);
+    public Task<IReadOnlyList<Post>> GetPostsByTagsAsync(List<string> tags);
 
-public interface IPostRepository : IGenericRepository<Post> { }
+    public Task<bool> ExistsAsync(int id);
+}
