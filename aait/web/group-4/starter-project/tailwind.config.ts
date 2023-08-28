22a1d9fb5592bd6340fccd14427b91f4a904e7e4
kira-1011 <kirakstade@gmail.com>
2023-08-28 05:34:55 -0900

    fix(aait.web.g4.homepage): Resolve PR comments

--- aait/web/group-4/starter-project/tailwind.config.ts
+++ aait/web/group-4/starter-project/tailwind.config.ts
@@ -10,21 +10,6 @@ const config: Config = {
     extend: {
       colors: {
         "primary-color": "#264FAD",
-        "blue-purple": "#160041",
-        "charcoal": "#363636",
-        "dark-gray": "#424242",
-        "medium-gray": "#7D7D7D",
-        "light-gray": "#878593",
-        "slate-blue": "#37474F",
-        "dim-gray": "#565656",
-        "smoke": "#FDFEFF",
-        "lavender-gray": "#F6F6FC",
-        "grayish-blue": "#878593",
-        "grayish-brown": "#4D4A49",
-        "dodger-blue": "#019CFA",
-        "jet-black":"#242424",
-        "gunmetal-gray":"#565656",
-        "very-dark-gray" : "#0F0F0F"
       },
       fontFamily: {
         "poppins": ['Poppins', 'sans-serif'],
