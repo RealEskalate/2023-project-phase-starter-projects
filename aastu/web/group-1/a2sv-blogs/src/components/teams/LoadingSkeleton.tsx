759444da6153a73034b6c825054a048a44abd456
ruth-wossen-a2sv <142803496+ruth-wossen-a2sv@users.noreply.github.com>
2023-08-28 21:00:31 +0300

    make teampage responsive and complete the pagination (#314)
    Co-authored-by: Ruth Wossen <ruthwossen75@gmail.com>

---
+++ aastu/web/group-1/a2sv-blogs/src/components/teams/LoadingSkeleton.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const LoadingSkeleton = () => {
+  return (
+    <div>
+        <div className="w-full md:w-[400px] h-[505px] rounded-[12px] bg-white shadow-lg mt-5 sm:w-full">
+  <div className="text-center py-5">
+    <div className="animate-pulse w-[150px] h-[150px] rounded-full mx-auto bg-gray-300"></div>
+  </div>
+
+  <div className="text-center py-2">
+    <div className="animate-pulse w-3/4 h-6 rounded bg-gray-300 mx-auto"></div>
+    <div className="animate-pulse w-1/2 h-4 rounded mt-2 bg-gray-300 mx-auto"></div>
+    <div className="animate-pulse w-1/3 h-4 rounded mt-2 bg-gray-300 mx-auto"></div>
+  </div>
+
+  <div className="text-center py-2">
+    <div className="animate-pulse w-5/6 h-16 rounded bg-gray-300 mx-auto"></div>
+  </div>
+
+  <div className="text-center pt-20">
+    <hr className="border-gray-200" />
+  </div>
+
+  <div className="flex justify-evenly pt-4">
+    <div className="animate-pulse w-10 h-10 rounded-full bg-gray-300"></div>
+    <div className="animate-pulse w-10 h-10 rounded-full bg-gray-300"></div>
+    <div className="animate-pulse w-10 h-10 rounded-full bg-gray-300"></div>
+  </div>
+</div>
+      
+    </div>
+  )
+}
+
+export default LoadingSkeleton
--- aastu/web/group-1/a2sv-blogs/src/components/teams/PaginationButtons.tsx
+++ aastu/web/group-1/a2sv-blogs/src/components/teams/PaginationButtons.tsx
@@ -6,24 +6,20 @@ interface PaginationButtonProps {
   setCurrentPage: (page: number) => void;
 }
 
-const PaginationButtons: React.FC<PaginationButtonProps> = ({
-  currentPage,
-  totalPages,
-  setCurrentPage
-}) => {
+const PaginationButtons: React.FC<PaginationButtonProps> = ({ currentPage, totalPages, setCurrentPage }) => {
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
   return (
-    <div className="flex justify-center space-x-2 font-poppins font-semibold text-sm leading-5 text-center pt-1 mt-4">
+    <div className="flex justify-center space-x-2 font-poppins font-semibold text-sm leading-5 text-center py-10">
       {Array.from({ length: totalPages }, (_, index) => (
-        <div 
+        <div
           key={index + 1}
-          className={ `${currentPage === index + 1 ? "bg-blue-700" : "bg-white"} w-10 h-10 ${
-            currentPage === index + 1 ? "text-white" : "text-gray-800"
-          } rounded-lg shadow-md px-2 py-2.5 cursor-pointer`}  
+          className={`${
+            currentPage === index + 1 ? "bg-blue-700 text-white" : "bg-white text-gray-800"
+          } w-12 h-12 rounded-[9px] shadow-md flex items-center justify-center cursor-pointer`}
           onClick={() => handlePageChange(index + 1)}
         >
           {index + 1}
