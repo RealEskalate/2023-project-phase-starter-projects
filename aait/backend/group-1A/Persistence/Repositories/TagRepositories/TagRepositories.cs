b89c39ee37c9b4bbc250fe31c853add1c63af846
amanuel <amanuelyihunie27@gmail.com>
2023-08-28 23:26:07 +0300

    feat(AAiT-backend-1A): add tag feature

--- aait/backend/group-1A/Persistence/Repositories/TagRepositories/TagRepositories.cs
+++ aait/backend/group-1A/Persistence/Repositories/TagRepositories/TagRepositories.cs
@@ -21,30 +21,43 @@ public class TagReposiotry : ITagRepository
         throw new NotImplementedException();
     }
 
-    public Task<bool> Delete(Tag entity)
+
+    public Task<bool> Exists(Tag entity)
     {
-        //delete tag
-        _socialMediaDbContext.Tags.Remove(entity);
-        _socialMediaDbContext.SaveChanges();
-        return Task.FromResult(true);
+       var result = _socialMediaDbContext.Tags.Where(x => x.Id == entity.Id).Any();
+         return Task.FromResult(result);
     }
 
-    public Task<bool> Exists(int id)
+
+    public Task<Tag> Update(Tag entity)
+    {
+        _socialMediaDbContext.Tags.Update(entity);
+        _socialMediaDbContext.SaveChanges();
+        return Task.FromResult(entity);
+    }
+   public Task<List<Tag>>  GetAll(Expression<Func<Tag, bool>> predicate)
+    {
+        var result = _socialMediaDbContext.Tags.Where(predicate).ToList();
+        return Task.FromResult(result);
+    }
+
+    Task<List<Tag>> ITagRepository.GetAll(int userd)
+    {
+        var result = _socialMediaDbContext.Tags.ToList();
+        return Task.FromResult(result);
+    }
+
+    Task<bool> IGenericRepository<Tag>.Delete(Tag entit)
+    {
+         _socialMediaDbContext.Tags.Remove(entit);
+         _socialMediaDbContext.SaveChanges();
+
+        return Task.FromResult(true);        
+    }
+
+    Task<Tag?> ITagRepository.GetTagByName(string tag)
     {
         var result = _socialMediaDbContext.Tags.Where(x => x.Title == tag).FirstOrDefault();
         return Task.FromResult(result);
-    }
-
-    public Task<List<Tag>> GetAll()
-    {
-        var result = _socialMediaDbContext.Tags.ToList();
-        return Task.FromResult(result);
-    }
-
-    public Task<List<Tag>> GetAll(int userId)
-    {
-        var result = _socialMediaDbContext.Tags.ToList();
-        return Task.FromResult(result);
-        
     }
 }
