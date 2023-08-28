44db2824bdb12158de27f38f8285cac93d3f30fa
Yoseph Shemeles <142782245+yoseph-shemeles-a2sv@users.noreply.github.com>
2023-08-28 10:10:17 +0300

    Aastu.web.g1.yoseph.shemeles (#282)
    Co-authored-by: Yoseph Shemeles <jossyfresh2002@gmail.com>
    Co-authored-by: ruth-wossen-a2sv <142803496+ruth-wossen-a2sv@users.noreply.github.com>
    Co-authored-by: Ruth Wossen <ruthwossen75@gmail.com>

--- aastu/web/group-1/a2sv-blogs/src/components/blogs/BlogCard.tsx
+++ aastu/web/group-1/a2sv-blogs/src/components/blogs/BlogCard.tsx
@@ -56,87 +56,124 @@ export default function BlogCard() {
 
     return pageButtons;
   };
+  const LoadingSkeleton = () => {
+    return (
+      <>
+        {Array(3)
+          .fill(null)
+          .map((i) => (
+            <div className="w-full border-t border-[#D7D7D7] py-5">
+              <div className="h-[25%] w-full flex items-center gap-3 px-4">
+                <div className="w-14 h-14 rounded-full bg-gray-300"></div>
+                <div className="flex flex-col gap-0.5 ">
+                  <h1 className="font-montserrat font-semibold text-base leading-5 h-4 w-32 flex gap-0 bg-gray-300 rounded-md"></h1>
+                  <p className="font-montserrat font-semibold tracking-wide text-xs h-4 w-32  text-[#9c9c9c] bg-gray-300 rounded-md"></p>
+                </div>
+              </div>
+              <div className="h-[60%] flex w-full lg:flex-row flex-col items-center  px-4 lg:gap-14 gap-5 py-5 justify-between">
+                <div className="flex flex-col w-full gap-2">
+                  <h1 className="font-montserrat h-4 w-full text-xl font-black leading-9 text-black line-clamp-4 bg-gray-300 rounded-md"></h1>
+                  <p className="font-montserrat h-36 w-full text-base font-normal leading-7 text-[#737373] line-clamp-4 bg-gray-300 rounded-md"></p>
+                </div>
+                <div className="lg:w-72 w-full h-48 rounded-xl bg-gray-300"></div>
+              </div>
+              <div className="flex items-center bg-white">
+                {/* Placeholder for the tags */}
+                <ul className="h-[15%] flex items-center gap-10 px-3 justify-start">
+                  <li className="px-5 py-1.5 font-montserrat font-semibold text-sm text-[#8E8E8E] bg-[#ededf0] rounded-full flex bg-gray-300"></li>
+                </ul>
+              </div>
+            </div>
+          ))}
+      </>
+    );
+  };
   return (
     <>
-      {currentItems?.map((item) => (
-        <Link className="w-full" href={`/blogs/${item._id}`}>
-          <div className="w-full border-t border-[#D7D7D7] py-5">
-            <div className="h-[25%] flex items-center gap-3 px-4">
-              <Image
-                className="w-14 h-14 rounded-full object-cover"
-                src={item.image}
-                alt=""
-                width={100}
-                height={100}
-              />
-              <div className="flex flex-col gap-0.5">
-                <h1 className="font-montserrat font-semibold text-base leading-5 flex gap-0">
-                  {item.author?.name}
-                  <span className="flex items-center">
-                    <svg
-                      className="w-5 h-5 shrink-0"
-                      viewBox="0 0 25 25"
-                      fill="none"
-                      xmlns=""
-                    >
-                      <circle
-                        cx="12.5"
-                        cy="12.5"
-                        r="1.5"
-                        fill="#121923"
-                        stroke="#121923"
-                        strokeWidth="1.2"
-                      />
-                    </svg>
-                  </span>
-                  <span className="text-xs flex items-center text-[#868686]">
-                    {new Date(item.updatedAt).toLocaleDateString(
-                      "en-US",
-                      options
-                    )}
-                  </span>
-                </h1>
-                <p className="font-montserrat font-semibold tracking-wide text-xs text-[#9c9c9c]">
-                  {item.author?.role.toUpperCase()}
-                </p>
+      {isLoading ? (
+        <LoadingSkeleton />
+      ) : (
+        currentItems?.map((item) => (
+          <Link className="w-full" href={`/blogs/${item._id}`}>
+            <div className="w-full border-t border-[#D7D7D7] py-5">
+              <div className="h-[25%] flex items-center gap-3 px-4">
+                <Image
+                  className="w-14 h-14 rounded-full object-cover"
+                  src={item.image}
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+                <div className="flex flex-col gap-0.5">
+                  <h1 className="font-montserrat font-semibold text-base leading-5 flex gap-0">
+                    {item.author?.name}
+                    <span className="flex items-center">
+                      <svg
+                        className="w-5 h-5 shrink-0"
+                        viewBox="0 0 25 25"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <circle
+                          cx="12.5"
+                          cy="12.5"
+                          r="1.5"
+                          fill="#121923"
+                          stroke="#121923"
+                          strokeWidth="1.2"
+                        />
+                      </svg>
+                    </span>
+                    <span className="text-xs flex items-center text-[#868686]">
+                      {new Date(item.updatedAt).toLocaleDateString(
+                        "en-US",
+                        options
+                      )}
+                    </span>
+                  </h1>
+                  <p className="font-montserrat font-semibold tracking-wide text-xs text-[#9c9c9c]">
+                    {item.author?.role.toUpperCase()}
+                  </p>
+                </div>
+              </div>
+              <div className="h-[60%] flex  lg:flex-row flex-col items-center  px-4 lg:gap-14 gap-5 py-5 justify-between">
+                <div className="flex flex-col h-full gap-5 px-2">
+                  <h1 className="font-montserrat text-xl font-black  text-black line-clamp-4">
+                    {item.title}
+                  </h1>
+                  <p className="font-montserrat text-base font-normal leading-7 text-[#737373] line-clamp-4">
+                    <div
+                      dangerouslySetInnerHTML={{ __html: item.description }}
+                    />
+                  </p>
+                </div>
+                <Image
+                  src={item.image}
+                  alt=""
+                  width={300}
+                  height={200}
+                  className="lg:w-72 w-full h-48 rounded-xl object-cover"
+                />
+              </div>
+              <div className="flex items-center bg-white flex-wrap lg:gap-1 gap-3">
+                {item?.tags?.map(
+                  (tag, index) =>
+                    tag && (
+                      <ul
+                        className="h-[15%] flex items-center gap-10 px-3 justify-start"
+                        key={index}
+                      >
+                        <li className="px-5 py-1.5 font-montserrat font-semibold lg:text-sm text-xs text-[#8E8E8E] bg-[#ededf0] rounded-full flex">
+                          {tag}
+                        </li>
+                      </ul>
+                    )
+                )}
               </div>
             </div>
-            <div className="h-[60%] flex w- border border-black lg:flex-row flex-col items-center  px-4 lg:gap-14 gap-5 py-5 justify-between">
-              <div className="flex flex-col">
-                <h1 className="font-montserrat text-xl font-black leading-9 text-black line-clamp-4">
-                  {item.title}
-                </h1>
-                <p
-                  className="font-montserrat text-base font-normal leading-7 text-[#737373] line-clamp-4"
-                  dangerouslySetInnerHTML={{ __html: item.description }}
-                ></p>
-              </div>
-              <Image
-                src={item.image}
-                alt=""
-                width={300}
-                height={200}
-                className="lg:w-72 w-full h-48 rounded-xl object-cover"
-              />
-            </div>
-            <div className="flex items-center bg-white">
-              {item?.tags?.map(
-                (tag, index) =>
-                  tag && (
-                    <ul
-                      className="h-[15%] flex items-center gap-10 px-3 justify-start"
-                      key={index}
-                    >
-                      <li className="px-5 py-1.5 font-montserrat font-semibold text-sm text-[#8E8E8E] bg-[#ededf0] rounded-full flex">
-                        {tag}
-                      </li>
-                    </ul>
-                  )
-              )}
-            </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))
+      )}
 
       <div className="flex justify-center mb-10">
         <div className="flex gap-5">{renderPageButtons()}</div>
