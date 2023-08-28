7985499c94459847630ce1181b6ac1bc99014cb3
abi26anamo <abianamo282@gmail.com>
2023-08-28 21:24:26 +0300

    fixed artile page

--- aait/mobile/group-3/blog_app/lib/features/article/presentation/screen/write_aricle_page.dart
+++ aait/mobile/group-3/blog_app/lib/features/article/presentation/screen/write_aricle_page.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../../core/color/colors.dart';
+import '../../../../core/util/image_sheet.dart';
 import '../widgets/widgets.dart';
 
-class WriteArticlePage extends StatelessWidget {
+class WriteArticlePage extends StatefulWidget {
   const WriteArticlePage({super.key});
 
+  @override
+  State<WriteArticlePage> createState() => _WriteArticlePageState();
+}
+
+class _WriteArticlePageState extends State<WriteArticlePage> {
+  XFile? _file;
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -29,19 +37,44 @@ class WriteArticlePage extends StatelessWidget {
                     children: [
                       const CustomTextField(hint: "Add title"),
                       const CustomTextField(hint: "add subtitle"),
-                      const AddTagsTextField(),
+                      AddTagsContainer(),
                       SizedBox(height: 4.39.h),
-                      Text(
-                        "add as many tags as you want",
-                        style: GoogleFonts.poppins(
-                          fontSize: 11.sp,
-                          fontWeight: FontWeight.w200,
-                          color: hintTextColor,
-                        ),
-                      ),
                       SizedBox(height: 49.h),
                       const ArticleContentTextField(),
-                      SizedBox(height: 87.h),
+                      SizedBox(height: 40.h),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Upload Image",
+                              style: GoogleFonts.poppins(
+                                  fontSize: 16.sp,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black)),
+                          GestureDetector(
+                            onTap: () {
+                              setState(() async {
+                                _file = await ImageSheet().show(context);
+                              });
+                            },
+                            child: Icon(
+                              Icons.camera_alt_outlined,
+                              color: Colors.blue,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      Container(
+                        child: TextField(
+                          controller: TextEditingController(text: _file?.path),
+                          decoration: InputDecoration(
+                              hintText: _file != null
+                                  ? _file?.path
+                                  : "No Image is selected"),
+                        ),
+                      ),
                       const FormSubmitButton()
                     ],
                   ),
@@ -52,11 +85,3 @@ class WriteArticlePage extends StatelessWidget {
         });
   }
 }
-
-
-
-
-
-
-
-
