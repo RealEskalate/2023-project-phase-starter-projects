6f46a170009535c36bf2a8fc859cdd3d7a66ea1d
NaolAklilu <90989814+NaolAklilu@users.noreply.github.com>
2023-08-28 17:18:29 +0300

    feat(AAiT-mobile-2): add on boarding screen (#276)

--- aait/mobile/group-2/blog_app/lib/core/presentation/theme/app_colors.dart
+++ aait/mobile/group-2/blog_app/lib/core/presentation/theme/app_colors.dart
@@ -4,12 +4,14 @@ abstract class AppColors {
   static const white = Color(0xFFFFFFFF);
   static const black = Color(0xFF000000);
 
-  static const lightBlue = Color(0xFF669AFF);
   static const blue = Color(0xFF376AED);
   static const translucentBlue = Color(0x26376AED);
+  static const blue100 = Color(0xFFBBDEFB);
+  static const blue300 = Color(0xFF2D4379);
   static const saturatedBlue = Color(0xFF2151CD);
   static const darkBlue = Color(0xFF2D4379);
   static const darkerBlue = Color(0xFF0D253C);
+  static const lightBlue = Color(0xFF669AFF);
 
   static const darkGray = Color(0xFF7B8BB2);
 
@@ -18,6 +20,7 @@ abstract class AppColors {
   static const gray300 = Color(0xFF979EA5);
   static const gray400 = Color(0xFF878585);
   static const gray500 = Color(0x427B8BB2);
+  static const grayLight = Color(0xFFC6D6DF);
 
   static const red = Color(0xFFCC3926);
 }
