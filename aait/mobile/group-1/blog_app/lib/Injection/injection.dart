7d09bd6f257b92bb7fde65823e9fd6d45a41d936
unknown <henokmekuanint79@gmail.com>
2023-08-28 21:58:27 +0300

    added

--- aait/mobile/group-1/blog_app/lib/Injection/injection.dart
+++ aait/mobile/group-1/blog_app/lib/Injection/injection.dart
@@ -7,21 +7,3 @@ import 'package:get_it/get_it.dart';
 //   // await userProfile(sl);
 //   await authentication(sl);
 // }
-
-
-// import 'package:get_it/get_it.dart';
-
-// import 'package:hakim_hub_mobile/injection/search_hospitals_injection.dart';
-
-// import 'chat_bot_injection.dart';
-// import 'doctor_detail_injection.dart';
-// import 'hospital_detail_injection.dart';
-
-// final sl = GetIt.instance;
-
-// Future<void> init() async {
-//   await searchHospitalsinit(sl);
-//   await doctorDetailInit(sl);
-//   await hospitalDetailsinit(sl);
-//   await chatBotInit(sl);
-// }
