781a32aded2b6ab8e6d8cc03b487beb576cf6106
Geleta116 <geleta.dabakumsa@gmail.com>
2023-08-29 12:01:46 +0300

    fixed the api response

--- aait/backend/group-2B/WebApi/Controllers/InteractionsController.cs
+++ aait/backend/group-2B/WebApi/Controllers/InteractionsController.cs
@@ -1,11 +1,14 @@
 using SocialSync.Application.Features.Interactions.Requests.Commands;
 using SocialSync.Application.DTOs.InteractionDTOs;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SocialSync.Application.Features.Comments.Requests.Commands;
 using SocialSync.Application.DTOs.InteractionDTOs.CommentDTOs;
 using SocialSync.Application.Features.Comments.Requests.Queries;
 using SocialSync.Domain.Entities;
+using SocialSync.WebApi.Services;
+using SocialSync.WebApi.Services.Interfaces;
 
 namespace SocialSync.WebApi.Controllers;
 
@@ -14,25 +17,28 @@ namespace SocialSync.WebApi.Controllers;
 public class InteractionController : ControllerBase
 {
     private readonly IMediator _mediator;
+    private readonly IUserService _userService;
 
-    public InteractionController(IMediator mediator)
+    public InteractionController(IMediator mediator, IUserService userService)
     {
         _mediator = mediator;
+        _userService = userService;
     }
 
-
+    [Authorize]
     [HttpPost("like")]
     public async Task<IActionResult> LikeUnlikePostAsync([FromBody] InteractionDto likeDto)
     {
         likeDto.Type = InteractionType.Like;
         likeDto.Body = null;
+        likeDto.UserId = int.Parse(_userService.GetUserId());
         var command = new LikeUnlikePostInteractionCommand { LikeDto = likeDto };
 
         var response = await _mediator.Send(command);
 
         if (response.IsSuccess)
         {
-            return Ok(response.Value);
+            return Ok(response);
         }
         else
         {
@@ -40,17 +46,18 @@ public class InteractionController : ControllerBase
         }
     }
 
-
+    [Authorize]
     [HttpPost("comment")]
     public async Task<IActionResult> CommentOnPostAsync([FromBody] InteractionDto interactionDto)
     {
         interactionDto.Type = InteractionType.Comment;
+        interactionDto.UserId = int.Parse(_userService.GetUserId());
         var command = new CreateCommentInteractionCommand { CreateCommentDto = interactionDto };
         var response = await _mediator.Send(command);
 
         if (response.IsSuccess)
         {
-            return Ok(response.Value);
+            return CreatedAtAction(nameof(GetCommentOfAPost), new { id = response.Value }, response);
         }
         else
         {
@@ -58,17 +65,18 @@ public class InteractionController : ControllerBase
         }
     }
 
-
+    [Authorize]
     [HttpPut("comment")]
     public async Task<IActionResult> UpdateCommentOfAPostAsync(
         [FromBody] UpdateCommentInteractionDto interactionDto
     )
     {
+        interactionDto.UserId = int.Parse(_userService.GetUserId());
         var command = new UpdateCommentInteractionCommand { UpdateCommentDto = interactionDto };
         var response = await _mediator.Send(command);
         if (response.IsSuccess)
         {
-            return Ok(response.Value);
+            return Ok(response);
         }
         else
         {
@@ -76,18 +84,19 @@ public class InteractionController : ControllerBase
         }
     }
 
-
+    [Authorize]
     [HttpDelete("comment")]
     public async Task<IActionResult> DeleteCommentOfAPost(
-        [FromBody] DeleteCommentInteractionDto interactionId
+        [FromBody] DeleteCommentInteractionDto interactionDto
     )
     {
+        interactionDto.UserId = int.Parse(_userService.GetUserId());
         var command =
-            new DeleteCommentInteractionCommand { DeleteCommentInteractionDto = interactionId };
+            new DeleteCommentInteractionCommand { DeleteCommentInteractionDto = interactionDto };
         var response = await _mediator.Send(command);
         if (response.IsSuccess)
         {
-            return Ok(response.Value);
+            return Ok(response);
         }
         else
         {
@@ -102,7 +111,7 @@ public class InteractionController : ControllerBase
         var response = await _mediator.Send(command);
         if (response.IsSuccess)
         {
-            return Ok(response.Value);
+            return Ok(response);
         }
         else
         {
@@ -118,7 +127,7 @@ public class InteractionController : ControllerBase
         var response = await _mediator.Send(command);
         if (response.IsSuccess)
         {
-            return Ok(response.Value);
+            return Ok(response);
         }
         else
         {
