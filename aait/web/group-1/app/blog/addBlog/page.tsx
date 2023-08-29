a900a6164a41c33f6f2e4b3e517a0fd3de09347a
Admas Terefe <93878882+Son-OfAnton@users.noreply.github.com>
2023-08-29 14:46:44 +0300

    Aait.web.g1.admas terefe.add blog (#379)

---
+++ aait/web/group-1/app/blog/addBlog/page.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { useAddBlogMutation } from "@/store/blog/blogApi";
+import { useState } from "react";
+import AddBlog from "@/components/blog/AddBlog";
+
+const CreateBlogPage: React.FC = () => {
+    const [addBlog, {isError, isLoading}] = useAddBlogMutation()
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState<File | null>(null);
+    const [content, setContent] = useState("");
+    const [imageText, setImageText] = useState('Please upload image');
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+    const handleTagClick = (tag: string) => {
+      if (selectedTags.includes(tag)) {
+        setSelectedTags(selectedTags.filter((t) => t !== tag));
+      } else {
+        setSelectedTags([...selectedTags, tag]);
+      }
+    };
+
+    const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+          setTitle(event.target.value);
+      };
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+            const file = event.target.files[0]
+            if (file) {
+                setImageText(file.name);
+            } else {
+                setImageText('Please upload image');
+            }
+        setImage(event.target.files[0]);
+        }
+    };
+
+      const handleCancel = (e: React.FormEvent) => {
+        e.preventDefault()
+        setImage(null)
+        setImageText('Please upload image')
+        setContent('')
+        setTitle('')
+        setSelectedTags([])
+
+      }
+
+      const handleDeleteImage = () => {
+          setImageText('Please upload image');
+          setImage(null);
+      };
+      const handleContentChange = (newContent:any) => {
+          setContent(newContent);
+      };
+
+      const handleSubmit = async (event: React.FormEvent) => {
+          event.preventDefault();
+          const formData = new FormData();
+          formData.append("title", title);
+          formData.append("description", content);
+          selectedTags.forEach(tag => formData.append("tags", tag));
+          if (image) {
+            formData.append("image", image);
+          }
+          addBlog(formData)         
+      };
+
+    return (
+     <AddBlog
+      title={title}
+      content={content}  
+      image={image} 
+      imageText={imageText} 
+      selectedTags={selectedTags} 
+      handleCancel={handleCancel}
+      handleContentChange={handleContentChange}
+      handleDeleteImage={handleDeleteImage}
+      handleImageChange={handleImageChange}
+      handleSubmit={handleSubmit}
+      handleTagClick={handleTagClick}
+      handleTitleChange={handleTitleChange}
+      />
+    );
+};
+
+export default CreateBlogPage;
---
+++ aait/web/group-1/store/blog/blogApi.ts
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { AddBlogResponse } from '@/types/blog/addBlog/AddBlogResponse'
+
+
+const BASE_URL = 'https://a2sv-backend.onrender.com/api'
+
+export const blogApi = createApi({
+  reducerPath: 'blogApi',
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: BASE_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const userData = localStorage.getItem('user')
+      const token = userData ? JSON.parse(userData)?.token : null
+      if(token) {
+        headers.set('Authorization', `Bearer ${token}`)
+      }
+      return headers
+    }
+  }),
+  endpoints: (builder) => ({
+    addBlog: builder.mutation<AddBlogResponse, FormData>({
+      query: (data) => ({
+        url: '/blogs',
+        method: 'POST',
+        body: data,
+      })
+    }),
+  })
+})
+
+export const { useAddBlogMutation } = blogApi
+
