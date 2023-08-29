e6b56ae2f4643fda5091319ed78a5c260c7a414f
Biruk Kassaw <se.biruk.kassaw@gmail.com>
2023-08-29 10:16:42 +0300

    adding blog page

---
+++ aait/web/group-2/app/blogs/create/page.tsx
@@ -0,0 +1,107 @@
+'use client'
+
+import { useState } from "react";
+import {useRouter} from 'next/navigation'
+// import { useAddBlogMutation } from "@/store/features/create-blog/create-blog-api";
+import AddBlog from "@/components/blog/AddBlog";
+
+const CreateBlogPage: React.FC = () => {
+    // const [addBlog, {isError, isLoading}] = useAddBlogMutation()
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState<File | null>(null);
+    const [content, setContent] = useState("");
+    const [imageText, setImageText] = useState('Please upload image');
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+    const router = useRouter()
+
+    const handleTagClick = (tag: string) => {
+      if (selectedTags.includes(tag)) {
+        setSelectedTags(selectedTags.filter((t) => t !== tag));
+      } else {
+        setSelectedTags([...selectedTags, tag]);
+      }
+    };
+
+    const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+          setTitle(event.target.value);
+      };
+
+    const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+            const file = event.target.files[0]
+            if (file) {
+                setImageText(file.name);
+            } else {
+                setImageText('Please upload image');
+            }
+        setImage(event.target.files[0]);
+        }
+    };
+
+      const handleCancel = (e: React.FormEvent) => {
+        e.preventDefault()
+        setImage(null)
+        setImageText('Please upload image')
+        setContent('')
+        setTitle('')
+        setSelectedTags([])
+
+      }
+
+      const handleDeleteImage = () => {
+          setImageText('Please upload image');
+          setImage(null);
+      };
+      const handleContentChange = (newContent:any) => {
+          setContent(newContent);
+      };
+
+      const handleSubmit = async (event: React.FormEvent) => {
+          event.preventDefault();
+          const formData = new FormData();
+          formData.append("title", title);
+          formData.append("description", content);
+          selectedTags.forEach(tag => formData.append("tags", tag));
+          if (image) {
+            formData.append("image", image as Blob);
+          }
+          console.log(title, content,selectedTags);
+      //     try {
+            
+      //       const response = await addBlog(formData);
+      //       if (response) {
+      //           router.push('/blogs')
+      //       }else{
+      //         setImage(null)
+      //         setImageText('Please upload image')
+      //         setContent('')
+      //         setTitle('')
+      //         setSelectedTags([])
+              
+      //       }
+
+      //     } catch (error) {
+            
+      //     }
+         
+      };
+
+    return (
+     <AddBlog
+      title={title}
+      content={content}  
+      image={image} 
+      imageText={imageText} 
+      selectedTags={selectedTags} 
+      handleCancel={handleCancel}
+      handleContentChange={handleContentChange}
+      handleDeleteImage={handleDeleteImage}
+      handleImageChange={handleImageChange}
+      handleSubmit={handleSubmit}
+      handleTagClick={handleTagClick}
+      handleTitleChange={handleTitleChange}
+      />
+    );
+};
+
+export default CreateBlogPage;
--- aait/web/group-2/components/Header.tsx
+++ aait/web/group-2/components/Header.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useState } from 'react';
+import Link from 'next/link';
+
 interface HeaderProps {
   title?: string;
 }
@@ -14,7 +16,7 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
       
   return (
     <>
-    <nav className="bg-white fixed top-0 left-0 right-0">
+    <nav className="bg-white fixed top-0 left-0 right-0 z-50">
 			<div className="max-w-8xl mx-auto px-4">
 				<div className="flex justify-between">
 					<div className="flex items-start space-x-7">
@@ -26,10 +28,10 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
 					</div>
                     <div className="hidden lg:flex justify-between items-center space-x-1 mx-auto">
 							<a href="" className="py-4 px-2 mr-5 text-blue-700 border-b-4 border-blue-700 font-semibold ">Home</a>
-							<a href="" className="py-4 px-2 mr-5 text-gray-500 font-semibold hover:text-blue-700 transition duration-300">Teams</a>
+							<Link href="/teams" className="py-4 px-2 mr-5 text-gray-500 font-semibold hover:text-blue-700 transition duration-300">Teams</Link>
 							<a href="" className="py-4 px-2 mr-5 text-gray-500 font-semibold hover:text-blue-700 transition duration-300">Success Stories</a>
 							<a href="" className="py-4 px-2 mr-5 text-gray-500 font-semibold hover:text-blue-700 transition duration-300">About Us</a>
-							<a href="" className="py-4 px-2 mr-5 text-gray-500 font-semibold hover:text-blue-700 transition duration-300">Blogs</a>
+							<Link href="/blogs" className="py-4 px-2 mr-5 text-gray-500 font-semibold hover:text-blue-700 transition duration-300">Blogs</Link>
 							<a href="" className="py-4 px-2 mr-5 text-gray-500 font-semibold hover:text-blue-700 transition duration-300">Get Involved</a>
 					</div>
 					<div className="hidden lg:flex items-center space-x-3 ">
