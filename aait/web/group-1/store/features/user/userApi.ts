075bdf506f31ef268c9adef981965853a499a294
Son-OfAnton <admasterefe00@gmail.com>
2023-08-30 02:53:43 +0300

    feat(AAiT.web.1): Implemented personal info edit

---
+++ aait/web/group-1/store/features/user/userApi.ts
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const BASE_URL = 'https://a2sv-backend.onrender.com/api';
+
+export const profileApi = createApi({
+  reducerPath: 'profileApi',
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: BASE_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const userData = localStorage.getItem('user');
+      const token = userData ? JSON.parse(userData)?.token : null;
+      if(token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    }
+  }),
+  endpoints: (builder) => ({
+    editProfile: builder.mutation({
+      query: (newUserData) => ({
+        url: '/auth/edit-profile',
+        method: 'PATCH',
+        body: newUserData,
+      }),
+    }),
+  }),
+});
+
+export const { useEditProfileMutation } = profileApi;
--- aait/web/group-1/store/index.ts
+++ aait/web/group-1/store/index.ts
@@ -1,17 +1,18 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import { authApi } from './auth/authApi'
+import { profileApi } from './features/user/userApi'
 import { teamMembersApi } from "./features/team-members/team-members";
 import { blogApi } from './blog/blogApi';
-
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [blogApi.reducerPath]: blogApi.reducer,
+    [profileApi.reducerPath]: profileApi.reducer,
     [teamMembersApi.reducerPath]: teamMembersApi.reducer,
   },
-
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(authApi.middleware, blogApi.middleware, teamMembersApi.middleware)
+  middleware: getDefaultMiddleware => getDefaultMiddleware()
+  .concat(authApi.middleware, blogApi.middleware, profileApi.middleware, teamMembersApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
