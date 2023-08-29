0b1178038c19e46ff828724c6bdb4bf28b53cc16
mintesnot-markos <mintesnot.markos@a2sv.org>
2023-08-29 06:24:00 +0300

    Fix(aastu-web-g2): Make add blog responsive

--- aastu/web/group-2/app/components/add-blog/TextEditor.tsx
+++ aastu/web/group-2/app/components/add-blog/TextEditor.tsx
@@ -5,11 +5,15 @@ import 'react-quill/dist/quill.snow.css';
 import Image from 'next/image';
 import file_icon from '@/assets/images/file_icon.png';
 
+const handleImgUpload = (e: any) => {
+  document.getElementById('img_upload')?.click();
+};
+
 const CustomToolbar = () => (
   <div id="toolbar" className="flex justify-evenly !border-t !border-b !border-[#CFD6DE] py-3">
     <div className="flex items-center w-full">
       <div className="border-r pr-2 border-[#CFD6DE] ">
-        <div className="bg-[#63A2D8] rounded w-6 h-6" />
+        <div className="bg-[#63A2D8] rounded w-6 h-6 hover:cursor-pointer" />
       </div>
       <div className="format-group border-r border-[#CFD6DE] flex justify-center w-1/8 px-2">
         <button className="ql-bold" />
@@ -19,18 +23,27 @@ const CustomToolbar = () => (
       <div className="border-r border-[#CFD6DE] flex justify-center w-1/8 px-2">
         <button className="ql-list" value="bullet" />
         <select className="ql-align"></select>
+        <select className="ql-header"></select>
       </div>
       <div className="border-r border-[#CFD6DE] flex justify-center w-1/8 px-2">
         <button className="ql-link" />
       </div>
     </div>
     <div className="col-span-3 flex w-full justify-end sm:w-1/4 px-2">
-      <div>
-        <button className="ql-video" />
-        <button className="ql-image" />
+      <div className="hidden md:block">
+        <button className="ql-video " />
+        <button className="ql-image" id="img_upload" />
         <button className="w-2">
           <Image src={file_icon} alt="" />
         </button>
+      </div>
+      <div className="px-1 py-2 mr-2">
+        <div
+          className="block md:hidden font-extrabold hover:cursor-pointer"
+          onClick={handleImgUpload}
+        >
+          +
+        </div>
       </div>
     </div>
   </div>
@@ -69,9 +82,6 @@ export default function TextEditor({
   setBlogContent: (value: string) => void;
   blogContent: string;
 }) {
-  useEffect(() => {
-    console.log(blogContent);
-  }, [blogContent]);
   return (
     <div>
       <div className="container mx-auto md:mb-8">
