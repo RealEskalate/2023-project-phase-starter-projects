5dbd8a9d1f0182f782897905e5fb439d770eb9f7
primequantuM4 <gediezra@gmail.com>
2023-08-29 04:33:32 +0300

    fix(AAiT-mobile-3): Fix Onboarding for integration

--- aait/mobile/group-3/blog_app/lib/features/article/presentation/screen/write_aricle_page.dart
+++ aait/mobile/group-3/blog_app/lib/features/article/presentation/screen/write_aricle_page.dart
@@ -2,20 +2,28 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../../core/color/colors.dart';
+import '../../../../core/util/image_sheet.dart';
 import '../widgets/widgets.dart';
 
-class WriteArticlePage extends StatelessWidget {
+class WriteArticlePage extends StatefulWidget {
   const WriteArticlePage({super.key});
 
+  @override
+  State<WriteArticlePage> createState() => _WriteArticlePageState();
+}
+
+class _WriteArticlePageState extends State<WriteArticlePage> {
+  XFile? _file;
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
         designSize: const Size(428, 926),
         builder: (BuildContext context, child) {
           return Scaffold(
-            backgroundColor: Colors.white,
+            backgroundColor: whiteColor,
             appBar: PreferredSize(
               preferredSize: Size.fromHeight(74.h),
               child: const CustomAppBarNewArticle(),
@@ -29,19 +37,44 @@ class WriteArticlePage extends StatelessWidget {
                     children: [
                       const CustomTextField(hint: "Add title"),
                       const CustomTextField(hint: "add subtitle"),
-                      const AddTagsTextField(),
+                      AddTagsContainer(),
                       SizedBox(height: 4.39.h),
-                      Text(
-                        "add as many tags as you want",
-                        style: GoogleFonts.poppins(
-                          fontSize: 11.sp,
-                          fontWeight: FontWeight.w200,
-                          color: hintTextColor,
-                        ),
-                      ),
                       SizedBox(height: 49.h),
                       const ArticleContentTextField(),
-                      SizedBox(height: 87.h),
+                      SizedBox(height: 40.h),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Upload Image",
+                              style: GoogleFonts.poppins(
+                                  fontSize: 16.sp,
+                                  fontWeight: FontWeight.w500,
+                                  color: darkBlue)),
+                          GestureDetector(
+                            onTap: () {
+                              setState(() async {
+                                _file = await ImageSheet().show(context);
+                              });
+                            },
+                            child: Icon(
+                              Icons.camera_alt_outlined,
+                              color: blue,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      Container(
+                        child: TextField(
+                          controller: TextEditingController(text: _file?.path),
+                          decoration: InputDecoration(
+                              hintText: _file != null
+                                  ? _file?.path
+                                  : "No Image is selected"),
+                        ),
+                      ),
                       const FormSubmitButton()
                     ],
                   ),
@@ -52,11 +85,3 @@ class WriteArticlePage extends StatelessWidget {
         });
   }
 }
-
-
-
-
-
-
-
-
--- aait/mobile/group-3/blog_app/lib/features/article/presentation/widgets/add_tags_text_field.dart
+++ aait/mobile/group-3/blog_app/lib/features/article/presentation/widgets/add_tags_text_field.dart
@@ -1,32 +1,126 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-import '../../../../core/color/colors.dart';
+class AddTagsContainer extends StatefulWidget {
+  @override
+  _AddTagsContainerState createState() => _AddTagsContainerState();
+}
 
-import 'custom_text_field.dart';
+class _AddTagsContainerState extends State<AddTagsContainer> {
+  List<String> availableTags = [
+    'Tag 1',
+    'Tag 2',
+    'Tag 3',
+    'Tag 4',
+    // Add more tags as needed
+  ];
 
-class AddTagsTextField extends StatelessWidget {
-  const AddTagsTextField({
-    super.key,
-  });
+  List<String> selectedTags = [];
+
+  void toggleTag(String tag) {
+    setState(() {
+      if (selectedTags.contains(tag)) {
+        selectedTags.remove(tag);
+      } else {
+        selectedTags.add(tag);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Expanded(
-            child: CustomTextField(hint: "Add tags")),
-        SizedBox(width: 27.w),
-        IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.add_circle_outline,
-            size: 36.h,
-            color: blue,
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Add Tags:'),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  "add as many tags as you want",
+                  style: GoogleFonts.poppins(
+                    fontSize: 11.sp,
+                    fontWeight: FontWeight.w200,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.add_circle_outline,
+                color: Colors.blue,
+              ),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Select Tags'),
+                      content: Container(
+                        width: double.maxFinite,
+                        child: Wrap(
+                          spacing: 8.0,
+                          children: availableTags.map((tag) {
+                            return GestureDetector(
+                              onTap: () {
+                                toggleTag(tag);
+                                Navigator.pop(context);
+                              },
+                              child: RawChip(
+                                label: Text(
+                                  tag,
+                                  style: TextStyle(
+                                      color: selectedTags.contains(tag)
+                                          ? Colors.white
+                                          : Colors.blue),
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  side: BorderSide(color: Colors.blue),
+                                ),
+                                backgroundColor: selectedTags.contains(tag)
+                                    ? Colors.blue
+                                    : Colors.white,
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ],
         ),
+        Wrap(
+          spacing: 8.0,
+          children: selectedTags.map((tag) {
+            return RawChip(
+              label: Text(
+                tag,
+                style: TextStyle(color: Colors.blue),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+                side: BorderSide(color: Colors.blue),
+              ),
+              onDeleted: () {
+                toggleTag(tag);
+              },
+              deleteIconColor: Colors.lightBlue,
+            );
+          }).toList(),
+        ),
+        SizedBox(height: 16.0),
       ],
     );
   }
