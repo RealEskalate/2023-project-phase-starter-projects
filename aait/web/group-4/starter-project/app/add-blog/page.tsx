da3ff39ab576cf8d3eb960346f96aaa1f27f861e
ante-neh <amareanteneh12@gmail.com>
2023-08-29 18:58:44 +0300

    fix reviews

---
+++ aait/web/group-4/starter-project/app/add-blog/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { store } from "@/store/store"
+import { Provider } from "react-redux"
+import {ApiProvider} from '@reduxjs/toolkit/query/react'
+import BlogForm from "@/components/blog/AddBlogForm"
+import { blogApi } from "@/store/blog/blogsApi"
+
+export default function Home() {
+  return (
+    <main>
+     <Provider store={store}>
+      <ApiProvider api={blogApi}>
+        <BlogForm/>
+      </ApiProvider>
+     </Provider>
+    </main>)
+}
