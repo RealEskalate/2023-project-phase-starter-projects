1803c69638fb4822075533db8ffbd34f35ef8ff2
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-08-29 22:04:46 +0300

    fix(AAiT-web-1): Fix font of teams page

--- aait/web/group-1/app/teams/page.tsx
+++ aait/web/group-1/app/teams/page.tsx
@@ -11,18 +11,18 @@ const page = () => {
   const { data: members, isLoading, error } = useGetTeamMembersQuery();
 
   return (
-    <div className="py-20">
+    <div className="py-20 font-poppins">
       {isLoading && <h1 className="text-center">Loading...</h1>}
       {!isLoading && !error && <div>
         {/* upper hero section */}
         <div className="flex items-center">
           {/*team text section */}
           <div className="w-1/2 ml-16 flex flex-col gap-6 px-10">
-            <div className="text-6xl font-extrabold uppercase">
+            <div className="text-6xl font-bold uppercase">
               The team we’re
             </div>
-            <div className="text-6xl font-extrabold uppercase">currently</div>
-            <div className="text-6xl font-extrabold uppercase">
+            <div className="text-6xl font-bold uppercase">currently</div>
+            <div className="text-6xl font-bold uppercase">
               working with
             </div>
             <p className="mt-10 text-xl text-gray-400">
