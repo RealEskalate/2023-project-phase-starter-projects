1c0d9baaa72d13aedba4b549d1eb2cbf38372475
Wubshet Zeleke Genta <wubezeleke@gmail.com>
2023-08-29 15:09:02 +0300

    fix(AASTU-web-g2): fix add blog page (#397)

--- aastu/web/group-2/app/components/add-blog/TextEditor.tsx
+++ aastu/web/group-2/app/components/add-blog/TextEditor.tsx
@@ -2,19 +2,25 @@
 import { useEffect } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import Image from 'next/image';
+import file_icon from '@/assets/images/file_icon.png';
+
+const handleImgUpload = (e: any) => {
+  document.getElementById('img_upload')?.click();
+};
 
 const CustomToolbar = () => (
   <div id="toolbar" className="flex justify-evenly !border-t !border-b !border-[#CFD6DE] py-3">
     <div className="flex items-center w-full">
       <div className="border-r pr-2 border-[#CFD6DE] ">
-        <div className="bg-[#63A2D8] rounded w-6 h-6" />
+        <div className="bg-[#63A2D8] rounded w-6 h-6 hover:cursor-pointer" />
       </div>
-      <div className="format-group border-r border-[#CFD6DE] flex justify-center sm:w-1/4 px-2">
-        <button className="ql-bold font-bold ml-4" />
+      <div className="format-group border-r border-[#CFD6DE] flex justify-center w-1/8 px-2">
+        <button className="ql-bold" />
         <button className="ql-italic" />
-        <button className="ql-underline mr-4" />
+        <button className="ql-underline" />
       </div>
-      <div className="border-r border-[#CFD6DE] flex sm:w-1/4 px-2">
+      <div className="border-r border-[#CFD6DE] flex justify-center w-1/8 px-2">
         <button className="ql-list" value="bullet" />
         <select className="ql-align"></select>
       </div>
@@ -23,10 +29,20 @@ const CustomToolbar = () => (
       </div>
     </div>
     <div className="col-span-3 flex w-full justify-end sm:w-1/4 px-2">
-      <div>
-        <button className="ql-video" />
-        <button className="ql-image" />
-        <button>#</button>
+    <div className="hidden md:block">
+        <button className="ql-video " />
+        <button className="ql-image" id="img_upload" />
+        <button className="w-2">
+          <Image src={file_icon} alt="" />
+        </button>
+      </div>
+      <div className="px-1 py-2 mr-2">
+        <div
+          className="block md:hidden font-extrabold hover:cursor-pointer"
+          onClick={handleImgUpload}
+        >
+          +
+        </div>
       </div>
     </div>
   </div>
