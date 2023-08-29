f5cd9d92a870d7ab4a8844080679d884fb01cb9c
ante-neh <amareanteneh12@gmail.com>
2023-08-29 09:43:27 +0300

    feat(aait.web.g4): impact-stories

---
+++ aait/web/group-4/starter-project/components/story/StoryHead.tsx
@@ -0,0 +1,11 @@
+export const StoryHead = () => {
+  return (
+    <div className="flex flex-col items-center w-1164 h-236">
+              <h1 className="font-semibold text-5xl leading-[78px] font-poppins">Impact Stories</h1>
+              <p className="font-poppins font-normal text-3xl leading-[54px] text-#2E374E">Behind every success is a story. Learn about the stories of </p>
+              <p className="font-poppins font-normal text-3xl leading-[54px] text-#2E374E">A2SVians</p>
+              <hr className='w-12 h-0 border-2 border-solid border-blue-500 mx-auto top-150'/>
+     </div>
+  )
+}
+
