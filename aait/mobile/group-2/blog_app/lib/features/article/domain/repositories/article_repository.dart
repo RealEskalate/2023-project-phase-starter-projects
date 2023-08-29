ec77089f4f4342d18ac029a804f51a5dd3246256
Leul Wujira <meraf000@gmail.com>
2023-08-29 19:51:48 +0300

    feat(AAiT-mobile-2): Add Bookmark to Article Repository

--- aait/mobile/group-2/blog_app/lib/features/article/domain/repositories/article_repository.dart
+++ aait/mobile/group-2/blog_app/lib/features/article/domain/repositories/article_repository.dart
@@ -4,7 +4,6 @@ import '../../../../core/error/failure.dart';
 import '../entities/article.dart';
 import '../entities/tag.dart';
 
-
 abstract class ArticleRepository {
   Future<Either<Failure, List<Article>>> getAllArticles();
   Future<Either<Failure, List<Tag>>> getTags();
@@ -13,4 +12,8 @@ abstract class ArticleRepository {
   Future<Either<Failure, Article>> updateArticle(Article article);
   Future<Either<Failure, Article>> deleteArticle(String id);
   Future<Either<Failure, List<Article>>> filterArticles(Tag tag, String title);
+
+  Future<Either<Failure, List<Article>>> getBookmarkedArticles();
+  Future<Either<Failure, Article>> addToBookmark(Article article);
+  Future<Either<Failure, Article>> removeFromBookmark(String articleId);
 }
