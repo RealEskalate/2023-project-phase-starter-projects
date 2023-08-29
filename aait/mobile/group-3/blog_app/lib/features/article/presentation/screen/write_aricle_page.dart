f903543a73e00108e13790b8c43faab96bc4b354
primequantuM4 <86236449+primequantuM4@users.noreply.github.com>
2023-08-29 03:26:35 +0300

    Revert "fix(AAiT-mobile-3): create Article page" (#368)

--- aait/mobile/group-3/blog_app/lib/features/article/presentation/screen/write_aricle_page.dart
+++ aait/mobile/group-3/blog_app/lib/features/article/presentation/screen/write_aricle_page.dart
@@ -2,21 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:image_picker/image_picker.dart';
 
 import '../../../../core/color/colors.dart';
-import '../../../../core/util/image_sheet.dart';
 import '../widgets/widgets.dart';
 
-class WriteArticlePage extends StatefulWidget {
+class WriteArticlePage extends StatelessWidget {
   const WriteArticlePage({super.key});
 
-  @override
-  State<WriteArticlePage> createState() => _WriteArticlePageState();
-}
-
-class _WriteArticlePageState extends State<WriteArticlePage> {
-  XFile? _file;
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -37,44 +29,19 @@ class _WriteArticlePageState extends State<WriteArticlePage> {
                     children: [
                       const CustomTextField(hint: "Add title"),
                       const CustomTextField(hint: "add subtitle"),
-                      AddTagsContainer(),
+                      const AddTagsTextField(),
                       SizedBox(height: 4.39.h),
-                      SizedBox(height: 49.h),
-                      const ArticleContentTextField(),
-                      SizedBox(height: 40.h),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Upload Image",
-                              style: GoogleFonts.poppins(
-                                  fontSize: 16.sp,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black)),
-                          GestureDetector(
-                            onTap: () {
-                              setState(() async {
-                                _file = await ImageSheet().show(context);
-                              });
-                            },
-                            child: Icon(
-                              Icons.camera_alt_outlined,
-                              color: Colors.blue,
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5.h,
-                      ),
-                      Container(
-                        child: TextField(
-                          controller: TextEditingController(text: _file?.path),
-                          decoration: InputDecoration(
-                              hintText: _file != null
-                                  ? _file?.path
-                                  : "No Image is selected"),
+                      Text(
+                        "add as many tags as you want",
+                        style: GoogleFonts.poppins(
+                          fontSize: 11.sp,
+                          fontWeight: FontWeight.w200,
+                          color: hintTextColor,
                         ),
                       ),
+                      SizedBox(height: 49.h),
+                      const ArticleContentTextField(),
+                      SizedBox(height: 87.h),
                       const FormSubmitButton()
                     ],
                   ),
@@ -85,3 +52,11 @@ class _WriteArticlePageState extends State<WriteArticlePage> {
         });
   }
 }
+
+
+
+
+
+
+
+
