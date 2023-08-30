8bb67a81fd71c39f7ced9c3a90d4d7c0bfba5ae2
mengesha9 <mengistuashebir127>
2023-08-29 19:45:26 -0700

    feat(aait.back.group-2B):adding querie feature

--- aait/backend/group-2B/WebApi/Controllers/FollowUnfllowUpdateDelete.cs
+++ aait/backend/group-2B/WebApi/Controllers/FollowUnfllowUpdateDelete.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Application.DTOs.Common;
 using Applicatin.Features.Users.Handlers.Commands;
 using Applicatin.Features.Users.Requests.Commands;
-
+using Application.DTOs.Users;
 
 namespace WebApi.Controllers;
 [ApiController]
@@ -19,7 +19,34 @@ public class FollowUnfllowUpdateDelete : ControllerBase
     {
         _mediator = mediator;
     }
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetUser(){
+        var user = new GetUserQuerieRequestHanler();
+        var response = await _mediator.Send(user);
+        return Ok(response);
+    }
+    [HttpGet("getusers")]
+    public async Task<ActionResult> GetAllUsers(int id)
+    {
+        var user = new GetAllUserQuerieRequestHanler{Id = id};
+        var response = await _mediator.Send(user);
+        return Ok(response);
+    }
+    [HttpGet("followers/{id}")]
+    public async  Task<ActionResult> GetAllFollowers(int id){
 
+        var followers = new GetFollowersQuerieRequestHandler { Id = id };
+        var response = await _mediator.Send(followers);
+        return Ok(response);
+    }
+    [HttpGet("followings/{id}")]
+    public async Task<ActionResult> GetAllFollowings(int id)
+    {
+
+        var followers = new GetFollowingsQuerieRequestHandler { Id = id };
+        var response = await _mediator.Send(followers);
+        return Ok(response);
+    }
 
     [HttpPost("Follow")]
     public async Task<IActionResult> UserFollowApi([FromBody] FollowUnFollowDto followDto)
@@ -34,9 +61,9 @@ public class FollowUnfllowUpdateDelete : ControllerBase
 
 
     [HttpPost("unfollow")]
-    public async Task<IActionResult> UserUnFollowApi([FromBody] FolllowUnFollowDto unFollowDto)
+    public async Task<IActionResult> UserUnFollowApi([FromBody] FollowUnFollowDto unFollowDto)
     {
-        var command = new UnFollowUserCommandRequest { UnfollowunFollowDto = unFollowDto };
+        var command = new UnFollowUserCommandRequest { UnfollowDto = unFollowDto };
         var response = await _mediator.Send(command);
 
         return Ok(response);
@@ -54,7 +81,7 @@ public class FollowUnfllowUpdateDelete : ControllerBase
 
     [HttpDelete("Delate")]
     public async Task<IActionResult> DelateUserAccount(
-        [FromBody] UserDeleteDto userDeleteDto
+        [FromBody] UserDeletDto userDeleteDto
     )
     {
         var command = new UserDeleteCommandRequest { UserdeleteDto = userDeleteDto };
