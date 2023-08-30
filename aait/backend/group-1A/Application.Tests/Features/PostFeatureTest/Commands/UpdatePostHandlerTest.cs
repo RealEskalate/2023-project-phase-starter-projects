24d023abbd6a723a41a2c500919001c4071482cc
Tofik Abdu <tofik.abdu@a2sv.org>
2023-08-30 16:59:26 +0300

    Add Reaction Test

--- aait/backend/group-1A/Application.Tests/Features/PostFeatureTest/Commands/UpdatePostHandlerTest.cs
+++ aait/backend/group-1A/Application.Tests/Features/PostFeatureTest/Commands/UpdatePostHandlerTest.cs
@@ -1,81 +1,81 @@
-// using Application.Contracts;
-// using Application.DTO.PostDTO.DTO;
-// using Application.Exceptions;
-// using Application.Features.PostFeature.Handlers.Commands;
-// using Application.Features.PostFeature.Requests.Commands;
-// using Application.Profiles;
-// using Application.Response;
-// using Application.Tests.Mocks;
-// using AutoMapper;
-// using Domain.Entities;
-// using Moq;
-// using Shouldly;
+using Application.Contracts;
+using Application.DTO.PostDTO.DTO;
+using Application.Exceptions;
+using Application.Features.PostFeature.Handlers.Commands;
+using Application.Features.PostFeature.Requests.Commands;
+using Application.Profiles;
+using Application.Response;
+using Application.Tests.Mocks;
+using AutoMapper;
+using Domain.Entities;
+using Moq;
+using Shouldly;
 
-// namespace Application.Tests.Features.PostFeatureTest.Commands
-// {
-//     public class UpdatePostHandlerTest
-//     {
-//             private readonly IMapper _mapper;
-//         private readonly Mock<IPostRepository> _mockRepo;
-//         public UpdatePostHandlerTest()
-//         {
-//              _mockRepo = MockPostRepository.GetPostRepository();
+namespace Application.Tests.Features.PostFeatureTest.Commands
+{
+    public class UpdatePostHandlerTest
+    {
+            private readonly IMapper _mapper;
+        private readonly Mock<IPostRepository> _mockRepo;
+        public UpdatePostHandlerTest()
+        {
+             _mockRepo = MockPostRepository.GetPostRepository();
 
-//             var mapperConfig = new MapperConfiguration(c => 
-//             {
-//                 c.AddProfile<MappingProfile>();
-//             });
+            var mapperConfig = new MapperConfiguration(c => 
+            {
+                c.AddProfile<MappingProfile>();
+            });
 
-//             _mapper = mapperConfig.CreateMapper();   
-//         }
+            _mapper = mapperConfig.CreateMapper();   
+        }
 
-//         public async Task UpdatePostValidTest()
-//         {
-//             var handler = new UpdatePostHandler(_mockRepo.Object,_mapper);
+        public async Task UpdatePostValidTest()
+        {
+            var handler = new UpdatePostHandler(_mockRepo.Object,_mapper);
 
-//             var updatedPost = new PostUpdateDTO(){
-//                 Title = "new updated Data",
-//                 Content = "new Updated Data content"
-//             };
-//             var result = await handler.Handle(new UpdatePostCommand(){Id = 1, userId = 1, PostUpdateData = updatedPost},CancellationToken.None);
+            var updatedPost = new PostUpdateDTO(){
+                Title = "new updated Data",
+                Content = "new Updated Data content"
+            };
+            var result = await handler.Handle(new UpdatePostCommand(){Id = 1, userId = 1, PostUpdateData = updatedPost},CancellationToken.None);
 
-//             result.ShouldBeOfType<BaseResponse<PostResponseDTO>>();
+            result.ShouldBeOfType<BaseResponse<PostResponseDTO>>();
             
-//         }
+        }
 
 
-//         [Fact]
-//         public async Task UpdatePostNonExistingPostTest()
-//         {
-//             // Arrange
-//             _mockRepo.Setup(repo => repo.Exists(It.IsAny<int>())).ReturnsAsync(false);
-//             var handler = new UpdatePostHandler(_mockRepo.Object, _mapper);
+        [Fact]
+        public async Task UpdatePostNonExistingPostTest()
+        {
+            // Arrange
+            _mockRepo.Setup(repo => repo.Exists(It.IsAny<int>())).ReturnsAsync(false);
+            var handler = new UpdatePostHandler(_mockRepo.Object, _mapper);
 
-//             var updatedPost = new PostUpdateDTO()
-//             {
-//                 Title = "new updated Data",
-//                 Content = "new Updated Data content"
-//             };
+            var updatedPost = new PostUpdateDTO()
+            {
+                Title = "new updated Data",
+                Content = "new Updated Data content"
+            };
 
-//             // Act & Assert
-//             await Should.ThrowAsync<NotFoundException>(async () =>
-//                 await handler.Handle(new UpdatePostCommand() { Id = 1, userId = 1, PostUpdateData = updatedPost }, CancellationToken.None));
-//             _mockRepo.Verify(repo => repo.Update(It.IsAny<Post>()), Times.Never);
-//         }
+            // Act & Assert
+            await Should.ThrowAsync<NotFoundException>(async () =>
+                await handler.Handle(new UpdatePostCommand() { Id = 1, userId = 1, PostUpdateData = updatedPost }, CancellationToken.None));
+            _mockRepo.Verify(repo => repo.Update(It.IsAny<Post>()), Times.Never);
+        }
 
 
-//         [Fact]
-//         public async Task UpdatePostInvalidDataTest()
-//         {
-//             // Arrange
-//             var handler = new UpdatePostHandler(_mockRepo.Object, _mapper);
+        [Fact]
+        public async Task UpdatePostInvalidDataTest()
+        {
+            // Arrange
+            var handler = new UpdatePostHandler(_mockRepo.Object, _mapper);
 
-//             var updatedPost = new PostUpdateDTO(); // Invalid data, missing required properties
+            var updatedPost = new PostUpdateDTO(); // Invalid data, missing required properties
 
-//             // Act & Assert
-//             await Should.ThrowAsync<ValidationException>(async () =>
-//                 await handler.Handle(new UpdatePostCommand() { Id = 1, userId = 1, PostUpdateData = updatedPost }, CancellationToken.None));
-//             _mockRepo.Verify(repo => repo.Update(It.IsAny<Post>()), Times.Never);
-//         }
-//     }
-// }
+            // Act & Assert
+            await Should.ThrowAsync<ValidationException>(async () =>
+                await handler.Handle(new UpdatePostCommand() { Id = 1, userId = 1, PostUpdateData = updatedPost }, CancellationToken.None));
+            _mockRepo.Verify(repo => repo.Update(It.IsAny<Post>()), Times.Never);
+        }
+    }
+}
--- aait/backend/group-1A/Application/Features/PostFeature/Handlers/Queries/GetAllPostHandler.cs
+++ aait/backend/group-1A/Application/Features/PostFeature/Handlers/Queries/GetAllPostHandler.cs
@@ -1,35 +1,35 @@
-﻿// using Application.Contracts;
-// using Application.DTO.PostDTO.DTO;
-// using Application.Features.PostFeature.Requests.Queries;
-// using Application.Response;
-// using AutoMapper;
-// using MediatR;
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Text;
-// using System.Threading.Tasks;
+﻿using Application.Contracts;
+using Application.DTO.PostDTO.DTO;
+using Application.Features.PostFeature.Requests.Queries;
+using Application.Response;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-// namespace Application.Features.PostFeature.Handlers.Queries
-// {
-//     public class GetAllPostHandler : IRequestHandler<GetAllPostsQuery, BaseResponse<List<PostResponseDTO>>>
-//     {
-//         private readonly IPostRepository _postRepository;
-//         private readonly IMapper _mapper;
+namespace Application.Features.PostFeature.Handlers.Queries
+{
+    public class GetAllPostHandler : IRequestHandler<GetAllPostsQuery, BaseResponse<List<PostResponseDTO>>>
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly IMapper _mapper;
 
-//         public GetAllPostHandler(IPostRepository postRepository, IMapper mapper)
-//         {
-//             _postRepository = postRepository;
-//             _mapper = mapper;
-//         }
-//         public async Task<BaseResponse<List<PostResponseDTO>>> Handle(GetAllPostsQuery request, CancellationToken cancellationToken)
-//         {
-//             var result = await _postRepository.GetAllPostsWithReaction(request.userId);
-//             return new BaseResponse<List<PostResponseDTO>> {
-//                 Success = true,
-//                 Message = "Posts are retrieved successfully",
-//                 Value = _mapper.Map<List<PostResponseDTO>>(result)
-//             };
-//         }
-//     }
-// }
+        public GetAllPostHandler(IPostRepository postRepository, IMapper mapper)
+        {
+            _postRepository = postRepository;
+            _mapper = mapper;
+        }
+        public async Task<BaseResponse<List<PostResponseDTO>>> Handle(GetAllPostsQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _postRepository.GetAllPostsWithReaction(request.userId);
+            return new BaseResponse<List<PostResponseDTO>> {
+                Success = true,
+                Message = "Posts are retrieved successfully",
+                Value = _mapper.Map<List<PostResponseDTO>>(result)
+            };
+        }
+    }
+}
