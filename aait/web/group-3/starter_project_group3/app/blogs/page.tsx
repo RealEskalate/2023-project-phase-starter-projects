7f246e3172512509ac6c773b453d18ee4f854756
Yohannes Ahunm <106800042+yohannes-07@users.noreply.github.com>
2023-08-30 09:28:45 +0300

    Aait.web.g3.yohannes ahunm.create blog page (#430)

--- aait/web/group-3/starter_project_group3/app/blogs/page.tsx
+++ aait/web/group-3/starter_project_group3/app/blogs/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import BlogCard from "@/components/blog/BlogCard";
 import SearchBar from "@/components/blog/SearchBar";
 import Pagination from "@/components/blog/Pagination";
@@ -9,12 +9,18 @@ import Error from "@/components/commons/Error";
 import { useGetBlogsQuery } from "@/store/features/blogs/blogs-api";
 import Loading from "@/components/commons/Loading";
 
-const itemsPerPage = 4;
+const PageSize = 4;
 
 const BlogPage: React.FC = () => {
     const {data:blogs, error, isLoading  } = useGetBlogsQuery();
-    const [currentPage, setCurrentPage] = useState(1)
+    const [currentPage, setCurrentPage] = useState(1);
 
+    const currentBlogs = useMemo(() => {
+      const firstPageIndex = (currentPage - 1) * PageSize;
+      const lastPageIndex = firstPageIndex + PageSize;
+      return blogs?.slice(firstPageIndex, lastPageIndex);
+    }, [currentPage]);
+    
     if (isLoading){ 
       return <Loading />
     
@@ -22,40 +28,33 @@ const BlogPage: React.FC = () => {
     if (error){
         return <Error />    
     }
-    if (blogs) {
 
-      const indexOfLastBlog = currentPage * itemsPerPage;
-      const indexOfFirstBlog = indexOfLastBlog - itemsPerPage
-      const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog);
-      const totalPages = Math.ceil(blogs.length / itemsPerPage)
-  
-      const handlePageChange = (page:number) => {
-          setCurrentPage(page)
+    if (blogs) {
+      return (
+        <div>
+          <SearchBar />
+          <div className="flex flex-col gap-4">
+            {currentBlogs?.map((blog) => (
+              <Link href={`/blogs/${blog._id}`}>
+                  <BlogCard key={blog._id} {...blog} />
+              </Link>
+            ))}
+          </div>
+          <div className="m-16 flex justify-center">
+            <Pagination
+              className="pagination-bar"
+              currentPage={currentPage}
+              totalCount={blogs.length}
+              pageSize={PageSize}
+              onPageChange={page => setCurrentPage(page)}
+            
+            />
+          </div>
+        </div>
+      );
+      }else{
+        return null
       }
-  
-    return (
-      <div>
-        <SearchBar />
-        <div className="flex flex-col gap-4">
-          {currentBlogs.map((blog) => (
-            <Link key={blog._id} href={`/blogs/${blog._id}`}>
-                <BlogCard key={blog._id} {...blog} />
-            </Link>
-          ))}
-        </div>
-        <div className="m-16 flex justify-center">
-          <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            onPageChange={handlePageChange}
-            className="pagination"
-          />
-        </div>
-      </div>
-    );
-    }else{
-      return null
-    }
 };
 
 export default BlogPage;
