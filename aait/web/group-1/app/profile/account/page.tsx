6c02ddf27e70103138205008bf4e008b2270b56d
Son-OfAnton <admasterefe00@gmail.com>
2023-08-30 09:39:41 +0300

    feat(AAiT.web.g1): Implemented change password

--- aait/web/group-1/app/profile/account/page.tsx
+++ aait/web/group-1/app/profile/account/page.tsx
@@ -1,9 +1,26 @@
 'use client'
 
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import { useChangePasswordMutation } from '@/store/features/user/userApi';
+
 
 export default function Page() {
+  const [changePassword] = useChangePasswordMutation()
+  const [oldPassword, setOldPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [confirmPass, setConfirmPass] = useState('')
+
+  const handleSaveChanges = () => {
+    if(newPassword !== confirmPass) {
+      window.alert("Password don't match")
+      return
+    }
+    changePassword({"oldPassword": oldPassword, "newPassword": newPassword})
+
+  }
+
+
   return (
     <div className='flex flex-col space-y-16 font-montserrat'>
       <div className='flex justify-between'>
@@ -11,7 +28,12 @@ export default function Page() {
           <h3 className='text-[13px] text-[#5D5D5D] font-semibold'>Manage Your Account</h3>
           <p className='text-[10px] text-[#868686] font-light'>You can change your password here</p>
         </div>
-        <button className='text-[10px] font-semibold bg-[#264FAD] rounded-md text-white px-12 py-2'>Save Changes</button>
+        <button 
+          className='text-[10px] font-semibold bg-[#264FAD] rounded-md text-white px-12 py-2'
+          onClick={handleSaveChanges}
+        >
+          Save Changes
+        </button>
       </div>
       <form className='grid grid-cols-3 place-items-start items-center text-[11px] font-poppins w-full max-w-md mx-auto gap-y-5'>
         <label className='col-span-1 font-semibold text-[#565656]'>Current Password</label>
@@ -20,6 +42,7 @@ export default function Page() {
             type="password"
             className='focus:outline-none bg-[#EFF3F9]'
             placeholder='Enter your current password'
+            onChange={(e) => setOldPassword(e.target.value)}
             />
           <Image
             src='/images/toggle-password-status.png'
@@ -35,6 +58,7 @@ export default function Page() {
             type="password"
             className='focus:outline-none bg-[#EFF3F9]'
             placeholder='Enter new password'
+            onChange={(e) => setNewPassword(e.target.value)}
             />
           <Image
             src='/images/toggle-password-status.png'
@@ -50,6 +74,7 @@ export default function Page() {
             type="password"
             className='focus:outline-none bg-[#EFF3F9]'
             placeholder='Confirm new password'
+            onChange={(e) => setConfirmPass(e.target.value)}
             />
           <Image
             src='/images/toggle-password-status.png'
