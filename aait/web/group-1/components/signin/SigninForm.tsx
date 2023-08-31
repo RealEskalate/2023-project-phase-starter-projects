2cd9eed7d0cb18e0ea9c35846ab01579bbf7a6b7
Son-OfAnton <admasterefe00@gmail.com>
2023-08-31 12:05:42 +0300

    fix(AAiT.web.1): Fixed multiple toast error signin

--- aait/web/group-1/components/signin/SigninForm.tsx
+++ aait/web/group-1/components/signin/SigninForm.tsx
@@ -1,54 +1,62 @@
-"use client"
+"use client";
 
-import { ChangeEvent, useState } from "react"
-import { useLoginMutation } from "@/store/auth/authApi"
+import { ChangeEvent, useState } from "react";
+import { useLoginMutation } from "@/store/auth/authApi";
 
-import TextField from "../signup/TextField"
-import { useRouter } from "next/navigation"
+import TextField from "../signup/TextField";
+import { useRouter } from "next/navigation";
 
-import { toast } from 'react-toastify'
+import { toast } from "react-toastify";
 
 const fieldInfo: Array<Array<string>> = [
-  ["text", "email", "Email"], 
-  ["password", "password", "Password"]
-]
-                                            
+  ["text", "email", "Email"],
+  ["password", "password", "Password"],
+];
 
 const SignInForm = () => {
-  const router = useRouter()
-  const [login, { isLoading, isError, isSuccess, error }] = useLoginMutation()
+  const router = useRouter();
+  const [login, { isLoading, data, isError, isSuccess, error }] =
+    useLoginMutation();
   const [credentials, setCredentials] = useState({
     email: "",
     password: "",
-  })
+  });
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setCredentials((prevData) => ({...prevData, [name]: value}))
-  }
+    const { name, value } = e.target;
+    setCredentials((prevData) => ({ ...prevData, [name]: value }));
+  };
 
-  const handleSignin = () => {
-    if(!credentials.email || !credentials.password) {
-      toast.error("Please fill all fields")
+  const handleSignin = async (e) => {
+    e.preventDefault();
+    if (!credentials.email || !credentials.password) {
+      toast.error("Please fill all fields");
     }
-    login(credentials)
-      .unwrap()
-      .then((response) => {
-        localStorage.setItem("user", JSON.stringify(response))
-        router.push('/')
-      })
-      .catch((err) => {
-        console.error('Login error', err);
-      })
-  }
+    const response = await login(credentials);
+    if (isSuccess) {
+      localStorage.setItem("user", JSON.stringify(response));
+      toast.success("Signed in successfully");
+      router.push("/");
+    }
+    if (error) {
+      if ("status" in error) {
+        const errMsg =
+          "error" in error ? error.error : JSON.stringify(error?.data?.message);
+
+        toast.error(errMsg || 'Unable to signin');
+      }
+    }
+  };
 
   return (
     <div className="flex flex-col text-left gap-3.5 bg-white text-white border rounded-lg p-10 w-96">
       <h2 className="text-form-gray-primary font-semibold text-3xl">Login</h2>
-      <p className="text-form-gray-primary font-light mb-6">Hey, enter your details to sign in to your account</p>
+      <p className="text-form-gray-primary font-light mb-6">
+        Hey, enter your details to sign in to your account
+      </p>
       {fieldInfo.map((field) => (
         <TextField
-          key={field[1]} 
+          key={field[1]}
           type={field[0]}
           name={field[1]}
           id={field[1]}
@@ -58,20 +66,17 @@ const SignInForm = () => {
         />
       ))}
       <button
-        className={`btn-blue ${isLoading ? "opacity-60": ""} py-3 px-6 mt-6 text-sm font-semibold`}
+        className={`btn-blue ${
+          isLoading ? "opacity-60" : ""
+        } py-3 px-6 mt-6 text-sm font-semibold`}
         type="button"
-        onClick={handleSignin}
+        onClick={(e) => handleSignin(e)}
         disabled={isLoading}
       >
-        {isLoading ? 'Signing in...' : 'Sign in'}
+        {isLoading ? "Signing in..." : "Sign in"}
       </button>
-      {isError && toast.error('Unable to signin')}
-      {isSuccess && toast.success('Signed in successfully')}
-
     </div>
-  )
-}
+  );
+};
 
-
-
-export default SignInForm
+export default SignInForm;
