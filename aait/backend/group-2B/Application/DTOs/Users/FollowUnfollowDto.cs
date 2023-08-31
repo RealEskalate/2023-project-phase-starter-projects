16bcf7b6882008669323e36e717baff5305956fd
ffekirnew <ffekirnew0808@gmail.com>
2023-08-31 16:39:35 +0300

    fix(aait-backend-2b): Fix user related dtos

--- aait/backend/group-2B/Application/DTOs/Users/FollowUnfollowDto.cs
+++ aait/backend/group-2B/Application/DTOs/Users/FollowUnfollowDto.cs
@@ -1,10 +1,7 @@
-
-
 namespace Application.DTOs.Common;
 
 public class FollowUnFollowDto
 {
     public int FollwerId { get; set; }
     public int FollowedId { get; set; }
-
 }
--- aait/backend/group-2B/Application/DTOs/Users/UserDeleteDto.cs
+++ aait/backend/group-2B/Application/DTOs/Users/UserDeleteDto.cs
@@ -1,10 +1,8 @@
-
 using SocialSync.Application.DTOs.Common;
 
 namespace Application.DTOs.Users;
 
 public class UserDeleteDto : BaseDto
 {
-
     public int Owner { get; set; }
 }
